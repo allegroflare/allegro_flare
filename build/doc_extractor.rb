26319878db4b9f993040c7b525207fb62af3f3ac
Mark Oates <euph_boy@hotmail.com>
2015-04-17 16:53:52 -0400

    improved the documentation generator script

--- build/doc_extractor.rb
+++ build/doc_extractor.rb
@@ -2,18 +2,26 @@
 
 
 
-
+ 
 
 class CodeDef
-	attr_reader :line_number, :section, :declaration, :description, :func_name
+	attr_reader :line_number, :section, :declaration, :description, :name, :todo
 
 	def initialize(line_number, section, declaration, description)
 		@line_number = line_number
 		@section = section
 		@declaration = declaration
-		function_name_reg = / (?<func_identifier>.*)\(/
-		declaration.scan(function_name_reg) { |val| puts val }
-		@description = description
+
+		# extract *only* the function name
+		function_name_matchdata = / (?<func_identifier>[_A-Za-z0-9:]*)\(/.match(declaration)
+		@name = function_name_matchdata[:func_identifier] if !function_name_matchdata.nil?
+
+		@todo = description.match(/TODO[\s\S]*/)
+		if @todo.nil?
+			@description = description
+		else
+			@description = description.gsub(@todo.to_s, "") if !@todo.nil?
+		end
 	end
 end
 
@@ -32,12 +40,12 @@ class DocParser
 
 	def parse_contents
 		# myregex = /\n(?<name>(\n\/\/ (.+))+)(?<funcs>(\n.+)+)/
-		myregex = /\n(?<name>(\n\/\/ (.+))+)(?<function_def>(\n.+)+)/
+		myregex = /\n(?<description>(\n\/\/ (.+))+)(?<function_def>(\n.+)+)/
 
 		str = File.read(@filename)
 
 		str.scan(myregex) do |a, b|
-			@function_defs.push(CodeDef.new(0, '--', b, a))
+			@function_defs.push(CodeDef.new(0, '--', b, a.gsub("// ", "")))
 		end
 	end
 
@@ -49,9 +57,10 @@ class DocParser
 		content += "<H1>AllegroFlare Documentation</H1>\n";
 
 		@function_defs.each do |fdef|
-			content += "<h3>#{fdef.func_name}</h3>\n"
+			content += "<h3>#{fdef.name}</h3>\n"
 			content += "<code><pre>#{fdef.declaration}</pre></code>\n"
 			content += "<p>#{fdef.description}</p>\n"
+			content += "<p class='todo'>#{fdef.todo}</p>" if !fdef.todo.nil? 
 		end
 
 		content += "\n\n</body>\n"
@@ -65,6 +74,7 @@ end
 doc_parser = DocParser.new
 #puts doc_parser.function_defs.inspect
 doc_parser.write_html
+
 
 
 
