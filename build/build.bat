e8ad62e0b4cc4af34fc3d13dcf5f18cfcfa372ba
Mark Oates <euph_boy@hotmail.com>
2015-04-21 16:33:27 -0400

    updated build.bat now builds in g++; .gitignore now ignores build/tmp

--- build/build.bat
+++ build/build.bat
@@ -1,9 +1,21 @@
-#!/bin/bash
 
 
-SET ALLEGRO_INCLUDE_DIR = "E:/allegro-5.1.8-msvc-10.0/include"
+SET ALLEGRO_INCLUDE_DIR="E:/allegro-5.1.8-mingw-4.7.0/include"
+SET ALLEGRO_LIB_DIR="E:/allegro-5.1.8-mingw-4.7.2/lib"
 
-g++ ./src/allegro_flare.cpp -I"./include" -I$ALLEGRO_INCLUDE_DIR
+SET BOOST_INCLUDE_DIR="E:/boost/boost_1_57_0"
+SET OPEN_CV_INCLUDE_DIR="E:/opencv-2.4.3/include"
+SET CURL_INCLUDE_DIR="E:/curl-7.19.3/include"
 
-g++ *.o -Wall -lallegro -lallegro_color -lallegro_primitives -lallegro_audio -lallegro_dialog -lallegro_image -lallegro_font -lallegro_ttf -lallegro_acodec -o allegro_flare
+SET ALLEGRO_FLARE_INCLUDE_DIR="E:/allegro_flare/include"
+
+REM g++ *.o -Wall -lallegro -lallegro_color -lallegro_primitives -lallegro_audio -lallegro_dialog -lallegro_image -lallegro_font -lallegro_ttf -lallegro_acodec -o allegro_flare
+
+
+
+REM "---------------------"
+
+FOR %%i IN (../src/*) DO (
+	g++ -c -o "./tmp/%%~ni.o" "../src/%%~ni.cpp" -I%ALLEGRO_FLARE_INCLUDE_DIR% -I%ALLEGRO_INCLUDE_DIR% -I%BOOST_INCLUDE_DIR% -I%OPEN_CV_INCLUDE_DIR% -I%CURL_INCLUDE_DIR%
+)
 
