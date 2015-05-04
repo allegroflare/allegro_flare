b80937f39c5f2612ed101633efc6fede34ab5e55
Mark Oates <euph_boy@hotmail.com>
2015-05-03 20:37:17 -0400

    added comment in render_path2d.h

--- include/allegro_flare/render_path2d.h
+++ include/allegro_flare/render_path2d.h
@@ -10,6 +10,8 @@
 
 
 
+// TODO: these functions might need to be placed somewhere else.  Maybe in assets or with the texture generator functions 
+
 path2d create_path_from_interpolator(interpolator::interpolator_func_t func, int num_segments=16);
 
 ALLEGRO_BITMAP *create_interpolator_render(int w, int h, interpolator::interpolator_func_t func, float line_thickness=8, float padding=8, int num_segments=16);
