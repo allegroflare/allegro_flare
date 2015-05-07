6d2049ed72ebf2eb12aee5b0882fd9c2cd331ec3
Mark Oates <euph_boy@hotmail.com>
2015-05-07 14:59:06 -0400

    added sanity check for new window positioning

--- src/display.cpp
+++ src/display.cpp
@@ -15,24 +15,39 @@ Display *Display::find_display(ALLEGRO_DISPLAY *display)
 
 
 Display::Display(int width, int height, int display_flags)
-	: _background_color(color::hex("6ca3fe"))
+	: _background_color(color::hex("abc788"))
 	, _width(width)
 	, _height(height)
 {
-
+	// set a few options and flags
 	al_set_new_display_option(ALLEGRO_SAMPLE_BUFFERS, 2, ALLEGRO_SUGGEST);
 	al_set_new_display_option(ALLEGRO_DEPTH_SIZE, 16, ALLEGRO_SUGGEST); // hmm... not sure about this one either
-	al_set_new_display_option(ALLEGRO_SAMPLES, 4, ALLEGRO_SUGGEST);
-//	al_set_new_display_flags(display_flags | ALLEGRO_OPENGL);	// Previously, OpenGL was forced because AllegroFlare
-																// required certain specific features.  However,
-																// now having the program run in OpenGL mode causes
-																// offscreen bitmaps to not be drawn correctly. So
-																// this feature has been removed.
-	
+	al_set_new_display_option(ALLEGRO_SAMPLES, 16, ALLEGRO_SUGGEST);
 	al_set_new_display_flags(display_flags);
 
+	// create the actual display
  	display = al_create_display(width, height);
+
+	// add the display to AllegroFlare's list of displays
 	displays.push_back(this);
+
+	// HACK: This is a sanity check.  When using an AMD Radeon HD 7670M on my system,
+	// newly created displays are positioned offscren, at (32767, 32767).
+	// In the event that this occurs, then a display will be repositioned
+	// back to a sane coordinate on the desktop.
+	int dx, dy;
+	al_get_window_position(display, &dx, &dy);
+	if (dx >= 1920 || dy >= 1080)
+	{
+		std::cout << "Display positioned at (" << dx << ", " << dy << ")." << std::endl;
+
+		int new_pos_x = 1920/2 - width/2;
+		int new_pos_y = 1080/2 - height/2;
+
+		std::cout << "Repositioning display to (" << new_pos_x << ", " << new_pos_y << ")" << std::endl;
+		al_set_window_position(display, new_pos_x, new_pos_y);
+	}
+
 }
 
 
