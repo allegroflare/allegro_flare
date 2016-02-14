57ea4def963dbccacdb261ea88d40bf9c0f3d4ea
Mark Oates <mark.oates@shopify.com>
2016-02-14 18:27:10 -0500

    Parse multiple header files for docs

--- docs/parse_source.py
+++ docs/parse_source.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import glob
 import sqlite3
 
 from pygccxml import utils
@@ -11,73 +12,100 @@ from pygccxml import parser
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
 
-# Find the location of the xml generator (castxml or gccxml)
-generator_path, generator_name = utils.find_xml_generator(name='castxml')
-
-
-# Configure the xml generator
-xml_generator_config = parser.xml_generator_configuration_t(
-    # cflags="-std=gnu++11",
-    include_paths=["/Users/markoates/Repos/allegro_flare/include"],
-    xml_generator_path=generator_path,
-    compiler="gcc",
-    xml_generator=generator_name)
 
 
 # The c++ file we want to parse
-filename = "../include/allegro_flare/useful.h"
+# filename_file = sys.argv[1]
+# filename = "../include/allegro_flare/" + filename_file
+
+'''
+create table parsed_declarations(
+           id int primary key not null
+           signature varchar(255),
+           header_file varchar(255),
+           line_number int,
+           name varchar(128),
+           );
+'''
+
+def parse_file(filename):
+    # Find the location of the xml generator (castxml or gccxml)
+    generator_path, generator_name = utils.find_xml_generator(name='castxml')
+
+    # Configure the xml generator
+    xml_generator_config = parser.xml_generator_configuration_t(
+        # cflags="-std=gnu++11",
+        cflags="-Wno-c++11-extensions",
+        include_paths=["/Users/markoates/Repos/allegro_flare/include"],
+        xml_generator_path=generator_path,
+        compiler="g++",
+        xml_generator=generator_name)
+
+    # Parse the c++ file
+    decls = parser.parse([filename], xml_generator_config)
 
 
-# Parse the c++ file
-decls = parser.parse([filename], xml_generator_config)
+    # output some GOLD!
+
+    global_namespace = declarations.get_global_namespace(decls)
+
+    # Search for functions which return a double. Two functions will be found
+    criteria = declarations.calldef_matcher(
+         #return_type="float",
+         #header_file="/Users/markoates/Repos/allegro_flare/include/allegro_flare/" + filename)
+         header_file=os.path.abspath(filename))
+    # criteria = declarations.calldef_matcher(return_type=double_type)
+
+    found_items = declarations.matcher.find(criteria, global_namespace)
 
 
 
-# output some GOLD!
+    # create a unique list of declaration names
 
-global_namespace = declarations.get_global_namespace(decls)
+    unique_item_names = set();
 
-# Search for functions which return a double. Two functions will be found
-criteria = declarations.calldef_matcher(
-     #return_type="float",
-     header_file="/Users/markoates/Repos/allegro_flare/include/allegro_flare/useful.h")
-# criteria = declarations.calldef_matcher(return_type=double_type)
-
-found_items = declarations.matcher.find(criteria, global_namespace)
+    for item in found_items:
+        print item.location.file_name + " : " + str(item.location.line)
+        unique_item_names.update({item.name})
 
 
 
-# create a unique list of declaration names
+    # cross-correlate declarations in the database
 
-unique_item_names = set();
-
-for item in found_items:
-    print item.location.file_name + " : " + str(item.location.line)
-    unique_item_names.update({item.name})
+    connection = sqlite3.connect('doc_entries.db')
+    connection.row_factory = sqlite3.Row
+    c = connection.cursor()
 
 
+    found_items = 0
+    unfound_items = 0
 
-# cross-correlate declarations in the database
+    for item in unique_item_names:
+        c.execute('SELECT * FROM entries WHERE decl=?', (item, ))
+        entries = c.fetchall()
+        if len(entries) == 0:
+            print item
+            unfound_items += 1
+        else:
+            print item + " - FOUND"
+            found_items += 1
 
-connection = sqlite3.connect('doc_entries.db')
-connection.row_factory = sqlite3.Row
-c = connection.cursor()
+    print "=============================="
+    print str(found_items) + " items found."
+    print str(unfound_items) + " matches missing."
+    print "=============================="
+
+    return
 
 
-found_items = 0
-unfound_items = 0
 
-for item in unique_item_names:
-    c.execute('SELECT * FROM entries WHERE decl=?', (item, ))
-    entries = c.fetchall()
-    if len(entries) == 0:
-        print item
-        unfound_items += 1
-    else:
-        print item + " - FOUND"
-        found_items += 1
 
-print "=============================="
-print str(found_items) + " items found."
-print str(unfound_items) + " matches missing."
-print "=============================="
+# get a file list
+header_files = glob.glob("../include/allegro_flare/*/*/*.h")
+header_files += glob.glob("../include/allegro_flare/*/*.h")
+header_files += glob.glob("../include/allegro_flare/*.h")
+
+
+
+for f in header_files:
+    parse_file(f)
