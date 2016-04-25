f59737ab0e3f2720eb502fdf5dec75b9e60a9403
Mark Oates <euph_boy@hotmail.com>
2016-04-24 23:53:45 -0400

    Add DecoratedCout for better logging

---
+++ include/allegro_flare/decorated_cout.h
@@ -0,0 +1,45 @@
+#ifndef __ALLEGRO_FLARE_DECORATED_COUT
+#define __ALLEGRO_FLARE_DECORATED_COUT
+
+
+
+#include <iostream>
+#include <fstream>
+
+
+struct DecoratedCout
+{
+private:
+   static const char *log_filename;
+   static std::ofstream log_file;
+   static int instances;
+
+public:
+   static const char *CONSOLE_RED;
+   static const char *CONSOLE_YELLOW;
+   static const char *CONSOLE_GREEN;
+   static const char *CONSOLE_BLUE;
+   static const char *CONSOLE_DEFAULT;
+
+   const char *prefix;
+   const char *console_color;
+   bool show_to_cout;
+   DecoratedCout(const char *prefix, const char *console_color, bool show_to_cout=false);
+   ~DecoratedCout();
+
+   template <typename T>
+   DecoratedCout& operator<<(const T &x)
+   {
+      if (show_to_cout) std::cout << console_color << prefix << x << CONSOLE_DEFAULT;
+      log_file << prefix << x << std::endl;
+      return *this;
+   }
+
+   DecoratedCout& operator<< (std::ostream& (*f)(std::ostream &));
+   DecoratedCout& operator<< (std::ostream& (*f)(std::ios &));
+   DecoratedCout& operator<< (std::ostream& (*f)(std::ios_base &));
+};
+
+
+
+#endif
