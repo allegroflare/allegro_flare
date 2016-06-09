a2094e12b487860e3c7124852e66cb4d700fc5ce
Mark Oates <euph_boy@hotmail.com>
2016-06-09 15:34:05 -0400

    Fix formatting in ex_model_viewer

--- examples/ex_model_viewer.cpp
+++ examples/ex_model_viewer.cpp
@@ -1,6 +1,7 @@
 
 
 
+
 #include <allegro_flare/allegro_flare.h>
 
 
@@ -9,33 +10,33 @@
 class Project : public Screen
 {
 public:
-	Camera3D camera;
-	BitmapBin bitmaps;
-	Model3D *model;
+   Camera3D camera;
+   BitmapBin bitmaps;
+   Model3D *model;
 
-	Project(Display *display)
-		: Screen(display)
-		, camera(vec3d(0, 0, -5), vec3d(0, 0, 1), vec3d(0, 1, 0))
-		, model(new Model3D())
-	{
-		model->load_obj_file("data/models/heart_item-01.obj");
-		model->set_texture(bitmaps["heart_item-02.png"]);
-	}
+   Project(Display *display)
+      : Screen(display)
+      , camera(vec3d(0, 0, -5), vec3d(0, 0, 1), vec3d(0, 1, 0))
+      , model(new Model3D())
+   {
+      model->load_obj_file("data/models/heart_item-01.obj");
+      model->set_texture(bitmaps["heart_item-02.png"]);
+   }
 
-	void primary_timer_func() override
-	{
-		camera.set_frustum_as_camera(display->al_display);
+   void primary_timer_func() override
+   {
+      camera.set_frustum_as_camera(display->al_display);
 
-		if (model)
-		{
-			ALLEGRO_TRANSFORM t;
-			al_identity_transform(&t);
-			al_rotate_transform_3d(&t, 0, 1, 0, al_get_time()*0.5);
-			al_use_transform(&t);
+      if (model)
+      {
+         ALLEGRO_TRANSFORM t;
+         al_identity_transform(&t);
+         al_rotate_transform_3d(&t, 0, 1, 0, al_get_time()*0.5);
+         al_use_transform(&t);
 
-			model->draw();
-		}
-	}
+         model->draw();
+      }
+   }
 };
 
 
@@ -43,14 +44,13 @@ public:
 
 int main(int argc, char *argv[])
 {
-	Framework::initialize();
-	Display *display = Framework::create_display(800, 600);
-
-	Project *proj = new Project(display);
-
-	Framework::run_loop();
-
-	return 0;
+   Framework::initialize();
+   Display *display = Framework::create_display(800, 600);
+   Project *proj = new Project(display);
+   Framework::run_loop();
+   return 0;
 }
+
+
 
 
