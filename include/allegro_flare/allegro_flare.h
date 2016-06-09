a02edadf10ec1c00576b5b0a7ec785914735a53a
Mark Oates <euph_boy@hotmail.com>
2016-06-09 16:18:39 -0400

    Alphabetize order of include files

--- include/allegro_flare/allegro_flare.h
+++ include/allegro_flare/allegro_flare.h
@@ -3,74 +3,65 @@
 
 
 
+
 #define ALLEGRO_FLARE_VER "0.8.8wip"
 
 
-#include <allegro5/allegro.h>
-#include <allegro5/allegro_image.h>
-#include <allegro5/allegro_acodec.h>
-#include <allegro5/allegro_audio.h>
-#include <allegro5/allegro_primitives.h>
-#include <allegro5/allegro_color.h>
-#include <allegro5/allegro_font.h>
-#include <allegro5/allegro_memfile.h>
-#include <allegro5/allegro_acodec.h>
-#include <allegro5/allegro_ttf.h>
-#include <allegro5/allegro_native_dialog.h>
 
 
 #include <string>
 #include <vector>
+#include <zlib.h>
 
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_acodec.h>
+#include <allegro5/allegro_audio.h>
+#include <allegro5/allegro_color.h>
+#include <allegro5/allegro_font.h>
+#include <allegro5/allegro_image.h>
+#include <allegro5/allegro_memfile.h>
+#include <allegro5/allegro_native_dialog.h>
+#include <allegro5/allegro_primitives.h>
+#include <allegro5/allegro_ttf.h>
 
-#include <allegro_flare/bin.h>
 #include <allegro_flare/bins/bitmap_bin.h>
 #include <allegro_flare/bins/font_bin.h>
-#include <allegro_flare/bins/sample_bin.h>
 #include <allegro_flare/bins/model_bin.h>
-
-#include <allegro_flare/useful.h>
-#include <allegro_flare/bit_flags.h>
-#include <allegro_flare/profile_timer.h>
-
-#include <allegro_flare/display.h>
-#include <allegro_flare/screen.h>
-#include <allegro_flare/placement2d.h>
-#include <allegro_flare/placement3d.h>
-#include <allegro_flare/camera2d.h>
-#include <allegro_flare/camera3d.h>
-
-#include <allegro_flare/interpolators.h>
-#include <allegro_flare/motion.h>
-#include <allegro_flare/attributes.h>
-#include <allegro_flare/element_id.h>
-#include <allegro_flare/scriptable.h>
-
-#include <allegro_flare/color.h>
-#include <allegro_flare/render_sample.h>
-#include <allegro_flare/generate_textures.h>
-#include <allegro_flare/render_cache.h>
-#include <allegro_flare/image_processing.h>
-#include <allegro_flare/path2d.h>
-
-#include <allegro_flare/shader.h>
-#include <allegro_flare/model3d.h>
-
+#include <allegro_flare/bins/sample_bin.h>
 #include <allegro_flare/objects/sound_object.h>
 #include <allegro_flare/objects/text_object.h>
 #include <allegro_flare/objects/bitmap_object.h>
-
+#include <allegro_flare/attributes.h>
+#include <allegro_flare/bin.h>
+#include <allegro_flare/bit_flags.h>
+#include <allegro_flare/camera2d.h>
+#include <allegro_flare/camera3d.h>
+#include <allegro_flare/clipboard.h>
+#include <allegro_flare/color.h>
+#include <allegro_flare/config.h>
+#include <allegro_flare/display.h>
+#include <allegro_flare/element_id.h>
 #include <allegro_flare/file_path.h>
 #include <allegro_flare/file_path_object.h>
-#include <allegro_flare/config.h>
-#include <allegro_flare/clipboard.h>
-
-#include <zlib.h>
-
 #include <allegro_flare/framework.h>
+#include <allegro_flare/generate_textures.h>
 #include <allegro_flare/gui.h>
+#include <allegro_flare/image_processing.h>
+#include <allegro_flare/interpolators.h>
+#include <allegro_flare/model3d.h>
+#include <allegro_flare/motion.h>
+#include <allegro_flare/placement2d.h>
+#include <allegro_flare/placement3d.h>
+#include <allegro_flare/profile_timer.h>
+#include <allegro_flare/path2d.h>
+#include <allegro_flare/render_cache.h>
+#include <allegro_flare/render_sample.h>
+#include <allegro_flare/screen.h>
+#include <allegro_flare/scriptable.h>
+#include <allegro_flare/shader.h>
+#include <allegro_flare/useful.h>
+
 
 
 
 #endif
-
