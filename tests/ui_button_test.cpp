187bd055e8d2e623f6fc07c15962df02530e10ef
Mark Oates <euph_boy@hotmail.com>
2016-06-13 01:10:00 -0400

    Add rudimentary test for a UI widget

---
+++ tests/ui_button_test.cpp
@@ -0,0 +1,39 @@
+
+
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE UIButtonTest
+#include <boost/test/unit_test.hpp>
+
+
+
+
+#include <allegro_flare/framework.h>
+#include <allegro_flare/gui/widgets/button.h>
+
+
+
+
+struct Fixture
+{
+   Fixture()
+   {
+      Framework::initialize();
+   }
+   ~Fixture()
+   {
+      Framework::destruct();
+   }
+};
+
+
+
+
+BOOST_FIXTURE_TEST_CASE(correctly_sets_its_widget_typename, Fixture)
+{
+   UIButton button = UIButton(NULL, 0, 0, 10, 10, "Hello");
+   BOOST_CHECK_EQUAL("UIButton", button.attr.get(UI_ATTR__UI_WIDGET_TYPE));
+}
+
+
+
+
