90176c5cbb666c4495b60829eecb7b897b83002f
Mark Oates <euph_boy@hotmail.com>
2016-06-14 20:51:47 -0400

    Add rudimentary JSON parser

---
+++ include/allegro_flare/json.h
@@ -0,0 +1,149 @@
+#ifndef __AF_JSON_HEADER
+#define __AF_JSON_HEADER
+
+
+
+
+#include <string>
+#include <vector>
+#include <map>
+#include <iostream>
+#include <stdlib.h>
+
+
+
+
+namespace JSON
+{
+   class Value
+   {
+      public:
+         std::string value;
+         virtual std::string toString(void);
+   };
+
+   class Object : public Value
+   {
+      public:
+         std::map<std::string, Value *> values;
+
+         virtual std::string toString(void);
+
+         Value& operator[](std::string const &key);
+   };
+
+   class Array : public Value
+   {
+      public:
+         std::vector<Value *> values;
+         void push(Value *value);	
+
+         virtual std::string toString(void);
+         Value &operator[](size_t i) const;
+   };
+
+   class String : public Value
+   {
+      public:
+         virtual std::string toString(void);
+         std::string stringValue() const;
+   };
+
+   class Integer : public Value
+   {
+      public :
+         int intValue() const;
+   };
+
+   class Float : public Value
+   {
+      public:
+         float floatValue() const;
+   };
+
+   class Boolean : public Value
+   {
+      public:
+         Boolean(bool b);
+         bool boolValue() const;
+   };
+
+   class Null : public Value
+   {
+      public:
+         Null();
+   };
+
+   /* private */
+   class Token
+   {
+      public:
+         enum {
+            ENDOF = 0,
+            INTEGER,
+            FLOAT,
+            STRING,
+            COMMA,
+            KEYWORD,
+            COLON,
+            BRACE_OPEN,
+            BRACE_CLOSE,
+            BRACKET_OPEN,
+            BRACKET_CLOSE
+         } type;
+         std::string value;
+   };
+
+   class Parser
+   {
+      std::string json;
+      int i;
+      Token current, prev;
+
+      //Value *rootValue, *value;
+
+      void nextToken();
+
+      public:
+      Parser(const std::string &json);
+
+      bool accept(int type);
+      void expect(int type);
+
+      Value *acceptArray();
+      Value *acceptObject();
+      Value *acceptInteger();
+      Value *acceptFloat();
+      Value *acceptString();
+      Value *acceptNumber();
+      Value *acceptBoolean();
+      Value *acceptNull();
+      Value *acceptValue();
+      Value *expectValue();
+      Value *parse();
+   };
+
+   Value &decode(const std::string &json);
+}
+
+
+
+
+JSON::String json_string(std::string str);
+
+JSON::Integer json_int(std::string str);
+
+JSON::Float json_float(std::string str);
+
+JSON::Boolean json_bool(std::string str);
+
+JSON::Array json_array(std::string str);
+
+JSON::Object json_object(std::string str);
+
+JSON::Null json_null(std::string str);
+
+
+
+
+#endif
---
+++ tests/json_test.cpp
@@ -0,0 +1,118 @@
+
+
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE JSON
+#include <boost/test/unit_test.hpp>
+
+
+
+
+#include <allegro_flare/json.h>
+
+#include <fstream>
+
+
+
+
+BOOST_AUTO_TEST_CASE(decodes_a_string_value)
+{
+   BOOST_CHECK_EQUAL("Alex", json_string("\"Alex\"").stringValue());
+   BOOST_CHECK_EQUAL("Toby", json_string("\"Toby\"").stringValue());
+   BOOST_CHECK_EQUAL("Matthew", json_string("\"Matthew\"").stringValue());
+   BOOST_CHECK_EQUAL("Mark", json_string("\"Mark\"").stringValue());
+}
+
+
+
+
+BOOST_AUTO_TEST_CASE(decodes_an_integer_value)
+{
+   BOOST_CHECK_EQUAL(1293, json_int("1293").intValue());
+   BOOST_CHECK_EQUAL(42, json_int("42").intValue());
+   BOOST_CHECK_EQUAL(1024768, json_int("1024768").intValue());
+}
+
+
+
+
+BOOST_AUTO_TEST_CASE(decodes_a_float_value)
+{
+   BOOST_CHECK_CLOSE(3.14, json_float("3.14").floatValue(), 0.00001);
+   BOOST_CHECK_CLOSE(13E-13, json_float("13E-13").floatValue(), 0.00001);
+   BOOST_CHECK_CLOSE(123456.789, json_float("123456.789").floatValue(), 0.00001);
+}
+
+
+
+
+BOOST_AUTO_TEST_CASE(decodes_a_boolean_value)
+{
+   BOOST_CHECK_EQUAL(false, json_bool("false").boolValue());
+   BOOST_CHECK_EQUAL(true, json_bool("true").boolValue());
+}
+
+
+
+
+BOOST_AUTO_TEST_CASE(decodes_an_array_value)
+{
+   JSON::Array a = json_array("[8, 16, 42, 98]");
+   BOOST_CHECK_EQUAL(4, a.values.size());
+
+   int decoded_value = 0;
+   decoded_value = json_int(a.values[0]->toString()).intValue();
+   BOOST_CHECK_EQUAL(8, decoded_value);
+
+   decoded_value = json_int(a.values[1]->toString()).intValue();
+   BOOST_CHECK_EQUAL(16, decoded_value);
+
+   decoded_value = json_int(a.values[2]->toString()).intValue();
+   BOOST_CHECK_EQUAL(42, decoded_value);
+
+   decoded_value = json_int(a.values[3]->toString()).intValue();
+   BOOST_CHECK_EQUAL(98, decoded_value);
+}
+
+
+
+
+BOOST_AUTO_TEST_CASE(decodes_a_JSON_object)
+{
+   JSON::Object o = json_object("{\"width\": 900, \"height\": 600, \"name\": \"George\"}");
+
+   BOOST_CHECK_EQUAL(3, o.values.size());
+
+   int decoded_int = json_int(o.values["width"]->toString()).intValue();
+   BOOST_CHECK_EQUAL(900, decoded_int);
+
+   decoded_int = json_int(o.values["height"]->toString()).intValue();
+   BOOST_CHECK_EQUAL(600, decoded_int);
+
+   std::string decoded_string = json_string(o.values["name"]->toString()).stringValue();
+   BOOST_CHECK_EQUAL("George", decoded_string);
+}
+
+
+
+
+BOOST_AUTO_TEST_CASE(decodes_a_complex_JSON_object)
+{
+}
+
+
+
+
+BOOST_AUTO_TEST_CASE(decodes_a_null_value)
+{
+}
+
+
+
+
+BOOST_AUTO_TEST_CASE(will_throw_an_error_when_JSON_does_not_parse)
+{
+}
+
+
+
+
