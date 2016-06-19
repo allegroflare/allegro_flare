cf766b37c564018442dd82c69c3929261a14f4b0
Mark Oates <euph_boy@hotmail.com>
2016-06-19 16:56:12 -0400

    Add initial tests for balsamiq layout loader

--- tests/balsamiq_layout_loader_test.cpp
+++ tests/balsamiq_layout_loader_test.cpp
@@ -7,15 +7,31 @@
 
 
 
+#include <allegro_flare/framework.h>
+
+#include <fstream>
 #include <allegro_flare/gui/layout_loaders/balsamiq_layout_loader.h>
 
 
 
 
-BOOST_AUTO_TEST_CASE(is_not_currently_implemented)
+
+
+
+
+BOOST_AUTO_TEST_CASE(necessairy_test_file_exists)
+{
+   std::ifstream ifile(TEST_FILENAME);
+   BOOST_CHECK_EQUAL(true, (bool)ifile);
+}
+
+
+
+
+BOOST_AUTO_TEST_CASE(returns_false_when_loading_a_file_that_does_not_exist)
 {
    BalsamiqLayoutLoader loader;
-   BOOST_CHECK_EQUAL((void *)0, loader.load_file("DNE.json"));
+   BOOST_CHECK_EQUAL(false, loader.load_file(nullptr, "DNE.json"));
 }
 
 
