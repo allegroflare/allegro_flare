d2f187d384e33883759f9c6f4a78896bbaa48ce9
Mark Oates <euph_boy@hotmail.com>
2016-07-03 11:12:47 -0400

    Add tests for bitmap_alpha surface area

---
+++ tests/ui_surface_area_bitmap_alpha_test.cpp
@@ -0,0 +1,87 @@
+
+
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE UISurfaceAreaBitmapAlpha
+#include <boost/test/unit_test.hpp>
+
+
+
+
+#include <allegro_flare/gui/surface_areas/bitmap_alpha.h>
+
+#include <allegro_flare/gui/widget.h>
+#include <allegro5/allegro_image.h>
+#include <allegro_flare/framework.h>
+#include <fstream>
+
+
+
+
+#define TEST_FILENAME "data/bitmaps/elm_circle.png"
+
+
+
+
+struct Fixture
+{
+   ALLEGRO_BITMAP *bitmap;
+   Fixture()
+      : bitmap(NULL)
+   {
+      BOOST_REQUIRE_EQUAL(false, al_is_system_installed());
+      BOOST_REQUIRE_EQUAL(true, al_init());
+      BOOST_REQUIRE_EQUAL(true, al_init_image_addon());
+      bitmap = al_load_bitmap(TEST_FILENAME);
+   }
+   ~Fixture()
+   {
+      al_destroy_bitmap(bitmap);
+      al_uninstall_system();
+   }
+};
+
+
+
+
+BOOST_AUTO_TEST_CASE(necessairy_test_file_exists)
+{
+   std::ifstream ifile(TEST_FILENAME);
+   BOOST_CHECK_EQUAL(true, (bool)ifile);
+}
+
+
+
+
+BOOST_FIXTURE_TEST_CASE(does_not_collide_at_coordinates_outside_its_bounding_box, Fixture)
+{
+   UISurfaceAreaBitmapAlpha surface_area = UISurfaceAreaBitmapAlpha(0, 0, Fixture::bitmap);
+   surface_area.placement.align = vec2d(0, 0);
+
+   BOOST_CHECK_EQUAL(false, surface_area.collides(-1, -1));
+}
+
+
+
+
+BOOST_FIXTURE_TEST_CASE(does_not_collide_on_a_pixel_with_alpha_less_than_threshold, Fixture)
+{
+   UISurfaceAreaBitmapAlpha surface_area = UISurfaceAreaBitmapAlpha(0, 0, Fixture::bitmap);
+   surface_area.placement.align = vec2d(0, 0);
+
+   BOOST_CHECK_EQUAL(false, surface_area.collides(2, 2));
+}
+
+
+
+
+BOOST_FIXTURE_TEST_CASE(will_collide_on_a_pixel_with_alpha_greater_than_threshold, Fixture)
+{
+   UISurfaceAreaBitmapAlpha surface_area = UISurfaceAreaBitmapAlpha(0, 0, Fixture::bitmap);
+   surface_area.placement.align = vec2d(0, 0);
+
+   BOOST_CHECK_EQUAL(true, surface_area.collides(150, 150));
+}
+
+
+
+
