533ca68909e77ac4e63f5b9a70254eae36663319
Mark Oates <euph_boy@hotmail.com>
2016-09-11 21:51:17 -0400

    Add shadow mask example program

---
+++ examples/ex_shadow_mask.cpp
@@ -0,0 +1,53 @@
+
+
+
+
+#include <allegro_flare/allegro_flare.h>
+
+
+
+
+class ExShadowMask : public Screen
+{
+public:
+   ShadowMask shadow_mask;
+   vec2d mouse;
+
+   ExShadowMask(Display *display)
+      : Screen(display)
+      , shadow_mask(display->width(), display->height())
+      , mouse(200, 200)
+   {}
+   void primary_timer_func() override
+   {
+      al_draw_rectangle(200, 200, 340, 280, color::red, 6.0);
+
+      shadow_mask.clear_points();
+      shadow_mask.add_point(mouse.x, mouse.y);
+      shadow_mask.add_point(600, 300);
+      shadow_mask.add_point(200, 500);
+
+      shadow_mask.draw();
+   }
+   void mouse_axes_func() override
+   {
+      mouse.x = Framework::current_event->mouse.x;
+      mouse.y = Framework::current_event->mouse.y;
+   }
+};
+
+
+
+
+int main(int argc, char **argv)
+{
+   Framework::initialize();
+   Display *display = Framework::create_display();
+   ExShadowMask *proj = new ExShadowMask(display);
+   Framework::run_loop();
+   return 0;
+}
+
+
+
+
