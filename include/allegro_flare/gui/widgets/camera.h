6b2889daa1b48d0437da70beb8dcf1bb7ab9394d
Mark Oates <euph_boy@hotmail.com>
2016-11-25 19:59:57 -0500

    Add helpful comment in UICamera

--- include/allegro_flare/gui/widgets/camera.h
+++ include/allegro_flare/gui/widgets/camera.h
@@ -13,6 +13,7 @@ class UICamera : public UIWidget
 public:
    UICamera(UIWidget *parent, float x, float y, float w, float h);
 
+   // will simply pass message up through to the above parent
    void on_message(UIWidget *sender, std::string message) override;
 };
 
