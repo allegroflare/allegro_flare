4acd7ebd10dfdcef0793b74140a7587f4f76a404
Mark Oates <euph_boy@hotmail.com>
2017-01-01 00:37:46 -0500

    Add vec2b class

---
+++ include/allegro_flare/vec2b.h
@@ -0,0 +1,16 @@
+#pragma once
+
+
+
+
+class vec2b
+{
+public:
+   bool x, y;
+
+   vec2b(bool x=false, bool y=false);
+};
+
+
+
+
---
+++ source/vec2b.cpp
@@ -0,0 +1,15 @@
+
+
+
+#include <allegro_flare/vec2b.h>
+
+
+
+vec2b::vec2b(bool x, bool y)
+   : x(x)
+   , y(y)
+{
+}
+
+
+
