60e325cde42241c8a86be43dc43311c4a258f323
Mark Oates <euph_boy@hotmail.com>
2017-02-20 16:39:24 -0500

    Cleanup comments and style in Frustum

--- include/allegro_flare/frustum.h
+++ include/allegro_flare/frustum.h
@@ -16,5 +16,4 @@ public:
 
 
 
-
 #endif
--- src/graphics/frustum.cpp
+++ src/graphics/frustum.cpp
@@ -7,14 +7,13 @@
 
 
 
-//Frustum(int width=800, int height=600, int znear=500, float multiplier=0.01);
 Frustum::Frustum(int width, int height, int znear, float multiplier)
    : znear(znear * multiplier)
    , zfar(30000)
-   , top(height/2 * multiplier)   //+					//-    <-previously
-   , left(-width/2 * multiplier)   //-					//-
-   , right(width/2 * multiplier)    //+				//+
-   , bottom(-height/2 * multiplier)  //-				//+
+   , top(height/2 * multiplier)
+   , left(-width/2 * multiplier)
+   , right(width/2 * multiplier)
+   , bottom(-height/2 * multiplier)
 {}
 
 
