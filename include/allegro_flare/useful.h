158aa9a8b0ec6f27242e4690763462eb5139df39
Mark Oates <euph_boy@hotmail.com>
2017-08-20 15:54:19 -0400

    Add tests for random_string

--- include/allegro_flare/useful.h
+++ include/allegro_flare/useful.h
@@ -165,7 +165,6 @@ unsigned char random_letter_or_number();
 
 // Generates a string of the size length consisting of randomly selected characters [A-Za-z0-0].
 // This is useful for generating random strings for identifiers or unique instances.
-// TODO: aaaagggghhh ;[ this is not funny tested
 std::string random_string(unsigned int length);
 
 // Returns a randomly selected element from the vector.
