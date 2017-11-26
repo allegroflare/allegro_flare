4968301c2e21c0332e5e1f0e38913eb6675862fb
Mark Oates <euph_boy@hotmail.com>
2017-11-26 03:33:47 -0500

    Modify KeyboardCommandMapper to allow compound mappings

--- include/allegro_flare/keyboard_command_mapper.h
+++ include/allegro_flare/keyboard_command_mapper.h
@@ -5,20 +5,21 @@
 #include <map>
 #include <string>
 #include <tuple>
+#include <vector>
 
 
 
 class KeyboardCommandMapper
 {
 private:
-   std::map<std::tuple<int, bool, bool, bool>, std::string> mapping;
+   std::map<std::tuple<int, bool, bool, bool>, std::vector<std::string>> mapping;
 
 public:
    KeyboardCommandMapper();
    ~KeyboardCommandMapper();
 
-   bool set_mapping(int al_keycode, bool shift, bool ctrl, bool alt, std::string comand_identifier);
-   std::string get_mapping(int al_keycode, bool shift, bool ctrl, bool alt);
+   bool set_mapping(int al_keycode, bool shift, bool ctrl, bool alt, std::vector<std::string> comand_identifier);
+   std::vector<std::string> get_mapping(int al_keycode, bool shift, bool ctrl, bool alt);
 };
 
 
