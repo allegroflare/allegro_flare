51143be8ac44d36bc10ff8503db676ef8854844a
Mark Oates <euph_boy@hotmail.com>
2018-12-15 21:24:26 -0500

    Modify "dev_transition_fx" dev program

--- examples/dev_transition_fx.cpp
+++ examples/dev_transition_fx.cpp
@@ -110,8 +110,18 @@ void Base::update()
 #include <cmath>
 
 
-#define START_TRANSITION_USER_EVENT ALLEGRO_GET_EVENT_TYPE('x','s','f','d')
-#define FINISH_TRANSITION_USER_EVENT ALLEGRO_GET_EVENT_TYPE('!','x','f','d')
+#define START_TRANSITION_EVENT ALLEGRO_GET_EVENT_TYPE('x','s','f','d')
+#define FINISH_TRANSITION_EVENT ALLEGRO_GET_EVENT_TYPE('!','x','f','d')
+
+#define TRANSITION_TO_SCREEN ALLEGRO_GET_EVENT_TYPE('T','n','s','n')
+
+
+enum 
+{
+   TRANSITION_TO_SCREEN_A,
+   TRANSITION_TO_SCREEN_B,
+   TRANSITION_TO_SCREEN_C,
+};
 
 
 class ScreenA : public Screen
@@ -162,38 +172,69 @@ public:
 };
 
 
+class ScreenC : public Screen
+{
+private:
+   TextObject text;
+   ALLEGRO_COLOR background_color;
+
+public:
+   ScreenC(Display *display) : Screen(display), text("DONE"), background_color(color::green)
+   {
+      text.font(Framework::font("DroidSans.ttf 300")).color(color::white);
+   }
+   ~ScreenC() {}
+
+   void primary_timer_func() override
+   {
+      text.align(0.5, 0.5);
+      text.position(display->width()/2, display->height()/2);
+      text.rotation(sin(al_get_time())*-0.12);
+      al_clear_to_color(background_color);
+      text.draw();
+   }
+};
+
+
 class DevTransitionFXScreen : public Screen
 {
 private:
-   ScreenA screen_a;
-   ScreenB screen_b;
+   Screen *current_screen;
+   Screen *transition_to_screen;
    TransitionFX::Base *transition_fx;
-   //UserEventEmitter user_event_emitter;
+   TextObject num_screens_text;
 
 public:
    DevTransitionFXScreen(Display *display)
       : Screen(display)
-      , screen_a(display)
-      , screen_b(display)
+      , current_screen(new ScreenA(display))
+      , transition_to_screen(nullptr)
       , transition_fx(nullptr)
-      //, user_event_emitter()
+      , num_screens_text("0")
    {
-      UserEventEmitter::emit_event(START_TRANSITION_USER_EVENT);
+      UserEventEmitter::emit_event(START_TRANSITION_EVENT);
+      num_screens_text.position(display->width() - 100, display->height()-50)
+         .color(color::white)
+         .scale(1.6)
+         .font(Framework::font("DroidSans.ttf 30"));
    }
    ~DevTransitionFXScreen() {}
 
-   void start_transition()
+   void start_transition(Screen *new_screen)
    {
-      if (transition_fx) return;
+      transition_to_screen = new_screen;
       transition_fx = new TransitionFX::Base(al_get_backbuffer(display->al_display));
-      screen_a.create_and_use_backbuffer_sub_bitmap_of(transition_fx->get_surface_A());
-      screen_b.create_and_use_backbuffer_sub_bitmap_of(transition_fx->get_surface_B());
+      current_screen->create_and_use_backbuffer_sub_bitmap_of(transition_fx->get_surface_A());
+      transition_to_screen->create_and_use_backbuffer_sub_bitmap_of(transition_fx->get_surface_B());
    }
 
    void end_transition()
    {
       if (transition_fx)
       {
+         delete current_screen;
+         current_screen = transition_to_screen;
+         current_screen->create_and_use_backbuffer_sub_bitmap_of(al_get_backbuffer(display->al_display));
          delete transition_fx;
          transition_fx = nullptr;
       }
@@ -201,24 +242,53 @@ public:
 
    void key_down_func() override
    {
-      UserEventEmitter::emit_event(START_TRANSITION_USER_EVENT);
+      switch(Framework::current_event->keyboard.keycode)
+      {
+      case ALLEGRO_KEY_A:
+         if (transition_fx) break;
+         UserEventEmitter::emit_event(START_TRANSITION_EVENT, TRANSITION_TO_SCREEN_A);
+         break;
+      case ALLEGRO_KEY_B:
+         if (transition_fx) break;
+         UserEventEmitter::emit_event(START_TRANSITION_EVENT, TRANSITION_TO_SCREEN_B);
+         break;
+      case ALLEGRO_KEY_C:
+         if (transition_fx) break;
+         UserEventEmitter::emit_event(START_TRANSITION_EVENT, TRANSITION_TO_SCREEN_C);
+         break;
+      }
    }
 
    void user_event_func() override
    {
       switch(Framework::current_event->type)
       {
-      case START_TRANSITION_USER_EVENT:
-         std::cout << "starting crossfade...";
-         start_transition();
-         std::cout << "done" << std::endl;
-         break;
-      case FINISH_TRANSITION_USER_EVENT:
+      case START_TRANSITION_EVENT:
+         {
+            std::cout << "starting crossfade...";
+            Screen *new_screen = nullptr;
+            switch (Framework::current_event->user.data1)
+            {
+            case TRANSITION_TO_SCREEN_A:
+               new_screen = new ScreenA(display);
+               break;
+            case TRANSITION_TO_SCREEN_B:
+               new_screen = new ScreenB(display);
+               break;
+            case TRANSITION_TO_SCREEN_C:
+               new_screen = new ScreenC(display);
+               break;
+            default:
+               std::runtime_error("Unrecognized transition target");
+               break;
+            }
+            start_transition(new_screen);
+            std::cout << "done" << std::endl;
+            break;
+         }
+      case FINISH_TRANSITION_EVENT:
          std::cout << "finishing crossfade...";
-         screen_a.create_and_use_backbuffer_sub_bitmap_of(al_get_backbuffer(display->al_display));
-         screen_b.create_and_use_backbuffer_sub_bitmap_of(al_get_backbuffer(display->al_display));
-         delete transition_fx;
-         transition_fx = nullptr;
+         end_transition();
          std::cout << "done" << std::endl;
          break;
       default:
@@ -233,8 +303,13 @@ public:
          transition_fx->update();
          transition_fx->draw();
 
-         if (transition_fx->is_finished()) UserEventEmitter::emit_event(FINISH_TRANSITION_USER_EVENT);
+         if (transition_fx->is_finished()) UserEventEmitter::emit_event(FINISH_TRANSITION_EVENT);
       }
+
+      std::stringstream num_screens;
+      num_screens << Screen::get_num_screens();
+      num_screens_text.text(num_screens.str()).draw();
+      Screen::bring_to_front(this);
    }
 };
 
