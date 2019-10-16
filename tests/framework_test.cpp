fbd1cc34633940d3f336f0b4471d65179ed0d9fd
Antoine Viallon <antoine.viallon@gmail.com>
2019-10-17 01:48:17 +0200

    Fixed tests and Linux builds Cleaned up some mess

--- tests/framework_test.cpp
+++ tests/framework_test.cpp
@@ -1,7 +1,14 @@
 
-
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE Suites
+#include <boost/test/unit_test.hpp>
 
 // TODO
+
+BOOST_AUTO_TEST_CASE(pass)
+{
+    
+}
 
 // test that AllegroColorAttributeDatatype definition is included in Attributes
 
