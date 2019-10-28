1f67a1f6549f314161f0c0ee31146d4bc8c44e69
Mark Oates <markoates0@gmail.com>
2019-10-27 22:13:19 -0400

    Rename internal "*_partial"s to "*_fragment"s

--- src/AllegroFlare/TimerFormatter.cpp
+++ src/AllegroFlare/TimerFormatter.cpp
@@ -21,24 +21,24 @@ namespace AllegroFlare
    std::string TimerFormatter::format()
    {
       std::stringstream ss;
-      int hours_partial = milliseconds / 3600000;
-      int minutes_partial = (milliseconds / 60000) % 60;
-      int seconds_partial = (milliseconds / 1000) % 60;
-      int milliseconds_partial = milliseconds % 1000;
+      int hours_fragment = milliseconds / 3600000;
+      int minutes_fragment = (milliseconds / 60000) % 60;
+      int seconds_fragment = (milliseconds / 1000) % 60;
+      int milliseconds_fragment = milliseconds % 1000;
 
-      if (hours_partial > 0) ss << std::setw(2) << std::setfill('0') << hours_partial << ":";
+      if (hours_fragment > 0) ss << std::setw(2) << std::setfill('0') << hours_fragment << ":";
 
       ss
          << std::setw(2)
          << std::setfill('0')
-         << minutes_partial
+         << minutes_fragment
          << ":"
          << std::setw(2)
-         << seconds_partial
+         << seconds_fragment
          << '.'
          << std::setfill('0')
          << std::setw(3)
-         << milliseconds_partial;
+         << milliseconds_fragment;
 
       return ss.str();
    }
@@ -74,8 +74,8 @@ namespace AllegroFlare
    std::string TimerFormatter::infer_milliseconds_fragment()
    {
       std::stringstream ss;
-      int milliseconds_partial = milliseconds % 1000;
-      ss << std::setfill('0') << std::setw(3) << milliseconds_partial;
+      int milliseconds_fragment = milliseconds % 1000;
+      ss << std::setfill('0') << std::setw(3) << milliseconds_fragment;
       return ss.str();
    }
 }
