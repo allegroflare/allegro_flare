dae3aa3c197ea2935a8095d5c0d14cb36cc71a3f
Mark Oates <markoates0@gmail.com>
2019-10-27 21:32:32 -0400

    Fix placement of "Timer" in namespace "AllegroFlare"

--- include/AllegroFlare/Timer.hpp
+++ include/AllegroFlare/Timer.hpp
@@ -5,22 +5,25 @@
 #include <string>
 
 
-class Timer
+namespace AllegroFlare
 {
-private:
-   std::chrono::high_resolution_clock::time_point start_time;
-   std::chrono::high_resolution_clock::time_point end_time;
-   std::chrono::duration<double, std::milli> elappsed_time;
-   bool running;
+   class Timer
+   {
+   private:
+      std::chrono::high_resolution_clock::time_point start_time;
+      std::chrono::high_resolution_clock::time_point end_time;
+      std::chrono::duration<double, std::milli> elappsed_time;
+      bool running;
 
-public:
-   Timer();
-   ~Timer();
+   public:
+      Timer();
+      ~Timer();
 
-   void start();
-   void stop();
+      void start();
+      void stop();
 
-   int get_elappsed_time_msec();
-};
+      int get_elappsed_time_msec();
+   };
+}
 
 
--- src/AllegroFlare/Timer.cpp
+++ src/AllegroFlare/Timer.cpp
@@ -5,43 +5,46 @@
 #include <sstream>
 
 
-Timer::Timer()
-   : start_time()
-   , end_time()
-   , elappsed_time(0.0)
-   , running(false)
-{}
-
-
-Timer::~Timer()
+namespace AllegroFlare
 {
-}
+   Timer::Timer()
+      : start_time()
+      , end_time()
+      , elappsed_time(0.0)
+      , running(false)
+   {}
 
 
-void Timer::start()
-{
-   if (running) return;
-
-   elappsed_time += end_time - start_time;
-   start_time = std::chrono::high_resolution_clock::now();
-   running = true;
-}
+   Timer::~Timer()
+   {
+   }
 
 
-void Timer::stop()
-{
-   if (!running) return;
+   void Timer::start()
+   {
+      if (running) return;
 
-   end_time = std::chrono::high_resolution_clock::now();
-   elappsed_time += end_time - start_time;
-   running = false;
-}
+      elappsed_time += end_time - start_time;
+      start_time = std::chrono::high_resolution_clock::now();
+      running = true;
+   }
 
 
-int Timer::get_elappsed_time_msec()
-{
-   std::chrono::high_resolution_clock::time_point current_time = std::chrono::high_resolution_clock::now();
-   return std::chrono::duration_cast<std::chrono::milliseconds>(elappsed_time + (current_time - start_time)).count();
+   void Timer::stop()
+   {
+      if (!running) return;
+
+      end_time = std::chrono::high_resolution_clock::now();
+      elappsed_time += end_time - start_time;
+      running = false;
+   }
+
+
+   int Timer::get_elappsed_time_msec()
+   {
+      std::chrono::high_resolution_clock::time_point current_time = std::chrono::high_resolution_clock::now();
+      return std::chrono::duration_cast<std::chrono::milliseconds>(elappsed_time + (current_time - start_time)).count();
+   }
 }
 
 
