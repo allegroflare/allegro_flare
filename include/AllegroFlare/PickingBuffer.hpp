caaadd8b5ef01d8f9f90f4aab9b220b307437b86
Mark Oates <markoates0@gmail.com>
2019-12-15 12:03:06 -0500

    Duplicate PickingBuffer into AllegroFlare/; Test does not run though due to allegro testing/linking/build issue

---
+++ include/AllegroFlare/PickingBuffer.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+
+#include <allegro5/allegro.h>
+
+
+namespace AllegroFlare
+{
+   class PickingBuffer
+   {
+   private:
+      ALLEGRO_BITMAP *surface_render;
+      int w, h;
+
+      ALLEGRO_BITMAP *create_new_surface(int w, int h, int depth);
+      static int decode_id(ALLEGRO_COLOR color);
+      static ALLEGRO_COLOR encode_id(int id);
+
+   public:
+      static const int ID_MAX = 16777216;
+
+      PickingBuffer(int w, int h, int depth);
+      ~PickingBuffer();
+
+      void clear_surface();
+      ALLEGRO_BITMAP *get_surface_render();
+      int get_surface_width();
+      int get_surface_height();
+      int get_id(int x, int y);
+   };
+}
+
+
