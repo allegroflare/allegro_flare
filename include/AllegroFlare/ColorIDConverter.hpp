d1ab3ca58456ea8b5e4206609b137e74a750eba1
Mark Oates <markoates0@gmail.com>
2019-12-15 22:58:52 -0500

    Copy ColorIDConverter from allegro_flare -> AllegroFlare

---
+++ include/AllegroFlare/ColorIDConverter.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+
+
+#include <allegro5/allegro_color.h>
+
+
+
+namespace AllegroFlare
+{
+   class ColorIDConverter
+   {
+   public:
+      static const int ID_MAX = 16777216;
+
+      static int decode_id(ALLEGRO_COLOR color);
+      static ALLEGRO_COLOR encode_id(int id);
+   };
+}
+
+
+
