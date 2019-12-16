4975252553a6ab7a9ff221000b8dd2091f359a4d
Mark Oates <markoates0@gmail.com>
2019-12-15 19:02:08 -0500

    Remove test that will be unused

--- tests/AllegroFlare/ShaderTest.cpp
+++ tests/AllegroFlare/ShaderTest.cpp
@@ -93,18 +93,6 @@ TEST_F(AllegroFlare_ShaderTest, can_be_created_without_blowing_up)
 }
 
 
-TEST_F(AllegroFlare_ShaderTest, initialize__with_source_files_that_are_not_present_raises_an_exception)
-{
-   AllegroFlare::Shader shader(
-         "vertex_shader_file_that_does_not_exist.shader",
-         "fragment_shader_file_that_does_not_exist.shader"
-      );
-
-   std::string expected_error_message = "There was an error attaching the VERTEX shader source from file:\n\"vertex_shader_file_that_does_not_exist.shader\"\nFailed to open vertex_shader_file_that_does_not_exist.shader\n";
-   ASSERT_THROW_WITH_MESSAGE(shader.initialize(), std::runtime_error, expected_error_message);
-}
-
-
 TEST_F(AllegroFlare_ShaderTest, initialize__creates_the_shader)
 {
    std::string path = "/Users/markoates/Repos/allegro_flare/";
