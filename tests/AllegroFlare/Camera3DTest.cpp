49936f14e38d657e1c627c4787fd67bb86265b1c
Mark Oates <markoates0@gmail.com>
2019-12-17 17:56:49 -0500

    Fix failing test; Add test fixture for Camera3DTest

--- tests/AllegroFlare/Camera3DTest.cpp
+++ tests/AllegroFlare/Camera3DTest.cpp
@@ -16,9 +16,39 @@
 #include <AllegroFlare/Camera3D.hpp>
 
 
-TEST(AllegroFlare_Camera3DTest, can_be_created_without_blowing_up)
+class AllegroFlare_Camera3DTest : public ::testing::Test
 {
-   AllegroFlare::Camera3D camera;
+protected:
+   ALLEGRO_DISPLAY *display;
+
+   AllegroFlare_Camera3DTest()
+      : display(nullptr)
+   {
+   }
+
+   virtual void SetUp() override
+   {
+      ASSERT_EQ(false, al_is_system_installed());
+      ASSERT_EQ(true, al_init());
+
+      al_set_new_display_flags(ALLEGRO_OPENGL | ALLEGRO_PROGRAMMABLE_PIPELINE);
+      ASSERT_EQ(ALLEGRO_OPENGL, al_get_new_display_flags() & ALLEGRO_OPENGL);
+      ASSERT_EQ(ALLEGRO_PROGRAMMABLE_PIPELINE, al_get_new_display_flags() & ALLEGRO_PROGRAMMABLE_PIPELINE);
+      ALLEGRO_DISPLAY *display = al_create_display(800, 600);
+      ASSERT_NE(nullptr, display);
+   }
+
+   virtual void TearDown() override
+   {
+      if (display) al_destroy_display(display);
+      al_uninstall_system();
+   }
+};
+
+
+TEST_F(AllegroFlare_Camera3DTest, can_be_created_without_blowing_up)
+{
+   //AllegroFlare::Camera3D camera;
 }
 
 
