6c50ea947708e6a9967edefda9b16aeec13922fc
Mark Oates <markoates0@gmail.com>
2019-12-17 21:34:47 -0500

    Add some camera test

--- tests/AllegroFlare/Camera3DTest.cpp
+++ tests/AllegroFlare/Camera3DTest.cpp
@@ -15,8 +15,20 @@
 #include <allegro5/allegro.h>
 #include <AllegroFlare/Camera3D.hpp>
 #include <AllegroFlare/Model3D.hpp>
+#include <AllegroFlare/Useful.hpp> // for TAU
 #include <allegro5/allegro_image.h>
 #include <allegro5/allegro_color.h>
+#include <cmath> // for std::abs
+
+
+bool basically_equal(const ALLEGRO_COLOR &col1, const ALLEGRO_COLOR &col2, float threshold=0.0001f)
+{
+   if (std::abs(col1.a - col2.a) > threshold) return false;
+   if (std::abs(col1.g - col2.g) > threshold) return false;
+   if (std::abs(col1.b - col2.b) > threshold) return false;
+   if (std::abs(col1.r - col2.r) > threshold) return false;
+   return true;
+}
 
 
 class AllegroFlare_Camera3DTest : public ::testing::Test
@@ -45,6 +57,21 @@ protected:
    {
       if (display) al_destroy_display(display);
       al_uninstall_system();
+   }
+
+   void draw_targets()
+   {
+      float x = 0;
+      float y = 0;
+      al_draw_filled_rectangle(-0.1+x, -0.1+y, 0.1+x, 0.1+y, al_color_name("white"));
+
+      x = 5;
+      y = 0;
+      al_draw_filled_rectangle(-0.1+x, -0.1+y, 0.1+x, 0.1+y, al_color_name("yellow"));
+
+      x = -5;
+      y = 0;
+      al_draw_filled_rectangle(-0.1+x, -0.1+y, 0.1+x, 0.1+y, al_color_name("orange"));
    }
 };
 
@@ -104,29 +131,66 @@ TEST_F(AllegroFlare_Camera3DTest, with_stepback__renders_the_expected_output)
 
 TEST_F(AllegroFlare_Camera3DTest, with_stepback__renders_the_expected_output_2)
 {
-   AllegroFlare::vec3d position(0, 0, -4);
+   AllegroFlare::vec3d position(0, 0, -5);
    AllegroFlare::vec3d view_vector(0, 0, 1);
    AllegroFlare::vec3d up_vector(0, 1, 0);
+   float stepback_rotation = 0.0;
+   float stepback_pitch = 0.0;
 
-   AllegroFlare::Camera3D camera(position, view_vector, up_vector);
-   camera.clear_screen_and_start_transform_on_current_bitmap();
+   ALLEGRO_DISPLAY *display = al_get_current_display();
+   ALLEGRO_BITMAP *rendering_surface = al_get_backbuffer(display);
+   ASSERT_NE(nullptr, rendering_surface);
+
+   ALLEGRO_BITMAP *backbuffer_sub_bitmap = al_create_sub_bitmap(al_get_backbuffer(display), 0, 0, al_get_display_width(display), al_get_display_height(display));
+   ASSERT_NE(nullptr, backbuffer_sub_bitmap);
+
+   float screen_center_x = al_get_bitmap_width(rendering_surface)/2;
+   float screen_center_y = al_get_bitmap_height(rendering_surface)/2;
+
+   int total_passes = 260;
+   for (int i=0; i<total_passes; i++)
+   {
+      float pass = (float) i / total_passes;
+      AllegroFlare::Camera3D camera(position, view_vector, up_vector);
+      //stepback_rotation += 0.01;
+      //stepback_pitch += 0.01;
+      camera.stepback_rotation.y = pass * AllegroFlare::TAU / 8;//stepback_rotation;
+      camera.stepback_pitch = 0;///stepback_pitch;
+      camera.clear_screen_and_start_transform_on_current_bitmap();
+
+      draw_targets();
+
+      al_set_target_bitmap(backbuffer_sub_bitmap);
+      al_draw_rectangle(screen_center_x - 10, screen_center_y - 5, screen_center_x + 10, screen_center_y + 5, al_color_name("yellow"), 3.0);
+
+      al_flip_display();
+   }
+
+   ALLEGRO_COLOR expected_color = al_color_name("orange");
+   ALLEGRO_COLOR actual_color = al_get_pixel(rendering_surface, screen_center_x, screen_center_y);
+   ASSERT_EQ(true, basically_equal(expected_color, actual_color));
+
+
+   for (int i=0; i<total_passes; i++)
+   {
+      float pass = (float) i / total_passes;
+      AllegroFlare::Camera3D camera(position, view_vector, up_vector);
+      //stepback_rotation += 0.01;
+      //stepback_pitch += 0.01;
+      camera.stepback_rotation.y = pass * AllegroFlare::TAU / 8;//stepback_rotation;
+      camera.stepback_pitch = 0;///stepback_pitch;
+      camera.clear_screen_and_start_transform_on_current_bitmap();
+
+      draw_targets();
+
+      al_flip_display();
+   }
 
    ALLEGRO_DISPLAY *current_display = al_get_current_display();
-
    al_init_image_addon();
    al_save_bitmap("tmp/hello_world.png", al_get_backbuffer(current_display));
 
-   float x, y = 0;
-   al_draw_filled_rectangle(-0.1+x, -0.1+y, 0.1+x, 0.1+y, al_color_name("white"));
-
-   x = 1; y = 1;
-   al_draw_filled_rectangle(-0.1+x, -0.1+y, 0.1+x, 0.1+y, al_color_name("yellow"));
-
-   x = -2; y = -1;
-   al_draw_filled_rectangle(-0.1+x, -0.1+y, 0.1+x, 0.1+y, al_color_name("orange"));
-
-   al_flip_display();
-   sleep(3);
+   sleep(1);
 
    SUCCEED();
 }
