a03e80528a974f93a0e92e7f51e431fad82a71fb
Mark Oates <markoates0@gmail.com>
2020-05-02 11:51:35 -0400

    Convert AllegroFlare/Useful test to gtest

--- tests/AllegroFlare/UsefulTest.cpp
+++ tests/AllegroFlare/UsefulTest.cpp
@@ -1,39 +1,25 @@
 
 
-//#define BOOST_TEST_DYN_LINK
-//#define BOOST_TEST_MODULE RANDOM_STRING
-//#include <boost/test/unit_test.hpp>
+#include <AllegroFlare/Useful.hpp>
 
 
+#include <gtest/gtest.h>
 
 
-//#include <AllegroFlare/Useful.hpp>
+TEST(UsefulTest, random_string__only_returns_strings_with_the_expected_characters_and_length)
+{
+   std::string allowed_characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
+   std::vector<std::string> errors;
 
+   for (int i=0; i<30; i++)
+   {
+      int length = AllegroFlare::random_int(1, 100);
+      std::string val = AllegroFlare::random_string(length);
 
-
-//using namespace AllegroFlare;
-
-
-
-
-//BOOST_AUTO_TEST_CASE(only_returns_strings_with_the_expected_characters_and_length)
-//{
-   //std::string allowed_characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-
-   //std::vector<std::string> errors;
-
-   //for (int i=0; i<30; i++)
-   //{
-      //int length = random_int(1, 100);
-      //std::string val = random_string(length);
-
-      //BOOST_CHECK_EQUAL(length, val.length());
-      //BOOST_CHECK_EQUAL(std::string::npos, val.find_first_not_of(allowed_characters));
-
-   //}
-//}
-
-
+      ASSERT_EQ(length, val.length());
+      ASSERT_EQ(std::string::npos, val.find_first_not_of(allowed_characters));
+   }
+}
 
 
