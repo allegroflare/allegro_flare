c73b118efccd1f4e7de89a79d8a8a6e40dbbe76d
Mark Oates <markoates0@gmail.com>
2020-05-02 11:58:15 -0400

    Remove implicit namespace, use explicit in AllegroFlare/Random test

--- tests/AllegroFlare/RandomTest.cpp
+++ tests/AllegroFlare/RandomTest.cpp
@@ -5,29 +5,26 @@
 #include <AllegroFlare/Random.hpp>
 
 
-using namespace AllegroFlare;
-
-
-TEST(RandomTest, gets_the_current_seed)
+TEST(AllegroFlare_RandomTest, gets_the_current_seed)
 {
-   Random number_generator = Random(123);
+   AllegroFlare::Random number_generator = AllegroFlare::Random(123);
 
    ASSERT_EQ(123, number_generator.get_seed());
 }
 
 
-TEST(RandomTest, sets_the_current_seed)
+TEST(AllegroFlare_RandomTest, sets_the_current_seed)
 {
-   Random number_generator = Random(123);
+   AllegroFlare::Random number_generator = AllegroFlare::Random(123);
    number_generator.set_seed(456);
 
    ASSERT_EQ(456, number_generator.get_seed());
 }
 
 
-TEST(RandomTest, only_returns_integers_within_the_bounds_inclusive)
+TEST(AllegroFlare_RandomTest, only_returns_integers_within_the_bounds_inclusive)
 {
-   Random number_generator = Random(123);
+   AllegroFlare::Random number_generator = AllegroFlare::Random(123);
    const int min_random_num = 1;
    const int max_random_num = 3;
 
@@ -41,9 +38,9 @@ TEST(RandomTest, only_returns_integers_within_the_bounds_inclusive)
 }
 
 
-TEST(RandomTest, only_returns_doubles_within_the_bounds_inclusive)
+TEST(AllegroFlare_RandomTest, only_returns_doubles_within_the_bounds_inclusive)
 {
-   Random number_generator = Random(123);
+   AllegroFlare::Random number_generator = AllegroFlare::Random(123);
    const double min_random_num = 1.0;
    const double max_random_num = 3.0;
 
@@ -57,9 +54,9 @@ TEST(RandomTest, only_returns_doubles_within_the_bounds_inclusive)
 }
 
 
-TEST(RandomTest, only_returns_floats_within_the_bounds_inclusive)
+TEST(AllegroFlare_RandomTest, only_returns_floats_within_the_bounds_inclusive)
 {
-   Random number_generator = Random(123);
+   AllegroFlare::Random number_generator = AllegroFlare::Random(123);
    const float min_random_num = 1.0;
    const float max_random_num = 3.0;
 
@@ -75,7 +72,7 @@ TEST(RandomTest, only_returns_floats_within_the_bounds_inclusive)
 
 TEST(RandomTest, returns_an_expected_sequence_of_random_numbers_given_a_seed)
 {
-   Random number_generator = Random(123456);
+   AllegroFlare::Random number_generator = AllegroFlare::Random(123456);
    const int min_random_num = 0;
    const int max_random_num = 10;
    std::vector<int> expected_numbers = {1, 10, 2, 1, 8};
