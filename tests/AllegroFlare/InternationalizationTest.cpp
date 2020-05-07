6b83ee27570328103bfb8071689b0af8ec10ad73
Mark Oates <markoates0@gmail.com>
2020-05-07 00:18:42 -0400

    Uncomment Internationalization tests

--- tests/AllegroFlare/InternationalizationTest.cpp
+++ tests/AllegroFlare/InternationalizationTest.cpp
@@ -48,217 +48,217 @@ TEST_F(AllegroFlare_InternationalizationTest, expected_test_files_and_folders_ex
 
 
 
-//TEST_F(AllegroFlare_InternationalizationTest, can_set_a_languages_folder)
-//{
-   //ASSERT_EQ(TEST_FOLDER, internationalization.get_languages_folder());
-//}
+TEST_F(AllegroFlare_InternationalizationTest, can_set_a_languages_folder)
+{
+   ASSERT_EQ(TEST_FOLDER, internationalization.get_languages_folder());
+}
 
 
 
 
-//TEST_F(AllegroFlare_InternationalizationTest, initialized_its_languages_folder_to__data_slash_languages__by_default)
-//{
-   //AllegroFlare::Internationalization internationalization;
-   //ASSERT_EQ("data/languages/", internationalization.get_languages_folder());
-//}
+TEST_F(AllegroFlare_InternationalizationTest, initialized_its_languages_folder_to__data_slash_languages__by_default)
+{
+   AllegroFlare::Internationalization internationalization;
+   ASSERT_EQ("data/languages/", internationalization.get_languages_folder());
+}
 
 
 
 
-//TEST_F(AllegroFlare_InternationalizationTest, returns_false_when_trying_to_set_a_languages_folder_that_does_not_exist)
-//{
-   //std::string fake_folder = "foo/dir/that/doesnt/exist/";
-   //ASSERT_EQ(false, internationalization.set_languages_folder(fake_folder));
-//}
+TEST_F(AllegroFlare_InternationalizationTest, returns_false_when_trying_to_set_a_languages_folder_that_does_not_exist)
+{
+   std::string fake_folder = "foo/dir/that/doesnt/exist/";
+   ASSERT_EQ(false, internationalization.set_languages_folder(fake_folder));
+}
 
 
 
-//TEST_F(AllegroFlare_InternationalizationTest,
-   //set_languages_folder__when_trying_to_set_a_languages_folder_that_does_not_exist__outputs_an_error_message_to_cout)
-//{
-   //std::string fake_folder = "foo/dir/that/doesnt/exist/";
-   //ASSERT_EQ(false, internationalization.set_languages_folder(fake_folder));
+TEST_F(AllegroFlare_InternationalizationTest,
+   set_languages_folder__when_trying_to_set_a_languages_folder_that_does_not_exist__outputs_an_error_message_to_cout)
+{
+   std::string fake_folder = "foo/dir/that/doesnt/exist/";
+   ASSERT_EQ(false, internationalization.set_languages_folder(fake_folder));
 
-   //testing::internal::CaptureStdout();
-   //ASSERT_EQ(false, internationalization.set_languages_folder(fake_folder));
-   //std::string output = testing::internal::GetCapturedStdout();
-   //std::string expected_cerr_output = "\x1B[1;31mCould not open folder \"foo/dir/that/doesnt/exist/\" " \
-                                      //"to find language files.\x1B[0m\n";
-   //ASSERT_EQ(expected_cerr_output, output);
-//}
+   testing::internal::CaptureStdout();
+   ASSERT_EQ(false, internationalization.set_languages_folder(fake_folder));
+   std::string output = testing::internal::GetCapturedStdout();
+   std::string expected_cerr_output = "\x1B[1;31mCould not open folder \"foo/dir/that/doesnt/exist/\" " \
+                                      "to find language files.\x1B[0m\n";
+   ASSERT_EQ(expected_cerr_output, output);
+}
 
 
 
-//TEST_F(AllegroFlare_InternationalizationTest, returns_a_list_of_language_files_in_the_languages_folder)
-//{
-   //std::vector<std::string> expected_files = {"en.txt", "fr.txt", "it.txt"};
-   //std::vector<std::string> returned_files = internationalization.get_language_filenames();
+TEST_F(AllegroFlare_InternationalizationTest, returns_a_list_of_language_files_in_the_languages_folder)
+{
+   std::vector<std::string> expected_files = {"en.txt", "fr.txt", "it.txt"};
+   std::vector<std::string> returned_files = internationalization.get_language_filenames();
 
-   //ASSERT_THAT(returned_files, ::testing::UnorderedElementsAreArray(expected_files.begin(), expected_files.end()));
-//}
+   ASSERT_THAT(returned_files, ::testing::UnorderedElementsAreArray(expected_files.begin(), expected_files.end()));
+}
 
 
 
 
-//TEST_F(AllegroFlare_InternationalizationTest, finds_a_language_file_by_language_designator)
-//{
-   //std::string expected_filename = "";
+TEST_F(AllegroFlare_InternationalizationTest, finds_a_language_file_by_language_designator)
+{
+   std::string expected_filename = "";
 
-   //expected_filename = "./data/languages/en.txt";
-   //ASSERT_EQ(expected_filename, internationalization.find_language_file("en"));
+   expected_filename = "./data/languages/en.txt";
+   ASSERT_EQ(expected_filename, internationalization.find_language_file("en"));
 
-   //expected_filename = "./data/languages/fr.txt";
-   //ASSERT_EQ(expected_filename, internationalization.find_language_file("fr"));
+   expected_filename = "./data/languages/fr.txt";
+   ASSERT_EQ(expected_filename, internationalization.find_language_file("fr"));
 
-   //expected_filename = "./data/languages/it.txt";
-   //ASSERT_EQ(expected_filename, internationalization.find_language_file("it"));
-//}
+   expected_filename = "./data/languages/it.txt";
+   ASSERT_EQ(expected_filename, internationalization.find_language_file("it"));
+}
 
 
 
 
-//TEST_F(AllegroFlare_InternationalizationTest, returns_an_empty_string_when_a_language_file_cannot_be_found)
-//{
-   //ASSERT_EQ("", internationalization.find_language_file("language_designator_that_doesnt_exist"));
-//}
+TEST_F(AllegroFlare_InternationalizationTest, returns_an_empty_string_when_a_language_file_cannot_be_found)
+{
+   ASSERT_EQ("", internationalization.find_language_file("language_designator_that_doesnt_exist"));
+}
 
 
 
 
-//TEST_F(AllegroFlare_InternationalizationTest, returns_true_when_a_language_file_loads_successfully)
-//{
-   //ASSERT_EQ(true, internationalization.load_language_file("en", "English", "./data/languages/en.txt"));
-   //ASSERT_EQ(true, internationalization.load_language_file("fr", "French", "./data/languages/fr.txt"));
-   //ASSERT_EQ(true, internationalization.load_language_file("it", "Italian", "./data/languages/it.txt"));
-//}
+TEST_F(AllegroFlare_InternationalizationTest, returns_true_when_a_language_file_loads_successfully)
+{
+   ASSERT_EQ(true, internationalization.load_language_file("en", "English", "./data/languages/en.txt"));
+   ASSERT_EQ(true, internationalization.load_language_file("fr", "French", "./data/languages/fr.txt"));
+   ASSERT_EQ(true, internationalization.load_language_file("it", "Italian", "./data/languages/it.txt"));
+}
 
 
 
 
-//TEST_F(AllegroFlare_InternationalizationTest, sets_the_language_designator_and_language_name_when_loading_a_language_file)
-//{
-   //internationalization.load_language_file("en", "English", "./data/languages/en.txt");
-   //ASSERT_EQ("en", internationalization.get_language());
-   //ASSERT_EQ("English", internationalization.get_language_name());
+TEST_F(AllegroFlare_InternationalizationTest, sets_the_language_designator_and_language_name_when_loading_a_language_file)
+{
+   internationalization.load_language_file("en", "English", "./data/languages/en.txt");
+   ASSERT_EQ("en", internationalization.get_language());
+   ASSERT_EQ("English", internationalization.get_language_name());
 
-   //internationalization.load_language_file("fr", "French", "./data/languages/fr.txt");
-   //ASSERT_EQ("fr", internationalization.get_language());
-   //ASSERT_EQ("French", internationalization.get_language_name());
+   internationalization.load_language_file("fr", "French", "./data/languages/fr.txt");
+   ASSERT_EQ("fr", internationalization.get_language());
+   ASSERT_EQ("French", internationalization.get_language_name());
 
-   //internationalization.load_language_file("it", "Italian", "./data/languages/it.txt");
-   //ASSERT_EQ("it", internationalization.get_language());
-   //ASSERT_EQ("Italian", internationalization.get_language_name());
-//}
+   internationalization.load_language_file("it", "Italian", "./data/languages/it.txt");
+   ASSERT_EQ("it", internationalization.get_language());
+   ASSERT_EQ("Italian", internationalization.get_language_name());
+}
 
 
 
 
-//TEST_F(AllegroFlare_InternationalizationTest, returns_false_when_unable_to_load_a_language_file)
-//{
-   //ASSERT_EQ(false, internationalization.load_language_file("foo", "Fooanese", "foo/folder/file_that_doesnt_exist.txt"));
-//}
+TEST_F(AllegroFlare_InternationalizationTest, returns_false_when_unable_to_load_a_language_file)
+{
+   ASSERT_EQ(false, internationalization.load_language_file("foo", "Fooanese", "foo/folder/file_that_doesnt_exist.txt"));
+}
 
 
 
 
-//TEST_F(AllegroFlare_InternationalizationTest, successfully_populates_labels_when_loading_a_language_file)
-//{
-   //EXPECT_EQ(true, internationalization.load_language_file("en", "English", "./data/languages/en.txt"));
+TEST_F(AllegroFlare_InternationalizationTest, successfully_populates_labels_when_loading_a_language_file)
+{
+   EXPECT_EQ(true, internationalization.load_language_file("en", "English", "./data/languages/en.txt"));
 
-   //ASSERT_EQ("Welcome to Allegro!", internationalization.t("welcome_message"));
-   //ASSERT_EQ("start", internationalization.t("start_button"));
-//}
+   ASSERT_EQ("Welcome to Allegro!", internationalization.t("welcome_message"));
+   ASSERT_EQ("start", internationalization.t("start_button"));
+}
 
 
 
 
-//TEST_F(AllegroFlare_InternationalizationTest, can_set_a_language)
-//{
-   //ASSERT_EQ(true, internationalization.set_language("en"));
-   //ASSERT_EQ(true, internationalization.set_language("fr"));
-   //ASSERT_EQ(true, internationalization.set_language("it"));
-//}
+TEST_F(AllegroFlare_InternationalizationTest, can_set_a_language)
+{
+   ASSERT_EQ(true, internationalization.set_language("en"));
+   ASSERT_EQ(true, internationalization.set_language("fr"));
+   ASSERT_EQ(true, internationalization.set_language("it"));
+}
 
 
 
 
-//TEST_F(AllegroFlare_InternationalizationTest, returns_false_when_unable_to_set_a_language)
-//{
-   //ASSERT_EQ(false, internationalization.set_language("not_a_language"));
-//}
+TEST_F(AllegroFlare_InternationalizationTest, returns_false_when_unable_to_set_a_language)
+{
+   ASSERT_EQ(false, internationalization.set_language("not_a_language"));
+}
 
 
 
 
-//TEST_F(AllegroFlare_InternationalizationTest, can_retrieve_the_current_language_designator)
-//{
-   //internationalization.set_language("en");
-   //ASSERT_EQ("en", internationalization.get_language());
+TEST_F(AllegroFlare_InternationalizationTest, can_retrieve_the_current_language_designator)
+{
+   internationalization.set_language("en");
+   ASSERT_EQ("en", internationalization.get_language());
 
-   //internationalization.set_language("fr");
-   //ASSERT_EQ("fr", internationalization.get_language());
+   internationalization.set_language("fr");
+   ASSERT_EQ("fr", internationalization.get_language());
 
-   //internationalization.set_language("it");
-   //ASSERT_EQ("it", internationalization.get_language());
-//}
+   internationalization.set_language("it");
+   ASSERT_EQ("it", internationalization.get_language());
+}
 
 
 
 
-//TEST_F(AllegroFlare_InternationalizationTest, loads_translations_when_a_language_designator_is_set)
-//{
-   //internationalization.set_language("en");
-   //ASSERT_EQ("Welcome to Allegro!", internationalization.t("welcome_message"));
-   //ASSERT_EQ("start", internationalization.t("start_button"));
+TEST_F(AllegroFlare_InternationalizationTest, loads_translations_when_a_language_designator_is_set)
+{
+   internationalization.set_language("en");
+   ASSERT_EQ("Welcome to Allegro!", internationalization.t("welcome_message"));
+   ASSERT_EQ("start", internationalization.t("start_button"));
 
-   //internationalization.set_language("fr");
-   //ASSERT_EQ("Bienvenue à Allegro!", internationalization.t("welcome_message"));
-   //ASSERT_EQ("démarrer", internationalization.t("start_button"));
+   internationalization.set_language("fr");
+   ASSERT_EQ("Bienvenue à Allegro!", internationalization.t("welcome_message"));
+   ASSERT_EQ("démarrer", internationalization.t("start_button"));
 
-   //internationalization.set_language("it");
-   //ASSERT_EQ("Benvenuti a Allegro!", internationalization.t("welcome_message"));
-   //ASSERT_EQ("inizio", internationalization.t("start_button"));
-//}
+   internationalization.set_language("it");
+   ASSERT_EQ("Benvenuti a Allegro!", internationalization.t("welcome_message"));
+   ASSERT_EQ("inizio", internationalization.t("start_button"));
+}
 
 
 
 
-//TEST_F(AllegroFlare_InternationalizationTest, knows_if_a_translation_label_exists_or_not)
-//{
-   //internationalization.set_language("en");
+TEST_F(AllegroFlare_InternationalizationTest, knows_if_a_translation_label_exists_or_not)
+{
+   internationalization.set_language("en");
 
-   //ASSERT_EQ(true, internationalization.t_exists("blank_content"));
-   //ASSERT_EQ(false, internationalization.t_exists("label_that_does_not_exist"));
-//}
+   ASSERT_EQ(true, internationalization.t_exists("blank_content"));
+   ASSERT_EQ(false, internationalization.t_exists("label_that_does_not_exist"));
+}
 
 
 
 
-//TEST_F(AllegroFlare_InternationalizationTest, returns_an_empty_string_when_a_translation_is_blank_or_does_not_exist_for_that_label)
-//{
-   //internationalization.set_language("en");
+TEST_F(AllegroFlare_InternationalizationTest, returns_an_empty_string_when_a_translation_is_blank_or_does_not_exist_for_that_label)
+{
+   internationalization.set_language("en");
 
-   //ASSERT_EQ("", internationalization.t("blank_content"));
-   //ASSERT_EQ("", internationalization.t("label_that_does_not_exist"));
-//}
+   ASSERT_EQ("", internationalization.t("blank_content"));
+   ASSERT_EQ("", internationalization.t("label_that_does_not_exist"));
+}
 
 
 
 
-//TEST_F(AllegroFlare_InternationalizationTest, formats_a_translation_with_variable_args)
-//{
-   //internationalization.set_language("en");
-   //ASSERT_EQ("Travel time is 6 hours and 45 minutes.", internationalization.tf("travel_time", 6, 45));
-   //ASSERT_EQ("Nice to meet you, Alex.", internationalization.tf("greeting", "Alex"));
+TEST_F(AllegroFlare_InternationalizationTest, formats_a_translation_with_variable_args)
+{
+   internationalization.set_language("en");
+   ASSERT_EQ("Travel time is 6 hours and 45 minutes.", internationalization.tf("travel_time", 6, 45));
+   ASSERT_EQ("Nice to meet you, Alex.", internationalization.tf("greeting", "Alex"));
 
-   //internationalization.set_language("fr");
-   //ASSERT_EQ("Voyage de temps est de 6 heures et 45 minutes.", internationalization.tf("travel_time", 6, 45));
-   //ASSERT_EQ("Ravi de vous rencontrer, Alex.", internationalization.tf("greeting", "Alex"));
+   internationalization.set_language("fr");
+   ASSERT_EQ("Voyage de temps est de 6 heures et 45 minutes.", internationalization.tf("travel_time", 6, 45));
+   ASSERT_EQ("Ravi de vous rencontrer, Alex.", internationalization.tf("greeting", "Alex"));
 
-   //internationalization.set_language("it");
-   //ASSERT_EQ("Il tempo di percorrenza è di 6 ore e 45 minuti.", internationalization.tf("travel_time", 6, 45));
-   //ASSERT_EQ("Piacere di conoscerti, Alex.", internationalization.tf("greeting", "Alex"));
-//}
+   internationalization.set_language("it");
+   ASSERT_EQ("Il tempo di percorrenza è di 6 ore e 45 minuti.", internationalization.tf("travel_time", 6, 45));
+   ASSERT_EQ("Piacere di conoscerti, Alex.", internationalization.tf("greeting", "Alex"));
+}
 
 
 
