c6cb0cc1e69271bfba17b9ae4dd3228af7596f96
Mark Oates <markoates0@gmail.com>
2020-05-16 11:29:54 -0400

    * Add first test for AllegroFlare/Color

--- tests/AllegroFlare/ColorTest.cpp
+++ tests/AllegroFlare/ColorTest.cpp
@@ -5,8 +5,13 @@
 #include <AllegroFlare/Color.hpp>
 
 
-TEST(AllegroFlare_ColorTest, necessairy_test_file_exists)
+TEST(AllegroFlare_ColorTest, null_color__contains_the_expected_values)
 {
+   ALLEGRO_COLOR color = AllegroFlare::color::null_color;
+   EXPECT_EQ(0, color.r);
+   EXPECT_EQ(0, color.g);
+   EXPECT_EQ(0, color.b);
+   EXPECT_EQ(1.0, color.a);
 }
 
 
