1ba0120642132fa36ce4e79bbd9ff23d5c79032f
Mark Oates <euph_boy@hotmail.com>
2020-07-28 14:05:36 -0400

    Add test file for Network/NetworkService

---
+++ tests/AllegroFlare/Network/NetworkServiceTest.cpp
@@ -0,0 +1,9 @@
+#include <gtest/gtest.h>
+
+#include <AllegroFlare/Network/NetworkService.hpp>
+
+TEST(NetworkServiceTest, can_be_created_without_blowing_up)
+{
+   NetworkService network_service;
+}
+
