8cc7a000bd60f0902c24bb79021b2a441859f669
Mark Oates <markoates0@gmail.com>
2021-12-16 17:32:30 -0500

    Remove commented code from AudioController test

--- tests/AllegroFlare/AudioControllerTest.cpp
+++ tests/AllegroFlare/AudioControllerTest.cpp
@@ -11,6 +11,5 @@ TEST(AllegroFlare_AudioControllerTest, can_be_created_without_blowing_up)
 TEST(AllegroFlare_AudioControllerTest, run__returns_the_expected_response)
 {
    AllegroFlare::AudioController audio_controller;
-   //std::string expected_string = "Hello World!";
-   //EXPECT_EQ(expected_string, audio_controller.run());
 }
+
