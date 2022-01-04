d1984b2f549e0822bc03552db16b01085b7a5061
Mark Oates <markoates0@gmail.com>
2022-01-03 19:39:24 -0500

    * Add scaffold for MD5

---
+++ include/AllegroFlare/MD5.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+
+#include <string>
+
+
+namespace AllegroFlare
+{
+   class MD5
+   {
+   private:
+
+   public:
+      MD5();
+      ~MD5();
+
+      std::string run();
+   };
+}
+
+
+
---
+++ src/AllegroFlare/MD5.cpp
@@ -0,0 +1,27 @@
+
+
+#include <AllegroFlare/MD5.hpp>
+
+
+
+namespace AllegroFlare
+{
+
+
+MD5::MD5()
+{
+}
+
+
+MD5::~MD5()
+{
+}
+
+
+std::string MD5::run()
+{
+   return "Hello World!";
+}
+} // namespace AllegroFlare
+
+
