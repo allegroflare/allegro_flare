5ea7f3497229ef78f254b6ccd7d4351f711b95eb
Mark Oates <markoates0@gmail.com>
2022-03-05 08:30:55 -0500

    * Add PrintTo test for ALLEGRO_COLOR

--- include/AllegroFlare/Testing/CustomComparison.hpp
+++ include/AllegroFlare/Testing/CustomComparison.hpp
@@ -2,11 +2,15 @@
 
 
 #include <AllegroFlare/Vec2D.hpp>
+#include <allegro5/allegro_color.h>
 #include <ostream>
 
+
+bool operator==(const ALLEGRO_COLOR& color, const ALLEGRO_COLOR& other_color);
+void PrintTo(const ALLEGRO_COLOR& color, ::std::ostream* os);
+
+
 bool operator==(const AllegroFlare::vec2d& vec, const AllegroFlare::vec2d& other_vec);
-
-
 namespace AllegroFlare
 {
    void PrintTo(const AllegroFlare::vec2d& vec, ::std::ostream* os);
--- src/AllegroFlare/Testing/CustomComparison.cpp
+++ src/AllegroFlare/Testing/CustomComparison.cpp
@@ -1,6 +1,21 @@
 
 #include <AllegroFlare/Testing/CustomComparison.hpp>
 
+
+void PrintTo(const ALLEGRO_COLOR& color, ::std::ostream* os)
+{
+  *os << "ALLEGRO_COLOR{" << color.r << ", " << color.g << ", " << color.b << ", " << color.a << "}";
+}
+
+bool operator==(const ALLEGRO_COLOR& color, const ALLEGRO_COLOR& other_color)
+{
+   if (color.r != other_color.r) return false;
+   if (color.g != other_color.g) return false;
+   if (color.b != other_color.b) return false;
+   if (color.a != other_color.a) return false;
+   return true;
+}
+
 bool operator==(const AllegroFlare::vec2d& vec, const AllegroFlare::vec2d& other_vec)
 {
    if (vec.x != other_vec.x) return false;
