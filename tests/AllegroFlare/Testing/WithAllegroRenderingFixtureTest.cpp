4b4212c2c568da4dda2d82ac57585b3611abf983
Mark Oates <markoates0@gmail.com>
2022-03-05 09:05:40 -0500

    * Add tests for #get_test_name# and #get_test_suite_name#

--- tests/AllegroFlare/Testing/WithAllegroRenderingFixtureTest.cpp
+++ tests/AllegroFlare/Testing/WithAllegroRenderingFixtureTest.cpp
@@ -3,8 +3,24 @@
 
 #include <AllegroFlare/Testing/WithAllegroRenderingFixture.hpp>
 
-TEST(AllegroFlare_Testing_WithAllegroRenderingFixtureTest, can_be_created_without_blowing_up)
+
+class TestClassFor_AllegroFlare_Testing_WithAllegroRenderingFixture
+   : public AllegroFlare::Testing::WithAllegroRenderingFixture{};
+
+
+TEST_F(TestClassFor_AllegroFlare_Testing_WithAllegroRenderingFixture,
+   get_test_name__will_return_the_name_of_the_test)
 {
-   //AllegroFlare::Testing::WithAllegroRenderingFixture with_allegro_rendering_fixture;
+   std::string expected_test_name = "get_test_name__will_return_the_name_of_the_test";
+   EXPECT_EQ(expected_test_name, get_test_name());
 }
+
+
+TEST_F(TestClassFor_AllegroFlare_Testing_WithAllegroRenderingFixture,
+   get_test_suite_name__will_return_the_name_of_the_test)
+{
+   std::string expected_test_name = "TestClassFor_AllegroFlare_Testing_WithAllegroRenderingFixture";
+   EXPECT_EQ(expected_test_name, get_test_suite_name());
+}
+
 
