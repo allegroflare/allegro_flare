1bd03f949a99fc3ee517d46969a45381df8584e5
Mark Oates <markoates0@gmail.com>
2022-05-24 10:32:30 -0400

    * Copy BitFlags from allegro_flare->AllegroFlare

---
+++ tests/AllegroFlare/BitFlagsTest.cpp
@@ -0,0 +1,16 @@
+
+#include <gtest/gtest.h>
+
+#define ASSERT_THROW_WITH_MESSAGE(code, raised_exception_type, raised_exception_message) \
+   try { code; FAIL() << "Expected " # raised_exception_type; } \
+   catch ( raised_exception_type const &err ) { EXPECT_EQ(err.what(), std::string( raised_exception_message )); } \
+   catch (...) { FAIL() << "Expected " # raised_exception_type; }
+
+#include <AllegroFlare/BitFlags.hpp>
+
+
+TEST(AllegroFlare_BitFlagsTest, can_be_crated_without_blowing_up)
+{
+   AllegroFlare::BitFlags<long> bit_flags;
+}
+
