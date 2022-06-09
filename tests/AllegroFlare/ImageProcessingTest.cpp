8c955d94a27d8706111a400bcc3c9ed39e95e0be
Mark Oates <markoates0@gmail.com>
2022-06-09 14:44:56 -0400

    * Add ImageProcessing port from allegro_flare

---
+++ tests/AllegroFlare/ImageProcessingTest.cpp
@@ -0,0 +1,12 @@
+
+#include <gtest/gtest.h>
+
+#include <AllegroFlare/ImageProcessing.hpp>
+
+
+TEST(AllegroFlare_ImageProcessingTest, DISABLED__can_be_created_without_blowing_up)
+{
+   //AllegroFlare::ImageProcessing image_processing;
+}
+
+
