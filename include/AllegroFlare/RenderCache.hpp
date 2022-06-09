bec7cb42529240bc6bd89c8885a0ef8f2ab86683
Mark Oates <markoates0@gmail.com>
2022-06-09 15:27:58 -0400

    * Add port from allegro_flare of RenderCache

---
+++ include/AllegroFlare/RenderCache.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#define ALLEGRO_UNSTABLE
+
+#include <allegro5/allegro.h>
+
+
+
+
+namespace AllegroFlare
+{
+   class RenderCache
+   {
+   private:
+      ALLEGRO_BITMAP *render;
+      int padding;
+      ALLEGRO_STATE previous_state;
+      int previous_samples; // should not be necessairy in future versions of Allegro
+      int previous_depth;
+      bool dirty;
+
+   public:
+      RenderCache();
+      ~RenderCache();
+
+      void setup_surface(int w, int h, int multisamples=0, int depth=0);
+      void finish_surface();
+      void draw(float origin_x, float origin_y);
+      void mark_as_dirty();
+      void mark_as_clean();
+      bool is_dirty();
+
+      ALLEGRO_BITMAP *get_surface_bitmap();
+   };
+}
+
+
+
---
+++ tests/AllegroFlare/RenderCacheTest.cpp
@@ -0,0 +1,12 @@
+
+#include <gtest/gtest.h>
+
+#include <AllegroFlare/RenderCache.hpp>
+
+
+TEST(AllegroFlare_RenderCacheTest, can_be_created_without_blowing_up)
+{
+   AllegroFlare::RenderCache render_cache;
+}
+
+
