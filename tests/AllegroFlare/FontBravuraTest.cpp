8dd0d7d31852f409bc82afa276185ae78bd41bc8
Mark Oates <markoates0@gmail.com>
2022-06-13 22:41:40 -0400

    * Remove excess test

--- tests/AllegroFlare/FontBravuraTest.cpp
+++ tests/AllegroFlare/FontBravuraTest.cpp
@@ -10,10 +10,3 @@ TEST(AllegroFlare_FontBravuraTest, can_be_created_without_blowing_up)
 }
 
 
-TEST(AllegroFlare_FontBravuraTest, run__returns_the_expected_response)
-{
-   AllegroFlare::FontBravura font_bravura;
-   std::string expected_string = "Hello World!";
-   EXPECT_EQ(expected_string, font_bravura.run());
-}
-
