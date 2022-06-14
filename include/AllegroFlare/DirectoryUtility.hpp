8fc984265e10ee437fc934f799566c6c00980404
Mark Oates <markoates0@gmail.com>
2022-06-14 17:44:55 -0400

    * Add AllegroFlare/DirectoryUtility

---
+++ include/AllegroFlare/DirectoryUtility.hpp
@@ -0,0 +1,90 @@
+#pragma once
+
+
+
+
+// wishlist
+// have a std::vector<std::string, std::string> with special path locations
+// project_path
+// program_path
+// system_path
+// user_path
+// etc...
+
+// don't use a singleton
+
+// implement a rescan
+
+// swap out the functionality of find() and find_from() as appropriate
+
+// make all functions operate with a FIND_LATEST flag or something
+
+
+
+
+#include <string>
+#include <vector>
+#include <allegro5/allegro.h>
+
+
+
+
+namespace AllegroFlare
+{
+   class DirectoryUtility
+   {
+   private:
+      //private:
+      //static PathObject *instance;
+
+      void _recursive_traverse_dir(ALLEGRO_FS_ENTRY *folder);
+      int _bfind(std::string filename); // find with a binary search.  This will find *only* by the raw filename, and returns the index.
+
+      //PathObject *get_instance();
+      ALLEGRO_PATH *root;
+      std::vector<ALLEGRO_PATH *> path;
+      bool sorted;
+
+   public:
+      DirectoryUtility(std::string root_directory="[directory-not-set]");
+      ~DirectoryUtility();
+
+      const char *find(std::string filename);
+      //TODO:: THIS FUNCTION find_all_versions WAS NEVER TESTED:
+      std::vector<std::string> find_all_versions(std::string filename_pattern); // e.g. find_all_versions("this_is_my_filename - ##.txt");
+      const char *bfind(std::string filename);
+      const char *find_latest(std::string filename);	// e.g. find_latest("this_is_my_filename - ##.txt");
+      const char *bfind_latest(std::string filename_pattern); // under construction
+      void str_out();
+      void rescan(); // untested
+      int get_num_items();
+      ALLEGRO_PATH *get_item(int index_num); // assumed read only, mkay? ;)
+      std::string get_listing();
+
+      // static functions
+
+      std::string get_exe_path();
+      std::string find_from(std::string absolute_base_folder, std::string filename_to_find); // does not use an instance, only realtime recursive search.  Creates and destroys the whole file list each time.  //TODO make this more ideal
+   };
+
+
+
+
+   // find_latest("this_is_my_filename - ##.txt");
+   // A convention I use frequently is to have the filename followed
+   // by a space-dash-space " - " and then the two digit version
+   // number.  You may see several files like this:
+   // "this_is_my_filename - 01.txt"
+   // "this_is_my_filename - 02.txt"
+   // "this_is_my_filename - 03.txt"
+   // all of them being the same file but the later numbered versions
+   // are revisions of previous versions.
+   // The function find_latest("this_is_my_filename - ##.txt") will
+   // return the file matching the pattern with the largest
+   // two-digit number.  This only works with two digits.
+   // if no file is found, then "" is returned.
+}
+
+
+
+
---
+++ tests/AllegroFlare/DirectoryUtilityTest.cpp
@@ -0,0 +1,12 @@
+
+#include <gtest/gtest.h>
+
+#include <AllegroFlare/DirectoryUtility.hpp>
+
+
+TEST(AllegroFlare_DirectoryUtilityTest, can_be_created_without_blowing_up)
+{
+   AllegroFlare::DirectoryUtility directory_utility;
+}
+
+
