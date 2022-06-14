497d861027f34055517385dff372fd9234bd7235
Mark Oates <markoates0@gmail.com>
2022-06-14 10:47:01 -0400

    * Add JSONLoaders/Allegro with ALLEGRO_COLOR json loading functions

---
+++ include/AllegroFlare/JSONLoaders/Allegro.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+
+#include <lib/nlohmann/json.hpp>
+#include <allegro5/allegro.h>
+
+
+
+// JSON Loaders for ALLEGRO_COLOR
+void to_json(nlohmann::json& j, const ALLEGRO_COLOR& color);
+void from_json(const nlohmann::json& j, ALLEGRO_COLOR& color);
+
+
+
---
+++ tests/AllegroFlare/JSONLoaders/AllegroTest.cpp
@@ -0,0 +1,6 @@
+
+#include <gtest/gtest.h>
+
+#include <AllegroFlare/JSONLoaders/Allegro.hpp>
+
+
