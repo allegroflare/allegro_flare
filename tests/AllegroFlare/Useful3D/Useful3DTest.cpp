94e836d50248787a3bb4d13026547f6851565096
Mark Oates <markoates0@gmail.com>
2022-06-13 20:36:32 -0400

    * Add test file for Useful3D/Useful3D

---
+++ tests/AllegroFlare/Useful3D/Useful3DTest.cpp
@@ -0,0 +1,15 @@
+
+#include <gtest/gtest.h>
+
+#include <AllegroFlare/Useful3D/Useful3D.hpp>
+
+
+TEST(AllegroFlare_Useful3D_Useful3D, DISABLED__can_be_created_without_blowing_up)
+{
+   // AllegroFlare::Useful3D::Useful3D:: is a namespace
+
+   //AllegroFlare::Useful3D::Useful3D useful3d;
+}
+
+
+
