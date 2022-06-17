4549428193b6c9a4160711214c7d035356a40ce7
Mark Oates <markoates0@gmail.com>
2022-06-17 12:12:17 -0400

    Remvoe test for missing function

--- tests/AllegroFlare/Screens/BasicTest.cpp
+++ tests/AllegroFlare/Screens/BasicTest.cpp
@@ -10,10 +10,3 @@ TEST(AllegroFlare_Screens_BasicTest, can_be_created_without_blowing_up)
 }
 
 
-TEST(AllegroFlare_Screens_BasicTest, run__returns_the_expected_response)
-{
-   AllegroFlare::Screens::Basic basic;
-   std::string expected_string = "Hello World!";
-   EXPECT_EQ(expected_string, basic.run());
-}
-
