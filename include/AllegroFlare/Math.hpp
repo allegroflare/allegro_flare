251636946be0ba66d3094fd3209b6ff9de7cf236
Mark Oates <markoates0@gmail.com>
2022-06-25 13:05:12 -0400

    * Add degrees_to_radians and radians_to_degrees

--- include/AllegroFlare/Math.hpp
+++ include/AllegroFlare/Math.hpp
@@ -14,8 +14,15 @@ namespace AllegroFlare
       Math();
       ~Math();
 
-      static const float FULL_ROTATION;
+      static const float PI;
       static const float TAU;
+      static const float FULL_ROTATION;
+
+      // Converts a value from degrees to radians.
+      static float degrees_to_radians(float deg);
+
+      // Converts a value from radians to degrees.
+      static float radians_to_degrees(float rad);
    };
 }
 
--- src/AllegroFlare/Math.cpp
+++ src/AllegroFlare/Math.cpp
@@ -18,10 +18,27 @@ Math::~Math()
 }
 
 
-const float Math::FULL_ROTATION = 6.28318531;
+const float Math::PI = 3.14159265358979323846;
 
 
-const float Math::TAU = 6.28318531;
+const float Math::TAU = PI * 2;
+
+
+const float Math::FULL_ROTATION = TAU;
+
+
+float Math::degrees_to_radians(float deg)
+{
+   return PI * deg / 180.0f;
+}
+
+
+float Math::radians_to_degrees(float radians)
+{
+    return 180.0f / PI * radians;
+}
+
+
 
 
 } // namespace AllegroFlare
