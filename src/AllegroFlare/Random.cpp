83421d792355607e566a423c37af6118d5602b0c
Mark Oates <markoates0@gmail.com>
2022-06-30 17:31:07 -0400

    * Modify Random to repeatablly reproduce outputs when mixing different method calls

--- src/AllegroFlare/Random.cpp
+++ src/AllegroFlare/Random.cpp
@@ -4,21 +4,111 @@
 #include <AllegroFlare/Color.hpp>
 
 
+static unsigned int basic_multiply_hash(std::string value)
+{
+   unsigned int result = 1;
+   for (int i=0; i<value.size(); i++)
+   {
+      result *= (unsigned int)value[i];
+   }
+   return result;
+}
+
+
 namespace AllegroFlare
 {
    Random::Random(unsigned int seed)
       : primary_seed(seed)
+      , random_number_generator__for_get_random_int()
+      , random_number_generator__for_get_random_bool()
+      , random_number_generator__for_get_random_sign()
+      , random_number_generator__for_get_random_float()
+      , random_number_generator__for_get_random_double()
+      , random_number_generator__for_get_one_in_chance()
+      , random_number_generator__for_get_random_letter()
+      , random_number_generator__for_get_random_letter_or_number()
+      , random_number_generator__for_get_random_string()
+      , random_number_generator__for_roll_dice()
+      , random_number_generator__for_get_random_element()
+      , random_number_generator__for_shuffle_elements()
+      , random_number_generator__for_get_random_color()
+      , random_number_generator__for_get_random_color_exhaustive()
    {
       set_seed(primary_seed);
    }
 
 
+   int Random::extract_random_int(int min, int max, std::mt19937 &rng)
+   {
+      int fixed_min = std::min(min, max);
+      int fixed_max = std::max(min, max);
+      int range = fixed_max - fixed_min + 1;
+      return rng() % range + fixed_min;
+   }
+
+
+   unsigned char Random::extract_random_letter(bool lowercase, std::mt19937 &rng)
+   {
+      int num = extract_random_int(0, 26, rng);
+      if (lowercase) return (unsigned char)(num%26 + 'a');
+      return (unsigned char)(num%26 + 'A');
+   }
+
+
+   unsigned char Random::extract_random_letter_or_number(std::mt19937 &rng)
+   {
+      int num = extract_random_int(0, 10+26+26, rng); // 10 digits, 26 uppercase, 26 lowercase
+      if (num <= 10) return (unsigned char)(num%10 + '0');
+      else if ((num-10) <= 26) return (unsigned char)(num%26 + 'A');
+      else if ((num-10-26) <= 26) return (unsigned char)(num%26 + 'a');
+      return '-';
+   }
 
 
    void Random::set_seed(unsigned int new_seed)
    {
       primary_seed = new_seed;
-      random_number_generator.seed(primary_seed);
+
+      // set the individual method seeds
+      random_number_generator__for_get_random_int.seed(
+            primary_seed + basic_multiply_hash("for_get_random_int")
+         );
+      random_number_generator__for_get_random_bool.seed(
+            primary_seed + basic_multiply_hash("for_get_random_bool")
+         );
+      random_number_generator__for_get_random_sign.seed(
+            primary_seed + basic_multiply_hash("for_get_random_sign")
+         );
+      random_number_generator__for_get_random_float.seed(
+            primary_seed + basic_multiply_hash("for_get_random_float")
+         );
+      random_number_generator__for_get_random_double.seed(
+            primary_seed + basic_multiply_hash("for_get_random_double")
+         );
+      random_number_generator__for_get_one_in_chance.seed(
+            primary_seed + basic_multiply_hash("for_get_one_in_chance")
+         );
+      random_number_generator__for_get_random_letter.seed(
+            primary_seed + basic_multiply_hash("for_get_random_letter")
+         );
+      random_number_generator__for_get_random_letter_or_number.seed(
+            primary_seed + basic_multiply_hash("for_get_random_letter_or_number")
+         );
+      random_number_generator__for_get_random_string.seed(
+            primary_seed + basic_multiply_hash("for_get_random_string")
+         );
+      random_number_generator__for_get_random_element.seed(
+            primary_seed + basic_multiply_hash("for_get_random_element")
+         );
+      random_number_generator__for_shuffle_elements.seed(
+            primary_seed + basic_multiply_hash("for_shuffle_elements")
+         );
+      std::mt19937 random_number_generator__for_get_random_color(
+            primary_seed + basic_multiply_hash("for_get_random_color")
+         );
+      std::mt19937 random_number_generator__for_get_random_color_exhaustive(
+            primary_seed + basic_multiply_hash("for_get_random_color_exhaustive")
+         );
    }
 
 
@@ -34,7 +124,7 @@ namespace AllegroFlare
 
    bool Random::get_random_bool()
    {
-      return random_number_generator() % 2;
+      return random_number_generator__for_get_random_bool() % 2;
    }
 
 
@@ -42,7 +132,7 @@ namespace AllegroFlare
 
    int Random::get_random_sign()
    {
-      return (random_number_generator() % 2) ? 1 : -1;
+      return (random_number_generator__for_get_random_sign() % 2) ? 1 : -1;
    }
 
 
@@ -50,10 +140,12 @@ namespace AllegroFlare
 
    int Random::get_random_int(int min, int max)
    {
+      std::mt19937 &rng = random_number_generator__for_get_random_int;
+      //std::mt19937 &rng = random_number_generator;
       int fixed_min = std::min(min, max);
       int fixed_max = std::max(min, max);
       int range = fixed_max - fixed_min + 1;
-      return random_number_generator() % range + fixed_min;
+      return rng() % range + fixed_min;
    }
 
 
@@ -61,11 +153,11 @@ namespace AllegroFlare
 
    float Random::get_random_float(float min, float max)
    {
+      std::mt19937 &rng = random_number_generator__for_get_random_float;
       float fixed_min = std::min(min, max);
       float fixed_max = std::max(min, max);
       float range = fixed_max - fixed_min;
-      
-      return (random_number_generator() / (float)random_number_generator.max()) * range + fixed_min;
+      return (rng() / (float)rng.max()) * range + fixed_min;
    }
 
 
@@ -73,11 +165,11 @@ namespace AllegroFlare
 
    double Random::get_random_double(double min, double max)
    {
+      std::mt19937 &rng = random_number_generator__for_get_random_double;
       double fixed_min = std::min(min, max);
       double fixed_max = std::max(min, max);
       double range = fixed_max - fixed_min;
-      
-      return (random_number_generator() / (double)random_number_generator.max()) * range + fixed_min;
+      return (rng() / (double)rng.max()) * range + fixed_min;
    }
 
 
@@ -85,7 +177,8 @@ namespace AllegroFlare
 
    unsigned char Random::get_random_letter(bool lowercase)
    {
-      int num = get_random_int(0, 26);
+      std::mt19937 &rng = random_number_generator__for_get_random_letter;
+      int num = extract_random_int(0, 26, rng);
       if (lowercase) return (unsigned char)(num%26 + 'a');
       return (unsigned char)(num%26 + 'A');
    }
@@ -95,7 +188,8 @@ namespace AllegroFlare
 
    unsigned char Random::get_random_letter_or_number()
    {
-      int num = get_random_int(0, 10+26+26); // 10 digits, 26 uppercase, 26 lowercase
+      std::mt19937 &rng = random_number_generator__for_get_random_letter_or_number;
+      int num = extract_random_int(0, 10+26+26, rng); // 10 digits, 26 uppercase, 26 lowercase
       if (num <= 10) return (unsigned char)(num%10 + '0');
       else if ((num-10) <= 26) return (unsigned char)(num%26 + 'A');
       else if ((num-10-26) <= 26) return (unsigned char)(num%26 + 'a');
@@ -107,9 +201,10 @@ namespace AllegroFlare
 
    std::string Random::get_random_string(unsigned int length)
    {
+      std::mt19937 &rng = random_number_generator__for_get_random_string;
       std::string return_str;
       for (unsigned i=0; i<length; i++)
-         return_str += get_random_letter_or_number();
+         return_str += extract_random_letter_or_number(rng);
       return return_str;
    }
 
@@ -118,7 +213,8 @@ namespace AllegroFlare
 
    bool Random::get_one_in_chance(int chance)
    {
-      if (get_random_int(0, chance - 1) == 0)
+      std::mt19937 &rng = random_number_generator__for_get_one_in_chance;
+      if (extract_random_int(0, chance - 1, rng) == 0)
          return true;
       return false;
    }
@@ -128,9 +224,10 @@ namespace AllegroFlare
 
    int Random::roll_dice(int sides, int number_of_die)
    {
+      std::mt19937 &rng = random_number_generator__for_roll_dice;
       int result = 0;
       for (int i = 0; i < number_of_die; i++)
-         result += get_random_int(sides, 1);
+         result += extract_random_int(sides, 1, rng);
       return result;
    }
 
@@ -284,7 +381,8 @@ namespace AllegroFlare
          AllegroFlare::Color::YellowGreen,
       };
 
-      return possible_colors[get_random_int(0, possible_colors.size())];
+      std::mt19937 &rng = random_number_generator__for_get_random_color;
+      return possible_colors[extract_random_int(0, possible_colors.size(), rng)];
    }
 
 
@@ -437,10 +535,12 @@ namespace AllegroFlare
          AllegroFlare::Color::YellowGreen,
       };
 
+      std::mt19937 &rng = random_number_generator__for_get_random_color_exhaustive;
+
       static int next = colors.size();
       if (next >= colors.size())
       {
-         std::shuffle(colors.begin(), colors.end(), random_number_generator);
+         std::shuffle(colors.begin(), colors.end(), rng);
          next = 0;
       }
       else
