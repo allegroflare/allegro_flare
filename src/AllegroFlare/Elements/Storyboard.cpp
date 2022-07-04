a67d75fb02476ac8da47cff0e2f0c741fd4f64ea
Mark Oates <markoates0@gmail.com>
2022-07-04 16:12:02 -0400

    Add Elements/Storyboard

---
+++ src/AllegroFlare/Elements/Storyboard.cpp
@@ -0,0 +1,256 @@
+
+
+#include <AllegroFlare/Elements/Storyboard.hpp>
+#include <allegro5/allegro_font.h>
+#include <AllegroFlare/Color.hpp>
+#include <stdexcept>
+#include <sstream>
+#include <stdexcept>
+#include <sstream>
+#include <stdexcept>
+#include <sstream>
+
+
+namespace AllegroFlare
+{
+namespace Elements
+{
+
+
+Storyboard::Storyboard(AllegroFlare::FontBin* font_bin, AllegroFlare::EventEmitter* event_emitter, std::vector<std::string> pages, std::string screen_identifier_to_switch_to_after_completing, std::string font_name, int font_size, ALLEGRO_COLOR text_color, float top_padding, float left_padding, float right_padding, float line_height_multiplier, float line_height_padding, intptr_t current_page_num)
+   : AllegroFlare::Screens::Base()
+   , font_bin(font_bin)
+   , event_emitter(event_emitter)
+   , pages(pages)
+   , screen_identifier_to_switch_to_after_completing(screen_identifier_to_switch_to_after_completing)
+   , font_name(font_name)
+   , font_size(font_size)
+   , text_color(text_color)
+   , top_padding(top_padding)
+   , left_padding(left_padding)
+   , right_padding(right_padding)
+   , line_height_multiplier(line_height_multiplier)
+   , line_height_padding(line_height_padding)
+   , current_page_num(current_page_num)
+   , can_go_to_next_page(true)
+{
+}
+
+
+Storyboard::~Storyboard()
+{
+}
+
+
+void Storyboard::set_font_bin(AllegroFlare::FontBin* font_bin)
+{
+   this->font_bin = font_bin;
+}
+
+
+void Storyboard::set_event_emitter(AllegroFlare::EventEmitter* event_emitter)
+{
+   this->event_emitter = event_emitter;
+}
+
+
+void Storyboard::set_pages(std::vector<std::string> pages)
+{
+   this->pages = pages;
+}
+
+
+void Storyboard::set_screen_identifier_to_switch_to_after_completing(std::string screen_identifier_to_switch_to_after_completing)
+{
+   this->screen_identifier_to_switch_to_after_completing = screen_identifier_to_switch_to_after_completing;
+}
+
+
+void Storyboard::set_font_name(std::string font_name)
+{
+   this->font_name = font_name;
+}
+
+
+void Storyboard::set_font_size(int font_size)
+{
+   this->font_size = font_size;
+}
+
+
+void Storyboard::set_text_color(ALLEGRO_COLOR text_color)
+{
+   this->text_color = text_color;
+}
+
+
+void Storyboard::set_top_padding(float top_padding)
+{
+   this->top_padding = top_padding;
+}
+
+
+void Storyboard::set_left_padding(float left_padding)
+{
+   this->left_padding = left_padding;
+}
+
+
+void Storyboard::set_right_padding(float right_padding)
+{
+   this->right_padding = right_padding;
+}
+
+
+void Storyboard::set_line_height_multiplier(float line_height_multiplier)
+{
+   this->line_height_multiplier = line_height_multiplier;
+}
+
+
+void Storyboard::set_line_height_padding(float line_height_padding)
+{
+   this->line_height_padding = line_height_padding;
+}
+
+
+std::string Storyboard::get_font_name()
+{
+   return font_name;
+}
+
+
+int Storyboard::get_font_size()
+{
+   return font_size;
+}
+
+
+ALLEGRO_COLOR Storyboard::get_text_color()
+{
+   return text_color;
+}
+
+
+float Storyboard::get_top_padding()
+{
+   return top_padding;
+}
+
+
+float Storyboard::get_left_padding()
+{
+   return left_padding;
+}
+
+
+float Storyboard::get_right_padding()
+{
+   return right_padding;
+}
+
+
+float Storyboard::get_line_height_multiplier()
+{
+   return line_height_multiplier;
+}
+
+
+float Storyboard::get_line_height_padding()
+{
+   return line_height_padding;
+}
+
+
+intptr_t Storyboard::get_current_page_num()
+{
+   return current_page_num;
+}
+
+
+void Storyboard::render()
+{
+   if (!(al_is_system_installed()))
+      {
+         std::stringstream error_message;
+         error_message << "Storyboard" << "::" << "render" << ": error: " << "guard \"al_is_system_installed()\" not met";
+         throw std::runtime_error(error_message.str());
+      }
+   if (!(al_is_font_addon_initialized()))
+      {
+         std::stringstream error_message;
+         error_message << "Storyboard" << "::" << "render" << ": error: " << "guard \"al_is_font_addon_initialized()\" not met";
+         throw std::runtime_error(error_message.str());
+      }
+   ALLEGRO_FONT *text_font = obtain_font();
+
+   al_clear_to_color(AllegroFlare::Color::Black);
+
+   if (current_page_num >= pages.size()) return;
+
+   float box_width = 1920 - (left_padding + right_padding);
+
+   al_draw_multiline_text(
+         text_font,
+         text_color, //AllegroFlare::Color(0xd0f2c5).to_al(),
+         left_padding, //x_padding,
+         top_padding, //y_padding,
+         box_width,
+         al_get_font_line_height(text_font)*line_height_multiplier + line_height_padding,
+         0,
+         pages[current_page_num].c_str()
+      );
+   return;
+}
+
+void Storyboard::reset()
+{
+   current_page_num = 0;
+   return;
+}
+
+void Storyboard::on_activate()
+{
+   reset();
+   return;
+}
+
+void Storyboard::primary_timer_func()
+{
+   render();
+   return;
+}
+
+void Storyboard::key_down_func(ALLEGRO_EVENT* event)
+{
+   if (!(event_emitter))
+      {
+         std::stringstream error_message;
+         error_message << "Storyboard" << "::" << "key_down_func" << ": error: " << "guard \"event_emitter\" not met";
+         throw std::runtime_error(error_message.str());
+      }
+   current_page_num++;
+   can_go_to_next_page = true;
+
+   if (current_page_num >= pages.size())
+      event_emitter->emit_switch_screen_event(screen_identifier_to_switch_to_after_completing);
+
+   return;
+}
+
+ALLEGRO_FONT* Storyboard::obtain_font()
+{
+   if (!(font_bin))
+      {
+         std::stringstream error_message;
+         error_message << "Storyboard" << "::" << "obtain_font" << ": error: " << "guard \"font_bin\" not met";
+         throw std::runtime_error(error_message.str());
+      }
+   std::stringstream composite_font_str;
+   composite_font_str << font_name << " " << font_size;
+   return font_bin->auto_get(composite_font_str.str());
+}
+} // namespace Elements
+} // namespace AllegroFlare
+
+
---
+++ tests/AllegroFlare/Elements/StoryboardTest.cpp
@@ -0,0 +1,144 @@
+
+#include <gtest/gtest.h>
+
+#define EXPECT_THROW_WITH_MESSAGE(code, raised_exception_type, expected_exception_message) \
+   try { code; FAIL() << "Expected " # raised_exception_type; } \
+   catch ( raised_exception_type const &err ) { EXPECT_EQ(std::string(expected_exception_message), err.what()); } \
+   catch (...) { FAIL() << "Expected " # raised_exception_type; }
+
+#include <AllegroFlare/Testing/WithAllegroRenderingFixture.hpp>
+
+#include <AllegroFlare/EventNames.hpp>
+
+
+class AllegroFlare_Elements_StoryboardTest : public ::testing::Test
+{};
+
+class AllegroFlare_Elements_StoryboardTestWithAllegroRenderingFixture
+   : public AllegroFlare::Testing::WithAllegroRenderingFixture
+{};
+
+
+#include <AllegroFlare/Elements/Storyboard.hpp>
+
+
+TEST_F(AllegroFlare_Elements_StoryboardTest, can_be_created_without_blowing_up)
+{
+   AllegroFlare::Elements::Storyboard storyboard;
+}
+
+
+TEST_F(AllegroFlare_Elements_StoryboardTest, render__without_allegro_initialized__raises_an_error)
+{
+   AllegroFlare::Elements::Storyboard storyboard;
+   std::string expected_error_message =
+      "Storyboard::render: error: guard \"al_is_system_installed()\" not met";
+   EXPECT_THROW_WITH_MESSAGE(storyboard.render(), std::runtime_error, expected_error_message);
+}
+
+
+TEST_F(AllegroFlare_Elements_StoryboardTestWithAllegroRenderingFixture, render__will_not_blow_up)
+{
+   AllegroFlare::Elements::Storyboard storyboard(&get_font_bin_ref());
+   storyboard.render();
+   SUCCEED();
+}
+
+
+TEST_F(AllegroFlare_Elements_StoryboardTestWithAllegroRenderingFixture,
+   render__will_draw_the_current_page_text_to_the_screen)
+{
+   std::vector<std::string> pages = { "Hello Storyboard!" };
+   AllegroFlare::Elements::Storyboard storyboard(&get_font_bin_ref(), nullptr, pages);
+
+   storyboard.render();
+   al_flip_display();
+
+   //sleep_for(1);
+
+   SUCCEED();
+}
+
+
+TEST_F(AllegroFlare_Elements_StoryboardTestWithAllegroRenderingFixture,
+   current_page_num__is_initialized_with_a_value_of_zero)
+{
+   AllegroFlare::Elements::Storyboard storyboard;
+   EXPECT_EQ(0, storyboard.get_current_page_num());
+}
+
+
+TEST_F(AllegroFlare_Elements_StoryboardTestWithAllegroRenderingFixture,
+   key_down_func__will_advance_to_the_next_page)
+{
+   AllegroFlare::EventEmitter event_emitter;
+   std::vector<std::string> pages = {
+      "This is page 1.",
+      "The second page looks like this.",
+      "A final page is this one, indeed.",
+   };
+   AllegroFlare::Elements::Storyboard storyboard(&get_font_bin_ref(), &event_emitter, pages);
+
+   storyboard.render();
+   al_flip_display();
+
+   EXPECT_EQ(0, storyboard.get_current_page_num()); // TODO: this line should be a separate test
+   for (int i=0; i<(pages.size()-1); i++)
+   {
+      storyboard.key_down_func();
+      int expected_page_num = i+1;
+      EXPECT_EQ(expected_page_num, storyboard.get_current_page_num());
+   }
+
+   SUCCEED();
+}
+
+
+TEST_F(AllegroFlare_Elements_StoryboardTestWithAllegroRenderingFixture,
+   key_down_func__without_an_event_emitter__will_throw_an_error)
+{
+   AllegroFlare::Elements::Storyboard storyboard;
+   std::string expected_error_message =
+      "Storyboard::key_down_func: error: guard \"event_emitter\" not met";
+   EXPECT_THROW_WITH_MESSAGE(storyboard.key_down_func(), std::runtime_error, expected_error_message);
+}
+
+
+TEST_F(AllegroFlare_Elements_StoryboardTestWithAllegroRenderingFixture,
+   key_down_func__when_at_the_final_page__will_emit_a_screen_switch_event_with_the_expected_param)
+{
+   ALLEGRO_EVENT_QUEUE *event_queue = al_create_event_queue();
+   AllegroFlare::EventEmitter event_emitter;
+   event_emitter.initialize();
+   ALLEGRO_EVENT event;
+   al_register_event_source(event_queue, &event_emitter.get_event_source_ref());
+   std::vector<std::string> pages = { "This is page 1.", "Here is the last page." };
+   std::string screen_identifier_to_switch_to_after_completing = "my-next-screen";
+
+   AllegroFlare::Elements::Storyboard storyboard(
+         &get_font_bin_ref(),
+         &event_emitter,
+         pages,
+         screen_identifier_to_switch_to_after_completing
+      );
+
+   // first page should not trigger an event
+   storyboard.key_down_func();
+   ASSERT_EQ(false, al_get_next_event(event_queue, &event));
+
+   // now at the last page, this should trigger an event
+   storyboard.key_down_func();
+   ASSERT_EQ(true, al_get_next_event(event_queue, &event));
+
+   // the generated event should have the expected values
+   EXPECT_EQ(ALLEGRO_FLARE_EVENT_SWITCH_SCREEN, event.type);
+   ASSERT_NE(nullptr, (void *)(event.user.data1));
+   std::string *data1 = (std::string *)event.user.data1;
+   EXPECT_EQ(screen_identifier_to_switch_to_after_completing, *data1);
+
+   // shutdown
+   al_unregister_event_source(event_queue, &event_emitter.get_event_source_ref());
+   al_destroy_event_queue(event_queue);
+}
+
+
