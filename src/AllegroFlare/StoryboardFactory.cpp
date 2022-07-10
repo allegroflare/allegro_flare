2aac3071af05ed865443b2b04a86de2bcd157514
Mark Oates <markoates0@gmail.com>
2022-07-10 14:24:00 -0400

    * Set auto_advance to true in #create_images_storyboard_screen#

--- src/AllegroFlare/StoryboardFactory.cpp
+++ src/AllegroFlare/StoryboardFactory.cpp
@@ -73,6 +73,7 @@ AllegroFlare::Screens::Storyboard* StoryboardFactory::create_images_storyboard_s
 
    AllegroFlare::Elements::Storyboard &storyboard_element = result->get_storyboard_element_ref();
    storyboard_element.set_button_font_size(button_font_size);
+   storyboard_element.set_auto_advance(true);
    storyboard_element.set_pages(pages);
 
    return result;
