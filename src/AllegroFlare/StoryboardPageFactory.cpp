431258e8391f23c1f59f2497952b7ad19438f209
Mark Oates <markoates0@gmail.com>
2022-07-10 20:28:55 -0400

    * Add StoryboardPageFactory

---
+++ src/AllegroFlare/StoryboardPageFactory.cpp
@@ -0,0 +1,62 @@
+
+
+#include <AllegroFlare/StoryboardPageFactory.hpp>
+#include <AllegroFlare/Elements/StoryboardPages/Text.hpp>
+#include <stdexcept>
+#include <sstream>
+#include <AllegroFlare/Elements/StoryboardPages/Text.hpp>
+#include <stdexcept>
+#include <sstream>
+#include <AllegroFlare/Elements/StoryboardPages/Image.hpp>
+
+
+namespace AllegroFlare
+{
+
+
+StoryboardPageFactory::StoryboardPageFactory(AllegroFlare::FontBin* font_bin)
+   : font_bin(font_bin)
+{
+}
+
+
+StoryboardPageFactory::~StoryboardPageFactory()
+{
+}
+
+
+void StoryboardPageFactory::set_font_bin(AllegroFlare::FontBin* font_bin)
+{
+   this->font_bin = font_bin;
+}
+
+
+AllegroFlare::Elements::StoryboardPages::Text* StoryboardPageFactory::create_text_page(std::string text)
+{
+   if (!(font_bin))
+      {
+         std::stringstream error_message;
+         error_message << "StoryboardPageFactory" << "::" << "create_text_page" << ": error: " << "guard \"font_bin\" not met";
+         throw std::runtime_error(error_message.str());
+      }
+   return new AllegroFlare::Elements::StoryboardPages::Text(font_bin, text);
+}
+
+AllegroFlare::Elements::StoryboardPages::AdvancingText* StoryboardPageFactory::create_advancing_text_page(std::string text)
+{
+   if (!(font_bin))
+      {
+         std::stringstream error_message;
+         error_message << "StoryboardPageFactory" << "::" << "create_advancing_text_page" << ": error: " << "guard \"font_bin\" not met";
+         throw std::runtime_error(error_message.str());
+      }
+   return new AllegroFlare::Elements::StoryboardPages::AdvancingText(font_bin, text);
+}
+
+AllegroFlare::Elements::StoryboardPages::Image* StoryboardPageFactory::create_image_page(ALLEGRO_BITMAP* image)
+{
+   return new AllegroFlare::Elements::StoryboardPages::Image(image);
+}
+} // namespace AllegroFlare
+
+
---
+++ tests/AllegroFlare/StoryboardPageFactoryTest.cpp
@@ -0,0 +1,51 @@
+
+#include <gtest/gtest.h>
+
+#define ASSERT_THROW_WITH_MESSAGE(code, raised_exception_type, expected_exception_message) \
+   try { code; FAIL() << "Expected " # raised_exception_type; } \
+   catch ( raised_exception_type const &err ) { ASSERT_EQ(std::string(expected_exception_message), err.what()); } \
+   catch (...) { FAIL() << "Expected " # raised_exception_type; }
+
+#include <AllegroFlare/Testing/WithAllegroRenderingFixture.hpp>
+
+
+class AllegroFlare_StoryboardPageFactoryTest : public ::testing::Test
+{};
+
+class AllegroFlare_StoryboardPageFactoryTestWithAllegroRenderingFixture
+   : public AllegroFlare::Testing::WithAllegroRenderingFixture
+{};
+
+
+#include <AllegroFlare/StoryboardPageFactory.hpp>
+
+
+TEST_F(AllegroFlare_StoryboardPageFactoryTest, can_be_created_without_blowing_up)
+{
+   AllegroFlare::StoryboardPageFactory storyboard_factory;
+}
+
+
+TEST_F(AllegroFlare_StoryboardPageFactoryTestWithAllegroRenderingFixture,
+   create_text_page__will_create_the_text_page_with_the_expected_params)
+{
+   AllegroFlare::StoryboardPageFactory storyboard_factory(&get_font_bin_ref());
+   AllegroFlare::Elements::StoryboardPages::Text* created_page = storyboard_factory.create_text_page("Hello Factory!");
+
+   EXPECT_EQ(&get_font_bin_ref(), created_page->get_font_bin());
+   EXPECT_EQ("Hello Factory!", created_page->get_text());
+}
+
+
+TEST_F(AllegroFlare_StoryboardPageFactoryTestWithAllegroRenderingFixture,
+   create_advancing_text_page__will_create_the_text_page_with_the_expected_params)
+{
+   AllegroFlare::StoryboardPageFactory storyboard_factory(&get_font_bin_ref());
+   AllegroFlare::Elements::StoryboardPages::AdvancingText* created_page =
+      storyboard_factory.create_advancing_text_page("Hello Factory!");
+
+   EXPECT_EQ(&get_font_bin_ref(), created_page->get_font_bin());
+   EXPECT_EQ("Hello Factory!", created_page->get_text());
+}
+
+
