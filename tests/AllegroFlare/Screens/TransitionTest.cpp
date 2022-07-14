a8448f1bbeb2db393e29ec2d6153648a70c804e1
Mark Oates <markoates0@gmail.com>
2022-07-14 12:46:42 -0400

    Update docs

--- tests/AllegroFlare/Screens/TransitionTest.cpp
+++ tests/AllegroFlare/Screens/TransitionTest.cpp
@@ -4,6 +4,7 @@
 #include <AllegroFlare/Screens/Transition.hpp>
 
 
+
 TEST(AllegroFlare_Screens_TransitionTest, can_be_created_without_blowing_up)
 {
    AllegroFlare::Screens::Transition transition;
