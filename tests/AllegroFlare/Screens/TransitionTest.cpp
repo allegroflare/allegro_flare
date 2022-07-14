4ca97cff36252d88e04bd95821ef228d5f8a785c
Mark Oates <markoates0@gmail.com>
2022-07-14 12:49:17 -0400

    * Add basic rendering test code for Screens/Transition

--- tests/AllegroFlare/Screens/TransitionTest.cpp
+++ tests/AllegroFlare/Screens/TransitionTest.cpp
@@ -1,13 +1,35 @@
 
 #include <gtest/gtest.h>
 
+#define ASSERT_THROW_WITH_MESSAGE(code, raised_exception_type, expected_exception_message) \
+   try { code; FAIL() << "Expected " # raised_exception_type; } \
+   catch ( raised_exception_type const &err ) { ASSERT_EQ(std::string(expected_exception_message), err.what()); } \
+   catch (...) { FAIL() << "Expected " # raised_exception_type; }
+
+#include <AllegroFlare/Testing/WithAllegroRenderingFixture.hpp>
+
+
+class AllegroFlare_Screens_TransitionTest : public ::testing::Test
+{};
+
+class AllegroFlare_Screens_TransitionTestWithAllegroRenderingFixture
+   : public AllegroFlare::Testing::WithAllegroRenderingFixture
+{};
+
+
 #include <AllegroFlare/Screens/Transition.hpp>
 
 
-
-TEST(AllegroFlare_Screens_TransitionTest, can_be_created_without_blowing_up)
+TEST_F(AllegroFlare_Screens_TransitionTest, can_be_created_without_blowing_up)
 {
    AllegroFlare::Screens::Transition transition;
 }
 
+
+TEST_F(AllegroFlare_Screens_TransitionTestWithAllegroRenderingFixture, initialize__will_not_blow_up)
+{
+   AllegroFlare::Screens::Transition transition;
+   transition.initialize();
+   SUCCEED();
+}
 
