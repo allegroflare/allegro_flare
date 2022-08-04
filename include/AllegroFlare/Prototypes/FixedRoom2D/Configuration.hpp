8064695012da423119c7fcfe11464ee8e49dd698
Mark Oates <markoates0@gmail.com>
2022-08-04 18:37:28 -0400

    Add new FixedRoom2D/Configuration

---
+++ include/AllegroFlare/Prototypes/FixedRoom2D/Configuration.hpp
@@ -0,0 +1,64 @@
+#pragma once
+
+
+#include <AllegroFlare/Inventory.hpp>
+#include <AllegroFlare/InventoryIndex.hpp>
+#include <AllegroFlare/Prototypes/FixedRoom2D/Entities/Base.hpp>
+#include <AllegroFlare/Prototypes/FixedRoom2D/Room.hpp>
+#include <AllegroFlare/Prototypes/FixedRoom2D/Script.hpp>
+#include <map>
+#include <string>
+
+
+namespace AllegroFlare
+{
+   namespace Prototypes
+   {
+      namespace FixedRoom2D
+      {
+         class Configuration
+         {
+         private:
+            AllegroFlare::InventoryIndex inventory_index;
+            AllegroFlare::Inventory af_inventory;
+            AllegroFlare::Inventory flags;
+            std::map<std::string, AllegroFlare::Prototypes::FixedRoom2D::Entities::Base*> entity_dictionary;
+            std::map<std::string, AllegroFlare::Prototypes::FixedRoom2D::Room*> room_dictionary;
+            std::map<std::string, std::string> entity_room_associations;
+            std::map<std::string, AllegroFlare::Prototypes::FixedRoom2D::Script> script_dictionary;
+            std::string starting_in_room_identifier;
+
+         public:
+            Configuration(AllegroFlare::InventoryIndex inventory_index={}, AllegroFlare::Inventory af_inventory={}, AllegroFlare::Inventory flags={}, std::map<std::string, AllegroFlare::Prototypes::FixedRoom2D::Entities::Base*> entity_dictionary={}, std::map<std::string, AllegroFlare::Prototypes::FixedRoom2D::Room*> room_dictionary={}, std::map<std::string, std::string> entity_room_associations={}, std::map<std::string, AllegroFlare::Prototypes::FixedRoom2D::Script> script_dictionary={});
+            ~Configuration();
+
+            void set_inventory_index(AllegroFlare::InventoryIndex inventory_index);
+            void set_af_inventory(AllegroFlare::Inventory af_inventory);
+            void set_flags(AllegroFlare::Inventory flags);
+            void set_entity_dictionary(std::map<std::string, AllegroFlare::Prototypes::FixedRoom2D::Entities::Base*> entity_dictionary);
+            void set_room_dictionary(std::map<std::string, AllegroFlare::Prototypes::FixedRoom2D::Room*> room_dictionary);
+            void set_entity_room_associations(std::map<std::string, std::string> entity_room_associations);
+            void set_script_dictionary(std::map<std::string, AllegroFlare::Prototypes::FixedRoom2D::Script> script_dictionary);
+            void set_starting_in_room_identifier(std::string starting_in_room_identifier);
+            AllegroFlare::InventoryIndex get_inventory_index();
+            AllegroFlare::Inventory get_af_inventory();
+            AllegroFlare::Inventory get_flags();
+            std::map<std::string, AllegroFlare::Prototypes::FixedRoom2D::Entities::Base*> get_entity_dictionary();
+            std::map<std::string, AllegroFlare::Prototypes::FixedRoom2D::Room*> get_room_dictionary();
+            std::map<std::string, std::string> get_entity_room_associations();
+            std::map<std::string, AllegroFlare::Prototypes::FixedRoom2D::Script> get_script_dictionary();
+            std::string get_starting_in_room_identifier();
+            AllegroFlare::InventoryIndex &get_inventory_index_ref();
+            AllegroFlare::Inventory &get_af_inventory_ref();
+            AllegroFlare::Inventory &get_flags_ref();
+            std::map<std::string, AllegroFlare::Prototypes::FixedRoom2D::Entities::Base*> &get_entity_dictionary_ref();
+            std::map<std::string, AllegroFlare::Prototypes::FixedRoom2D::Room*> &get_room_dictionary_ref();
+            std::map<std::string, std::string> &get_entity_room_associations_ref();
+            std::map<std::string, AllegroFlare::Prototypes::FixedRoom2D::Script> &get_script_dictionary_ref();
+         };
+      }
+   }
+}
+
+
+
---
+++ src/AllegroFlare/Prototypes/FixedRoom2D/Configuration.cpp
@@ -0,0 +1,175 @@
+
+
+#include <AllegroFlare/Prototypes/FixedRoom2D/Configuration.hpp>
+
+
+
+namespace AllegroFlare
+{
+namespace Prototypes
+{
+namespace FixedRoom2D
+{
+
+
+Configuration::Configuration(AllegroFlare::InventoryIndex inventory_index, AllegroFlare::Inventory af_inventory, AllegroFlare::Inventory flags, std::map<std::string, AllegroFlare::Prototypes::FixedRoom2D::Entities::Base*> entity_dictionary, std::map<std::string, AllegroFlare::Prototypes::FixedRoom2D::Room*> room_dictionary, std::map<std::string, std::string> entity_room_associations, std::map<std::string, AllegroFlare::Prototypes::FixedRoom2D::Script> script_dictionary)
+   : inventory_index(inventory_index)
+   , af_inventory(af_inventory)
+   , flags(flags)
+   , entity_dictionary(entity_dictionary)
+   , room_dictionary(room_dictionary)
+   , entity_room_associations(entity_room_associations)
+   , script_dictionary(script_dictionary)
+   , starting_in_room_identifier("[unset-starting_in_room_identifier]")
+{
+}
+
+
+Configuration::~Configuration()
+{
+}
+
+
+void Configuration::set_inventory_index(AllegroFlare::InventoryIndex inventory_index)
+{
+   this->inventory_index = inventory_index;
+}
+
+
+void Configuration::set_af_inventory(AllegroFlare::Inventory af_inventory)
+{
+   this->af_inventory = af_inventory;
+}
+
+
+void Configuration::set_flags(AllegroFlare::Inventory flags)
+{
+   this->flags = flags;
+}
+
+
+void Configuration::set_entity_dictionary(std::map<std::string, AllegroFlare::Prototypes::FixedRoom2D::Entities::Base*> entity_dictionary)
+{
+   this->entity_dictionary = entity_dictionary;
+}
+
+
+void Configuration::set_room_dictionary(std::map<std::string, AllegroFlare::Prototypes::FixedRoom2D::Room*> room_dictionary)
+{
+   this->room_dictionary = room_dictionary;
+}
+
+
+void Configuration::set_entity_room_associations(std::map<std::string, std::string> entity_room_associations)
+{
+   this->entity_room_associations = entity_room_associations;
+}
+
+
+void Configuration::set_script_dictionary(std::map<std::string, AllegroFlare::Prototypes::FixedRoom2D::Script> script_dictionary)
+{
+   this->script_dictionary = script_dictionary;
+}
+
+
+void Configuration::set_starting_in_room_identifier(std::string starting_in_room_identifier)
+{
+   this->starting_in_room_identifier = starting_in_room_identifier;
+}
+
+
+AllegroFlare::InventoryIndex Configuration::get_inventory_index()
+{
+   return inventory_index;
+}
+
+
+AllegroFlare::Inventory Configuration::get_af_inventory()
+{
+   return af_inventory;
+}
+
+
+AllegroFlare::Inventory Configuration::get_flags()
+{
+   return flags;
+}
+
+
+std::map<std::string, AllegroFlare::Prototypes::FixedRoom2D::Entities::Base*> Configuration::get_entity_dictionary()
+{
+   return entity_dictionary;
+}
+
+
+std::map<std::string, AllegroFlare::Prototypes::FixedRoom2D::Room*> Configuration::get_room_dictionary()
+{
+   return room_dictionary;
+}
+
+
+std::map<std::string, std::string> Configuration::get_entity_room_associations()
+{
+   return entity_room_associations;
+}
+
+
+std::map<std::string, AllegroFlare::Prototypes::FixedRoom2D::Script> Configuration::get_script_dictionary()
+{
+   return script_dictionary;
+}
+
+
+std::string Configuration::get_starting_in_room_identifier()
+{
+   return starting_in_room_identifier;
+}
+
+
+AllegroFlare::InventoryIndex &Configuration::get_inventory_index_ref()
+{
+   return inventory_index;
+}
+
+
+AllegroFlare::Inventory &Configuration::get_af_inventory_ref()
+{
+   return af_inventory;
+}
+
+
+AllegroFlare::Inventory &Configuration::get_flags_ref()
+{
+   return flags;
+}
+
+
+std::map<std::string, AllegroFlare::Prototypes::FixedRoom2D::Entities::Base*> &Configuration::get_entity_dictionary_ref()
+{
+   return entity_dictionary;
+}
+
+
+std::map<std::string, AllegroFlare::Prototypes::FixedRoom2D::Room*> &Configuration::get_room_dictionary_ref()
+{
+   return room_dictionary;
+}
+
+
+std::map<std::string, std::string> &Configuration::get_entity_room_associations_ref()
+{
+   return entity_room_associations;
+}
+
+
+std::map<std::string, AllegroFlare::Prototypes::FixedRoom2D::Script> &Configuration::get_script_dictionary_ref()
+{
+   return script_dictionary;
+}
+
+
+} // namespace FixedRoom2D
+} // namespace Prototypes
+} // namespace AllegroFlare
+
+
