0c91a15dba14f4e2721f8c79197b046800b22a32
Mark Oates <markoates0@gmail.com>
2022-08-10 11:50:35 -0400

    * Use new #restart# AudioRepositoryElement property in FixedRoom2D example

--- examples/AllegroFlare/Prototypes/FixedRoom2DExample.cpp
+++ examples/AllegroFlare/Prototypes/FixedRoom2DExample.cpp
@@ -11,10 +11,16 @@ int main(int argc, char **argv)
 
    AllegroFlare::BitmapBin &bitmap_bin = framework.get_bitmap_bin_ref();
    AllegroFlare::FontBin &font_bin = framework.get_font_bin_ref();
+   AllegroFlare::SampleBin &sample_bin = framework.get_sample_bin_ref();
    AllegroFlare::EventEmitter &event_emitter = framework.get_event_emitter_ref();
    AllegroFlare::AudioController &audio_controller = framework.get_audio_controller_ref();
    bitmap_bin.set_full_path("/Users/markoates/Repos/allegro_flare/bin/data/bitmaps");
    font_bin.set_full_path("/Users/markoates/Repos/allegro_flare/bin/data/fonts");
+   sample_bin.set_full_path("/Users/markoates/Repos/allegro_flare/bin/data/samples");
+
+   audio_controller.set_and_load_sound_effect_elements({
+         { "menu-click-01.wav", { "menu-click-01.wav", false, "restart" } },
+         });
 
    AllegroFlare::Prototypes::FixedRoom2D::Screen example(&bitmap_bin, &font_bin, &event_emitter, &audio_controller);
    example.initialize();
