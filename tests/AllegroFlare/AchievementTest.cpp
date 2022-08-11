86154e7c46923107e05f69ad64a2b5ed84b76616
Mark Oates <markoates0@gmail.com>
2022-08-11 16:51:35 -0400

    * Prefix Achievement test suite name with #AllegroFlare_#

--- tests/AllegroFlare/AchievementTest.cpp
+++ tests/AllegroFlare/AchievementTest.cpp
@@ -6,20 +6,20 @@
 #include <AllegroFlare/Achievement.hpp>
 
 
-TEST(AchievementTest, can_be_created_without_arguments)
+TEST(AllegroFlare_AchievementTest, can_be_created_without_arguments)
 {
    AllegroFlare::Achievement achievements;
 }
 
 
-TEST(AchievementTest, unlocked__is_initialized_to_false)
+TEST(AllegroFlare_AchievementTest, unlocked__is_initialized_to_false)
 {
    AllegroFlare::Achievement achievements;
    EXPECT_EQ(false, achievements.is_unlocked());
 }
 
 
-TEST(AchievementTest, unlock_manually__will_unlock_the_achievement_and_return_true)
+TEST(AllegroFlare_AchievementTest, unlock_manually__will_unlock_the_achievement_and_return_true)
 {
    AllegroFlare::Achievement achievements;
    EXPECT_EQ(true, achievements.unlock_manually());
@@ -27,13 +27,13 @@ TEST(AchievementTest, unlock_manually__will_unlock_the_achievement_and_return_tr
 }
 
 
-TEST(AchievementTest, unlock_manually__will_call_the_achievements_on_unlocked_method)
+TEST(AllegroFlare_AchievementTest, unlock_manually__will_call_the_achievements_on_unlocked_method)
 {
    // TODO: this test
 }
 
 
-TEST(AchievementTest, unlock_manually__if_already_unlocked__will_do_nothing_and_return_false)
+TEST(AllegroFlare_AchievementTest, unlock_manually__if_already_unlocked__will_do_nothing_and_return_false)
 {
    AllegroFlare::Achievement achievements;
    EXPECT_EQ(true, achievements.unlock_manually());
