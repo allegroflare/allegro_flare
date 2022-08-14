94785d7b663a7fba265a97b705cb7fb442d6d3c2
Mark Oates <markoates0@gmail.com>
2022-08-14 09:25:59 -0400

    * DISABLED interactive test for AchievementsList

--- tests/AllegroFlare/Elements/AchievementsListTest.cpp
+++ tests/AllegroFlare/Elements/AchievementsListTest.cpp
@@ -75,7 +75,7 @@ TEST_F(AllegroFlare_Elements_AchievementsListTestWithAllegroRenderingFixture,
 
 
 TEST_F(AllegroFlare_Elements_AchievementsListTestWithAllegroRenderingFixture,
-   INTERACTIVE__will_work_as_expected)
+   DISABLED__INTERACTIVE__will_work_as_expected)
 {
    // setup system
    al_install_keyboard();
