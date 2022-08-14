1c8f7c12ceebeafd50b8edb1649812455c06d074
Mark Oates <markoates0@gmail.com>
2022-08-14 09:29:38 -0400

    * Rename instances of #scroll_bar# to #scrollbar# in tests

--- tests/AllegroFlare/Elements/ScrollBarTest.cpp
+++ tests/AllegroFlare/Elements/ScrollBarTest.cpp
@@ -22,45 +22,45 @@ class AllegroFlare_Elements_ScrollbarTestWithAllegroRenderingFixture
 
 TEST_F(AllegroFlare_Elements_ScrollbarTest, can_be_created_without_blowing_up)
 {
-   AllegroFlare::Elements::Scrollbar scroll_bar;
+   AllegroFlare::Elements::Scrollbar scrollbar;
 }
 
 
 TEST_F(AllegroFlare_Elements_ScrollbarTest, render__without_allegro_initialized__raises_an_error)
 {
-   AllegroFlare::Elements::Scrollbar scroll_bar;
+   AllegroFlare::Elements::Scrollbar scrollbar;
    std::string expected_error_message =
       "Scrollbar::render: error: guard \"al_is_system_installed()\" not met";
-   EXPECT_THROW_WITH_MESSAGE(scroll_bar.render(), std::runtime_error, expected_error_message);
+   EXPECT_THROW_WITH_MESSAGE(scrollbar.render(), std::runtime_error, expected_error_message);
 }
 
 
 TEST_F(AllegroFlare_Elements_ScrollbarTest, render__without_the_primitives_addon_initialized__raises_an_error)
 {
    al_init();
-   AllegroFlare::Elements::Scrollbar scroll_bar;
+   AllegroFlare::Elements::Scrollbar scrollbar;
    std::string expected_error_message =
       "Scrollbar::render: error: guard \"al_is_primitives_addon_initialized()\" not met";
-   EXPECT_THROW_WITH_MESSAGE(scroll_bar.render(), std::runtime_error, expected_error_message);
+   EXPECT_THROW_WITH_MESSAGE(scrollbar.render(), std::runtime_error, expected_error_message);
    al_uninstall_system();
 }
 
 
 TEST_F(AllegroFlare_Elements_ScrollbarTestWithAllegroRenderingFixture, render__will_not_blow_up)
 {
-   AllegroFlare::Elements::Scrollbar scroll_bar;
-   scroll_bar.render();
+   AllegroFlare::Elements::Scrollbar scrollbar;
+   scrollbar.render();
 }
 
 
 TEST_F(AllegroFlare_Elements_ScrollbarTestWithAllegroRenderingFixture,
-   CAPTURE__render__will_draw_the_bar_as_expected)
+   CAPTURE__render__will_draw_thebar_as_expected)
 {
    AllegroFlare::Placement2D place = build_centered_placement(10, 300);
-   AllegroFlare::Elements::Scrollbar scroll_bar;
+   AllegroFlare::Elements::Scrollbar scrollbar;
 
    place.start_transform();
-   scroll_bar.render();
+   scrollbar.render();
    al_flip_display();
    place.restore_transform();
 }
