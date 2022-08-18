d5481251f2afdb37d341c2056516c90b3b33027d
Mark Oates <markoates0@gmail.com>
2022-08-18 08:13:25 -0400

    Convert "motion_edit" to basic threaded program

--- programs/motion_edit.cpp
+++ programs/motion_edit.cpp
@@ -1,6 +1,8 @@
+#include <thread>
+//#include <mutext>
+
 #include <AllegroFlare/Screens/Base.hpp>
 #include <AllegroFlare/Frameworks/Full.hpp>
-
 
 #include <AllegroFlare/Timeline/Actor.hpp>
 #include <AllegroFlare/Timeline/Track.hpp>
@@ -8,50 +10,6 @@
 
 
 ALLEGRO_FONT *icon_font = nullptr;
-
-
-void draw_centered_unicode_character(ALLEGRO_FONT* font, ALLEGRO_COLOR color, int x, int y, uint32_t icon, int flags)
-{
-   static ALLEGRO_USTR *ustr = NULL;
-   if (!ustr) ustr = al_ustr_new("");
-   al_ustr_set_chr(ustr, 0, icon);
-   float width = al_get_ustr_width(font, ustr);
-   float height = al_get_font_line_height(font);
-   al_draw_ustr(font, color, x-width*0.5, y-height*0.5, flags, ustr);
-   return;
-}
-
-
-
-class TrackView
-{
-private:
-   AllegroFlare::Timeline::Track *track;
-   float width;
-   float height;
-   float time_start;
-   float time_scale;
-
-public:
-   TrackView(AllegroFlare::Timeline::Track *track)
-      : track(track)
-   {};
-   void draw()
-   {
-      float track_x = 0;
-      float track_y = 0;
-
-      int count = 0;
-      for (auto &keyframe : track->get_keyframes())
-      {
-         int32_t diamond = 0xf219;
-         ALLEGRO_COLOR color = AllegroFlare::Color::DeepSkyBlue;
-         draw_centered_unicode_character(icon_font, color, track_x, track_y, diamond, 0);
-         count++;
-      }
-   }
-};
-
 
 
 class MotionEdit : public AllegroFlare::Screens::Base
@@ -75,7 +33,7 @@ public:
 
 
 
-int main(int argc, char **argv)
+void framework_main()
 {
    AllegroFlare::Frameworks::Full framework;
    framework.initialize();
@@ -87,6 +45,29 @@ int main(int argc, char **argv)
    framework.activate_screen("motion_edit");
 
    framework.run_loop();
+}
+
+
+void network_main()
+{
+   for (unsigned i=0; i<5; i++)
+   {
+      std::cout << "NETWORK_MAIN() count: " << i << std::endl;
+      sleep(1);
+   }
+}
+
+
+
+int main(int argc, char **argv)
+{
+   std::thread first(framework_main);
+   std::thread second(network_main);
+
+   first.join();
+   second.join();
+
+   std::cout << "executed_expectedly." << std::endl;
 }
 
 
