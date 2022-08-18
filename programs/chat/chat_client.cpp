09b5a7c76140d2d350220022727e175b47ef00fb
Mark Oates <markoates0@gmail.com>
2022-08-18 12:24:13 -0400

    Refactor asio example extract function

--- programs/chat/chat_client.cpp
+++ programs/chat/chat_client.cpp
@@ -129,20 +129,23 @@ private:
   chat_message_queue write_msgs_;
 };
 
-int main(int argc, char* argv[])
+
+
+
+void client_runner(std::string host, std::string port)
 {
   try
   {
-    if (argc != 3)
-    {
-      std::cerr << "Usage: chat_client <host> <port>\n";
-      return 1;
-    }
+    //if (argc != 3)
+    //{
+      //std::cerr << "Usage: chat_client <host> <port>\n";
+      //return 1;
+    //}
 
     asio::io_context io_context;
 
     tcp::resolver resolver(io_context);
-    auto endpoints = resolver.resolve(argv[1], argv[2]);
+    auto endpoints = resolver.resolve(host, port);
     chat_client c(io_context, endpoints);
 
     std::thread t([&io_context](){ io_context.run(); });
@@ -164,6 +167,56 @@ int main(int argc, char* argv[])
   {
     std::cerr << "Exception: " << e.what() << "\n";
   }
+}
 
-  return 0;
+
+
+
+int main(int argc, char* argv[])
+{
+ if (argc != 3)
+ {
+   std::cerr << "Usage: chat_client <host> <port>\n";
+   return 1;
+ }
+
+ std::string host = argv[1];
+std::string port = argv[2];
+
+  client_runner(host, port);
+  //try
+  //{
+    //if (argc != 3)
+    //{
+      //std::cerr << "Usage: chat_client <host> <port>\n";
+      //return 1;
+    //}
+
+    //asio::io_context io_context;
+
+    //tcp::resolver resolver(io_context);
+    //auto endpoints = resolver.resolve(argv[1], argv[2]);
+    //chat_client c(io_context, endpoints);
+
+    //std::thread t([&io_context](){ io_context.run(); });
+
+    //char line[chat_message::max_body_length + 1];
+    //while (std::cin.getline(line, chat_message::max_body_length + 1))
+    //{
+      //chat_message msg;
+      //msg.body_length(std::strlen(line));
+      //std::memcpy(msg.body(), line, msg.body_length());
+      //msg.encode_header();
+      //c.write(msg);
+    //}
+
+    //c.close();
+    //t.join();
+  //}
+  //catch (std::exception& e)
+  //{
+    //std::cerr << "Exception: " << e.what() << "\n";
+  //}
+
+  //return 0;
 }
