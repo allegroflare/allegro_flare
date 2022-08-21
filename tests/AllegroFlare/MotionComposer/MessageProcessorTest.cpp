16af34c72d77986d4026f0266e2d77b40ecd6745
Mark Oates <markoates0@gmail.com>
2022-08-21 14:18:46 -0400

    * Add feature to validate presence of property, along with error, in MessageProcessor

--- tests/AllegroFlare/MotionComposer/MessageProcessorTest.cpp
+++ tests/AllegroFlare/MotionComposer/MessageProcessorTest.cpp
@@ -1,6 +1,11 @@
 
 #include <gtest/gtest.h>
 
+#define EXPECT_THROW_WITH_MESSAGE(code, raised_exception_type, expected_exception_message) \
+   try { code; FAIL() << "Expected " # raised_exception_type; } \
+   catch ( raised_exception_type const &err ) { EXPECT_EQ(std::string(expected_exception_message), err.what()); } \
+   catch (...) { FAIL() << "Expected " # raised_exception_type; }
+
 #include <AllegroFlare/MotionComposer/MessageProcessor.hpp>
 
 
@@ -10,18 +15,27 @@ TEST(AllegroFlare_MotionComposer_MessageProcessorTest, can_be_created_without_bl
 }
 
 
-TEST(AllegroFlare_MotionComposer_MessageProcessorTest, hold)
+TEST(AllegroFlare_MotionComposer_MessageProcessorTest,
+   build_message_from_json__when_the_message_key_is_not_present__will_throw_an_error)
 {
    AllegroFlare::MotionComposer::MessageProcessor message_processor;
-   //std::string json =
-//R"({
-  //"position": 1.5
-//})";
+   std::string message_json =
+R"({
+  "a_key_but_not_the_message_key": 1.5
+})";
+   std::string expected_message = "AllegroFlare::MotionComposer::MessageProcessor::build_message_from_json: error: "
+                                  "Expecting JSON to have a \"messages\" property but it does not.  JSON:\n"
+                                  "\n"
+                                  "{\n"
+                                  "  \"a_key_but_not_the_message_key\": 1.5\n"
+                                  "}";
 
-   //nlohmann::json parsed_json = nlohmann::json::parse(json);
-   //parsed_json.get_to(set_playhead_position);
-   //std::string json_message = R
-   //std::vector<std::string> messages { some_json }
+
+   EXPECT_THROW_WITH_MESSAGE(message_processor.build_message_from_json(
+         message_json),
+         std::runtime_error,
+         expected_message
+   );
 }
 
 
