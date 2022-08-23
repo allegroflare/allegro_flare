a810930998397c57cc9ec57291c1e4533cd17056
Mark Oates <markoates0@gmail.com>
2022-08-23 13:19:04 -0400

    Disable INTERACTIVE test

--- tests/AllegroFlare/Screens/RollingCreditsTest.cpp
+++ tests/AllegroFlare/Screens/RollingCreditsTest.cpp
@@ -61,7 +61,7 @@ TEST_F(AllegroFlare_Screens_RollingCreditsTestWithAllegroRenderingFixture,
 
 
 TEST_F(AllegroFlare_Screens_RollingCreditsTestWithAllegroRenderingFixture,
-   INTERACTIVE__will_work_as_expected)
+   DISABLED__INTERACTIVE__will_work_as_expected)
 {
    // setup system
    al_install_keyboard();
