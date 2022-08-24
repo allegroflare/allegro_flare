7c37460ec55dacdf62403c9846baa829eaa1a902
Mark Oates <markoates0@gmail.com>
2022-08-24 07:41:31 -0400

    * Restore tests for ColorIDConverter

--- tests/AllegroFlare/ColorIDConverterTest.cpp
+++ tests/AllegroFlare/ColorIDConverterTest.cpp
@@ -1,128 +1,105 @@
 
 
-//#define BOOST_TEST_DYN_LINK
-//#define BOOST_TEST_MODULE AllegroFlare_ColorIDConverter
-//#include <boost/test/unit_test.hpp>
+#include <gtest/gtest.h>
+
+#include <AllegroFlare/ColorIDConverter.hpp>
 
 
+struct AllegroFlare_ColorIDConverterTest : public ::testing::Test
+{
+   virtual void SetUp() override
+   {
+      ASSERT_EQ(false, al_is_system_installed());
+      ASSERT_EQ(true, al_init());
+   }
+
+   virtual void TearDown() override
+   {
+      al_uninstall_system();
+   }
+};
 
 
-//#include <AllegroFlare/ColorIDConverter.hpp>
+TEST_F(AllegroFlare_ColorIDConverterTest, decodes_transparent_to_ID_0)
+{
+   ALLEGRO_COLOR color = al_map_rgba(0, 0, 0, 0);
+   int decoded_id = AllegroFlare::ColorIDConverter::decode_id(color);
+
+   EXPECT_EQ(0, decoded_id);
+}
 
 
+TEST_F(AllegroFlare_ColorIDConverterTest, encodes_ID_0_to_transparent)
+{
+   ALLEGRO_COLOR color = AllegroFlare::ColorIDConverter::encode_id(0);
+
+   unsigned char r, g, b, a;
+   al_unmap_rgba(color, &r, &g, &b, &a);
+
+   EXPECT_EQ(0, r);
+   EXPECT_EQ(0, g);
+   EXPECT_EQ(0, b);
+   EXPECT_EQ(0, a);
+}
 
 
-//using namespace AllegroFlare;
+TEST_F(AllegroFlare_ColorIDConverterTest, encodes_IDs_greater_than_0_with_an_alpha_of_255__test_1)
+{
+   ALLEGRO_COLOR color = AllegroFlare::ColorIDConverter::encode_id(1);
+
+   unsigned char r, g, b, a;
+   al_unmap_rgba(color, &r, &g, &b, &a);
+
+   EXPECT_EQ(255, a);
+}
 
 
+TEST_F(AllegroFlare_ColorIDConverterTest, encodes_IDs_greater_than_0_with_an_alpha_of_255__test_2)
+{
+   for (unsigned i=1; i<1000; i++)
+   {
+      ALLEGRO_COLOR color = AllegroFlare::ColorIDConverter::encode_id(i);
+      unsigned char r, g, b, a;
+      al_unmap_rgba(color, &r, &g, &b, &a);
+
+      EXPECT_EQ(255, a);
+   }
+}
 
 
-//struct Setup
-//{
-   //Setup()
-   //{
-      //BOOST_REQUIRE_EQUAL(false, al_is_system_installed());
-      //BOOST_REQUIRE_EQUAL(true, al_init());
-   //}
-   //~Setup()
-   //{
-      //al_uninstall_system();
-   //}
-//};
+TEST_F(AllegroFlare_ColorIDConverterTest, encodes_all_numbers_less_than_ColorIDConverter_ID_MAX)
+{
+   for (int i=0; i<AllegroFlare::ColorIDConverter::ID_MAX; i++)
+   {
+      int encoded_num = AllegroFlare::ColorIDConverter::decode_id(AllegroFlare::ColorIDConverter::encode_id(i));
+      EXPECT_EQ(i, encoded_num);
+   }
+}
 
 
+TEST_F(AllegroFlare_ColorIDConverterTest, does_not_encode_numbers_greater_than_or_equal_to_ColorIDConverter_ID_MAX)
+{
+   int encoded_num = 0;
+   int expected_num = 0;
+
+   encoded_num = AllegroFlare::ColorIDConverter::decode_id(
+      AllegroFlare::ColorIDConverter::encode_id(AllegroFlare::ColorIDConverter::ID_MAX)
+   );
+   expected_num = AllegroFlare::ColorIDConverter::ID_MAX;
+   EXPECT_NE(expected_num, encoded_num);
+
+   encoded_num = AllegroFlare::ColorIDConverter::decode_id(
+      AllegroFlare::ColorIDConverter::encode_id(AllegroFlare::ColorIDConverter::ID_MAX+1)
+   );
+   expected_num = AllegroFlare::ColorIDConverter::ID_MAX+1;
+   EXPECT_NE(expected_num, encoded_num);
+}
 
 
-//BOOST_FIXTURE_TEST_CASE(decodes_transparent_to_ID_0, Setup)
-//{
-   //ALLEGRO_COLOR color = al_map_rgba(0, 0, 0, 0);
-   //int decoded_id = ColorIDConverter::decode_id(color);
-
-   //BOOST_CHECK_EQUAL(0, decoded_id);
-//}
-
-
-
-
-//BOOST_FIXTURE_TEST_CASE(encodes_ID_0_to_transparent, Setup)
-//{
-   //ALLEGRO_COLOR color = ColorIDConverter::encode_id(0);
-
-   //unsigned char r, g, b, a;
-   //al_unmap_rgba(color, &r, &g, &b, &a);
-
-   //BOOST_CHECK_EQUAL(0, r);
-   //BOOST_CHECK_EQUAL(0, g);
-   //BOOST_CHECK_EQUAL(0, b);
-   //BOOST_CHECK_EQUAL(0, a);
-//}
-
-
-
-
-//BOOST_FIXTURE_TEST_CASE(encodes_IDs_greater_than_0_with_an_alpha_of_255__test_1, Setup)
-//{
-   //ALLEGRO_COLOR color = ColorIDConverter::encode_id(1);
-
-   //unsigned char r, g, b, a;
-   //al_unmap_rgba(color, &r, &g, &b, &a);
-
-   //BOOST_CHECK_EQUAL(255, a);
-//}
-
-
-
-
-//BOOST_FIXTURE_TEST_CASE(encodes_IDs_greater_than_0_with_an_alpha_of_255__test_2, Setup)
-//{
-   //for (unsigned i=1; i<1000; i++)
-   //{
-      //ALLEGRO_COLOR color = ColorIDConverter::encode_id(i);
-      //unsigned char r, g, b, a;
-      //al_unmap_rgba(color, &r, &g, &b, &a);
-
-      //BOOST_CHECK_EQUAL(255, a);
-   //}
-//}
-
-
-
-
-//BOOST_FIXTURE_TEST_CASE(encodes_all_numbers_less_than_ColorIDConverter_ID_MAX, Setup)
-//{
-   //for (int i=0; i<ColorIDConverter::ID_MAX; i++)
-   //{
-      //int encoded_num = ColorIDConverter::decode_id(ColorIDConverter::encode_id(i));
-      //BOOST_CHECK_EQUAL(i, encoded_num);
-   //}
-//}
-
-
-
-
-//BOOST_FIXTURE_TEST_CASE(does_not_encode_numbers_greater_than_or_equal_to_ColorIDConverter_ID_MAX, Setup)
-//{
-   //int encoded_num = 0;
-   //int expected_num = 0;
-
-   //encoded_num = ColorIDConverter::decode_id(ColorIDConverter::encode_id(ColorIDConverter::ID_MAX));
-   //expected_num = ColorIDConverter::ID_MAX;
-   //BOOST_CHECK_NE(expected_num, encoded_num);
-
-   //encoded_num = ColorIDConverter::decode_id(ColorIDConverter::encode_id(ColorIDConverter::ID_MAX+1));
-   //expected_num = ColorIDConverter::ID_MAX+1;
-   //BOOST_CHECK_NE(expected_num, encoded_num);
-//}
-
-
-
-
-//BOOST_FIXTURE_TEST_CASE(does_not_encode_negative_numbers, Setup)
-//{
-   //int encoded_num = ColorIDConverter::decode_id(ColorIDConverter::encode_id(-1));
-   //BOOST_CHECK_NE(-1, encoded_num);
-//}
-
-
+TEST_F(AllegroFlare_ColorIDConverterTest, does_not_encode_negative_numbers)
+{
+   int encoded_num = AllegroFlare::ColorIDConverter::decode_id(AllegroFlare::ColorIDConverter::encode_id(-1));
+   EXPECT_NE(-1, encoded_num);
+}
 
 
