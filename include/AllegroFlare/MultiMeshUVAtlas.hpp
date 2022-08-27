e8dfeee6601ea942a7bdc7b18b62523c8738f7bd
Mark Oates <markoates0@gmail.com>
2022-08-27 00:10:20 -0400

    * Add MultiMeshUVAtlas (untested) #add#, #exists#, #get#

--- include/AllegroFlare/MultiMeshUVAtlas.hpp
+++ include/AllegroFlare/MultiMeshUVAtlas.hpp
@@ -18,6 +18,9 @@ namespace AllegroFlare
 
       void set_index(std::map<int, AllegroFlare::MultiMeshUV> index);
       std::map<int, AllegroFlare::MultiMeshUV> get_index() const;
+      void add(int index_num=0, float u1=0.0f, float v1=0.0f, float u2=1.0f, float v2=1.0f);
+      bool exists(int index_num=0);
+      AllegroFlare::MultiMeshUV get(int index_num=0);
    };
 }
 
--- src/AllegroFlare/MultiMeshUVAtlas.cpp
+++ src/AllegroFlare/MultiMeshUVAtlas.cpp
@@ -1,7 +1,8 @@
 
 
 #include <AllegroFlare/MultiMeshUVAtlas.hpp>
-
+#include <sstream>
+#include <iostream>
 
 
 namespace AllegroFlare
@@ -31,6 +32,29 @@ std::map<int, AllegroFlare::MultiMeshUV> MultiMeshUVAtlas::get_index() const
 }
 
 
+void MultiMeshUVAtlas::add(int index_num, float u1, float v1, float u2, float v2)
+{
+   index[index_num] = AllegroFlare::MultiMeshUV(u1, v1, u2, v2);
+   return;
+}
+
+bool MultiMeshUVAtlas::exists(int index_num)
+{
+   return (index.count(index_num) == 1);
+}
+
+AllegroFlare::MultiMeshUV MultiMeshUVAtlas::get(int index_num)
+{
+   if (!exists(index_num))
+   {
+      std::stringstream error_message;
+      error_message << "AllegroFlare::MultiMeshUVAtlas::get error: "
+                    << "Item does not exist at index_num " << index_num << ". Returning a default MultiMeshUV.";
+      std::cout << error_message.str() << std::endl;
+      return AllegroFlare::MultiMeshUV();
+   }
+   return index[index_num];
+}
 } // namespace AllegroFlare
 
 
