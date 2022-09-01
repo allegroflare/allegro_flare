ddedd8bfec17d0a035ab10134d082e7a5e257c1e
Mark Oates <markoates0@gmail.com>
2022-09-01 03:03:17 -0400

    * Use random ints to distribute more glyphs on the screen

--- tests/AllegroFlare/MusicMesh/MusicMeshTest.cpp
+++ tests/AllegroFlare/MusicMesh/MusicMeshTest.cpp
@@ -7,6 +7,7 @@
    catch (...) { FAIL() << "Expected " # raised_exception_type; }
 
 #include <AllegroFlare/Testing/WithAllegroRenderingFixture.hpp>
+#include <AllegroFlare/Random.hpp>
 
 
 class AllegroFlare_MusicMesh_MusicMeshTest : public ::testing::Test
@@ -40,11 +41,15 @@ TEST_F(AllegroFlare_MusicMesh_MusicMeshTestWithAllegroRenderingFixture,
 {
    AllegroFlare::MusicMesh::MusicMesh music_mesh(&get_font_bin_ref());
    music_mesh.initialize();
+   AllegroFlare::Random random;
 
    uint32_t start = AllegroFlare::MusicMesh::FontCharacterAtlasBuilder::UNICODE_RANGE_START;
 
-   music_mesh.add_music_symbol(400, 300, start + 32 + 2); // f clef
-   music_mesh.add_music_symbol(300, 300, start + 32 - 2); // g clef
+   for (int i=0; i<256*8; i++)
+   {
+      music_mesh.add_music_symbol(random.get_random_int(0, 1920), random.get_random_int(0, 1080), start + 32 + 2);
+      //music_mesh.add_music_symbol(300, 300, start + 32 - 2); // g clef
+   }
 
    music_mesh.render();
    al_flip_display();
