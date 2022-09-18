30aaff6adbdd7b3db6767594274a1da2a41c2d59
Mark Oates <markoates0@gmail.com>
2022-09-17 23:52:07 -0400

    Simplify FunzDemo::run()

--- programs/wicked_funz_demo.cpp
+++ programs/wicked_funz_demo.cpp
@@ -1,26 +1,11 @@
 #include <AllegroFlare/Frameworks/Full.hpp>
 #include <WickedDemos/FunzDemo.hpp>
 
+// https://docs.microsoft.com/en-us/windows/win32/dxtecharts/common-techniques-to-improve-shadow-depth-maps
 
 int main(int argc, char **argv)
 {
    FunzDemo::run();
-
-   AllegroFlare::Frameworks::Full framework;
-   framework.disable_fullscreen();
-   framework.initialize();
-
-   FunzDemo *funz_demo = new FunzDemo(&framework, framework.get_primary_display());
-   funz_demo->initialize();
-
-   framework.register_screen("funz_demo", funz_demo);
-   framework.activate_screen("funz_demo");
-
-	framework.run_loop();
-
-   framework.unregister_screen(funz_demo);
-   delete funz_demo;
-
 	return 0;
 }
 
