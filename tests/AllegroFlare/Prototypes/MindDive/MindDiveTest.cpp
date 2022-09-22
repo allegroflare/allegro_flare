eb3c0dea4109b2169e0bf00e2a7802109e145360
Mark Oates <markoates0@gmail.com>
2022-09-21 21:31:12 -0400

    * Add basic INTERACTIVE test for MindDive/MindDive

--- tests/AllegroFlare/Prototypes/MindDive/MindDiveTest.cpp
+++ tests/AllegroFlare/Prototypes/MindDive/MindDiveTest.cpp
@@ -16,6 +16,7 @@ class AllegroFlare_Prototypes_MindDive_MindDiveTestWithAllegroRenderingFixture
 
 #include <AllegroFlare/Prototypes/MindDive/MindDive.hpp>
 #include <allegro5/allegro_primitives.h> // for al_is_primitives_addon_initialized();
+#include <AllegroFlare/EventEmitter.hpp>
 
 
 TEST_F(AllegroFlare_Prototypes_MindDive_MindDiveTest, can_be_created_without_blowing_up)
@@ -25,7 +26,7 @@ TEST_F(AllegroFlare_Prototypes_MindDive_MindDiveTest, can_be_created_without_blo
 
 
 TEST_F(AllegroFlare_Prototypes_MindDive_MindDiveTestWithAllegroRenderingFixture,
-   INTERACTIVE__render__will_work_as_expected)
+   CAPTURE__basic_update_and_render_will_work_as_expected)
 {
    AllegroFlare::Prototypes::MindDive::MindDive mind_dive(&get_bitmap_bin_ref());
    mind_dive.initialize();
@@ -36,7 +37,72 @@ TEST_F(AllegroFlare_Prototypes_MindDive_MindDiveTestWithAllegroRenderingFixture,
    mind_dive.render();
 
    al_flip_display();
-   sleep_for(1);
+}
+
+
+TEST_F(AllegroFlare_Prototypes_MindDive_MindDiveTestWithAllegroRenderingFixture,
+   INTERACTIVE__will_work_as_expected)
+{
+   // setup system
+   al_install_keyboard();
+   al_install_joystick();
+   ALLEGRO_EVENT_QUEUE *event_queue = al_create_event_queue();
+   ALLEGRO_TIMER *primary_timer = al_create_timer(ALLEGRO_BPS_TO_SECS(60));
+   al_register_event_source(event_queue, al_get_keyboard_event_source());
+   al_register_event_source(event_queue, al_get_timer_event_source(primary_timer));
+   //al_register_event_source(event_queue, al_get_timer_event_source(primary_timer));
+   bool abort = false;
+   ALLEGRO_EVENT event;
+
+   // setup environment
+   AllegroFlare::EventEmitter event_emitter;
+   event_emitter.initialize();
+   al_register_event_source(event_queue, &event_emitter.get_event_source_ref());
+
+   // initialize test subject
+   
+   //AllegroFlare::Screens::PauseScreen pause_screen;
+   AllegroFlare::Prototypes::MindDive::MindDive mind_dive;
+   mind_dive.set_bitmap_bin(&get_bitmap_bin_ref());
+   mind_dive.initialize();
+   //pause_screen.set_font_bin(&get_font_bin_ref());
+   //pause_screen.set_event_emitter(&event_emitter);
+
+   // run the interactive test
+   al_start_timer(primary_timer);
+   while(!abort)
+   {
+      al_wait_for_event(event_queue, &event);
+
+      switch(event.type)
+      {
+         case ALLEGRO_EVENT_KEY_DOWN:
+         {
+            switch(event.keyboard.keycode)
+            {
+               case ALLEGRO_KEY_ESCAPE:
+                  abort = true;
+               break;
+            }
+         }
+         break;
+
+         case ALLEGRO_EVENT_TIMER:
+            clear();
+            mind_dive.update();
+            mind_dive.render();
+            al_flip_display();
+         break;
+      }
+
+      //if (event.type == ALLEGRO_EVENT_KEY_CHAR && event.keyboard.keycode == ALLEGRO_KEY_ESCAPE) abort = true;
+   }
+   
+   // teardown
+   // TODO: Audit if this teardown is complete. It may require other calls to destroy resources.
+   al_destroy_event_queue(event_queue);
+   al_destroy_timer(primary_timer);
+   al_uninstall_keyboard();
 }
 
 
