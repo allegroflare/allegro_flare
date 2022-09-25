31c571928c140986374f2687c39829d17a580879
Mark Oates <markoates0@gmail.com>
2022-09-25 19:23:23 -0400

    * Remove AllegroFlare/TileMaps/Basic2D dependency on WickedDemos/TileMaps/Basic2D

--- include/WickedDemos/TileMaps/Basic2D.hpp
+++ include/WickedDemos/TileMaps/Basic2D.hpp
@@ -1,7 +1,9 @@
 #pragma once
 
 
-#include <AllegroFlare/TileMaps/Basic2D.hpp>
+#include <AllegroFlare/TileMaps/PrimMesh.hpp>
+#include <AllegroFlare/TileMaps/PrimMeshAtlas.hpp>
+#include <AllegroFlare/TileMaps/TileMap.hpp>
 #include <Wicked/Entities/Basic2D.hpp>
 
 
@@ -9,9 +11,12 @@ namespace WickedDemos
 {
    namespace TileMaps
    {
-      class Basic2D : public Wicked::Entities::Basic2D, public AllegroFlare::TileMaps::Basic2D
+      class Basic2D : public Wicked::Entities::Basic2D
       {
       private:
+         AllegroFlare::TileMaps::PrimMeshAtlas* tile_atlas;
+         AllegroFlare::TileMaps::PrimMesh* tile_mesh;
+         AllegroFlare::TileMaps::TileMap<int>* collision_tile_mesh;
 
       protected:
 
@@ -20,6 +25,12 @@ namespace WickedDemos
          Basic2D();
          ~Basic2D();
 
+         void set_tile_atlas(AllegroFlare::TileMaps::PrimMeshAtlas* tile_atlas);
+         void set_tile_mesh(AllegroFlare::TileMaps::PrimMesh* tile_mesh);
+         void set_collision_tile_mesh(AllegroFlare::TileMaps::TileMap<int>* collision_tile_mesh);
+         AllegroFlare::TileMaps::PrimMeshAtlas* get_tile_atlas() const;
+         AllegroFlare::TileMaps::PrimMesh* get_tile_mesh() const;
+         AllegroFlare::TileMaps::TileMap<int>* get_collision_tile_mesh() const;
       };
    }
 }
--- src/WickedDemos/TileMaps/Basic2D.cpp
+++ src/WickedDemos/TileMaps/Basic2D.cpp
@@ -13,13 +13,51 @@ namespace TileMaps
 
 Basic2D::Basic2D()
    : Wicked::Entities::Basic2D()
-   , AllegroFlare::TileMaps::Basic2D()
+   , tile_atlas(nullptr)
+   , tile_mesh(nullptr)
+   , collision_tile_mesh(nullptr)
 {
 }
 
 
 Basic2D::~Basic2D()
 {
+}
+
+
+void Basic2D::set_tile_atlas(AllegroFlare::TileMaps::PrimMeshAtlas* tile_atlas)
+{
+   this->tile_atlas = tile_atlas;
+}
+
+
+void Basic2D::set_tile_mesh(AllegroFlare::TileMaps::PrimMesh* tile_mesh)
+{
+   this->tile_mesh = tile_mesh;
+}
+
+
+void Basic2D::set_collision_tile_mesh(AllegroFlare::TileMaps::TileMap<int>* collision_tile_mesh)
+{
+   this->collision_tile_mesh = collision_tile_mesh;
+}
+
+
+AllegroFlare::TileMaps::PrimMeshAtlas* Basic2D::get_tile_atlas() const
+{
+   return tile_atlas;
+}
+
+
+AllegroFlare::TileMaps::PrimMesh* Basic2D::get_tile_mesh() const
+{
+   return tile_mesh;
+}
+
+
+AllegroFlare::TileMaps::TileMap<int>* Basic2D::get_collision_tile_mesh() const
+{
+   return collision_tile_mesh;
 }
 
 
