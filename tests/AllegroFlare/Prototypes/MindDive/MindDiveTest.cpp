501fba975b73177b56f6088d71410f4cbb05edab
Mark Oates <markoates0@gmail.com>
2022-09-28 12:04:49 -0400

    * Disable two interactive tests

--- tests/AllegroFlare/Prototypes/MindDive/MindDiveTest.cpp
+++ tests/AllegroFlare/Prototypes/MindDive/MindDiveTest.cpp
@@ -41,7 +41,8 @@ TEST_F(AllegroFlare_Prototypes_MindDive_MindDiveTestWithAllegroRenderingFixture,
 
 
 TEST_F(AllegroFlare_Prototypes_MindDive_MindDiveTestWithAllegroRenderingFixture,
-   INTERACTIVE__will_work_as_expected)
+   //INTERACTIVE__will_work_as_expected)
+   DISABLED__INTERACTIVE__will_work_as_expected)
 {
    // setup system
    al_install_keyboard();
