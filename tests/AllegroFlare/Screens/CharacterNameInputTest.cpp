aefa0032201b4519985dd85ce4a46ab5efe4ec4e
Mark Oates <markoates0@gmail.com>
2022-09-28 11:53:33 -0400

    * Add copied DISABLED interactive test for CharacterNameInput

--- tests/AllegroFlare/Screens/CharacterNameInputTest.cpp
+++ tests/AllegroFlare/Screens/CharacterNameInputTest.cpp
@@ -1,12 +1,120 @@
-
 #include <gtest/gtest.h>
 
+#define EXPECT_THROW_WITH_MESSAGE(code, raised_exception_type, expected_exception_message) \
+   try { code; FAIL() << "Expected " # raised_exception_type; } \
+   catch ( raised_exception_type const &err ) { EXPECT_EQ(std::string(expected_exception_message), err.what()); } \
+   catch (...) { FAIL() << "Expected " # raised_exception_type; }
+
+#include <AllegroFlare/Testing/WithAllegroRenderingFixture.hpp>
+
+
+class AllegroFlare_Screens_CharacterNameInputTest : public ::testing::Test {};
+class AllegroFlare_Screens_CharacterNameInputTestWithAllegroRenderingFixture
+   : public AllegroFlare::Testing::WithAllegroRenderingFixture
+{};
+
+
 #include <AllegroFlare/Screens/CharacterNameInput.hpp>
+#include <AllegroFlare/EventNames.hpp> // for ALLEGRO_FLARE_EVENT_GAME_EVENT
 
 
-TEST(AllegroFlare_Screens_CharacterNameInputTest, can_be_created_without_blowing_up)
+TEST_F(AllegroFlare_Screens_CharacterNameInputTest, can_be_created_without_blowing_up)
 {
+   AllegroFlare::Screens::CharacterNameInput character_name_input;
+}
+
+
+TEST_F(AllegroFlare_Screens_CharacterNameInputTest, render__before_initialized__raises_an_error)
+{
+   AllegroFlare::Screens::CharacterNameInput character_name_input;
+   std::string expected_error_message =
+      "CharacterNameInput::render: error: guard \"initialized\" not met";
+   EXPECT_THROW_WITH_MESSAGE(character_name_input.render(), std::runtime_error, expected_error_message);
+}
+
+
+TEST_F(AllegroFlare_Screens_CharacterNameInputTestWithAllegroRenderingFixture,
+   initialize__will_not_blow_up)
+{
+   AllegroFlare::EventEmitter event_emitter;
+   event_emitter.initialize();
+   AllegroFlare::Screens::CharacterNameInput character_name_input(&event_emitter, &get_font_bin_ref());
+   character_name_input.initialize();
+}
+
+
+TEST_F(AllegroFlare_Screens_CharacterNameInputTestWithAllegroRenderingFixture,
+   INTERACTIVE__will_work_as_expected)
+{
+   AllegroFlare::EventEmitter event_emitter;
+   event_emitter.initialize();
+   AllegroFlare::Screens::CharacterNameInput character_name_input(&event_emitter, &get_font_bin_ref());
+   character_name_input.initialize();
+}
+
+
+TEST_F(AllegroFlare_Screens_CharacterNameInputTestWithAllegroRenderingFixture,
+   DISABLED__INTERACTIVE__will_work_as_expected)
+{
+   // setup system
+   al_install_keyboard();
+   al_install_joystick();
+   ALLEGRO_EVENT_QUEUE *event_queue = al_create_event_queue();
+   ALLEGRO_TIMER *primary_timer = al_create_timer(ALLEGRO_BPS_TO_SECS(60));
+   al_register_event_source(event_queue, al_get_keyboard_event_source());
+   al_register_event_source(event_queue, al_get_timer_event_source(primary_timer));
+   //al_register_event_source(event_queue, al_get_timer_event_source(primary_timer));
+   bool abort = false;
+   ALLEGRO_EVENT event;
+
+   // setup environment
+   AllegroFlare::EventEmitter event_emitter;
+   event_emitter.initialize();
+   al_register_event_source(event_queue, &event_emitter.get_event_source_ref());
+
+   // initialize test subject
    AllegroFlare::Screens::CharacterNameInput character_name_input_screen;
+   character_name_input_screen.set_font_bin(&get_font_bin_ref());
+   //character_name_input_screen.set_bitmap_bin(&get_bitmap_bin_ref());
+   character_name_input_screen.set_event_emitter(&event_emitter);
+
+   // run the interactive test
+   al_start_timer(primary_timer);
+   while(!abort)
+   {
+      al_wait_for_event(event_queue, &event);
+
+      switch(event.type)
+      {
+         case ALLEGRO_EVENT_KEY_CHAR:
+         {
+            int button_num = 0;
+            //if (event.keyboard.keycode == ALLEGRO_KEY_UP) button_num = AllegroFlare::VirtualControls::get_BUTTON_UP();
+            //if (event.keyboard.keycode == ALLEGRO_KEY_DOWN) button_num = AllegroFlare::VirtualControls::get_BUTTON_DOWN();
+            //if (event.keyboard.keycode == ALLEGRO_KEY_ENTER) button_num = AllegroFlare::VirtualControls::get_BUTTON_A();
+            //if (button_num != 0) .virtual_control_button_down_func(0, button_num, event.keyboard.repeat);
+         }
+         break;
+
+         case ALLEGRO_EVENT_TIMER:
+            al_clear_to_color(ALLEGRO_COLOR{0, 0, 0, 0});
+            character_name_input_screen.primary_timer_func();
+            al_flip_display();
+         break;
+
+         case ALLEGRO_FLARE_EVENT_GAME_EVENT:
+            abort = true;
+         break;
+      }
+
+      if (event.type == ALLEGRO_EVENT_KEY_CHAR && event.keyboard.keycode == ALLEGRO_KEY_ESCAPE) abort = true;
+   }
+   
+   // teardown
+   // TODO: Audit if this teardown is complete. It may require other calls to destroy resources.
+   al_destroy_event_queue(event_queue);
+   al_destroy_timer(primary_timer);
+   al_uninstall_keyboard();
 }
 
 
