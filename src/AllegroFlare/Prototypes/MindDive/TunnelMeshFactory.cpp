385cb133fdff26b3e18228334cfe7aedc7975813
Mark Oates <markoates0@gmail.com>
2022-09-30 20:30:15 -0400

    * Add random placement function to place tiles into TunnelMesh

--- src/AllegroFlare/Prototypes/MindDive/TunnelMeshFactory.cpp
+++ src/AllegroFlare/Prototypes/MindDive/TunnelMeshFactory.cpp
@@ -66,11 +66,13 @@ AllegroFlare::Prototypes::MindDive::TunnelMesh* TunnelMeshFactory::create_random
    }
    AllegroFlare::Prototypes::MindDive::TunnelMesh *result = new AllegroFlare::Prototypes::MindDive::TunnelMesh;
    result->set_bitmap_bin(bitmap_bin);
-   result->set_atlas_configuration("uv-with-decorations-02.png", 50, 50);
+   result->set_atlas_configuration("uv-with-decorations-0x.png", 50, 50);
    result->initialize();
-   result->rescale_tile_dimentions_to(2, 2*2);
+   result->rescale_tile_dimentions_to(2, 6);
    result->resize(12, 32);
-   random_fill_excluding(result, {0, 1, 2, 3});
+   random_fill_from(result, { 2 });
+   random_sparce_placement(result, { { 0, 0 } }, 6);
+   random_sparce_placement(result, { { 1, 1 } }, 20);
    return result;
 }
 
@@ -96,6 +98,26 @@ void TunnelMeshFactory::random_fill_from(AllegroFlare::Prototypes::MindDive::Tun
       {
          int random_int = random.get_random_element(inclusion_list);
          prim_mesh.set_tile_id(x, y, random_int);
+      }
+   return;
+}
+
+void TunnelMeshFactory::random_sparce_placement(AllegroFlare::Prototypes::MindDive::TunnelMesh* tunnel_mesh, std::vector<std::pair<int, int>> inclusion_list, int one_in_chance)
+{
+   if (inclusion_list.empty()) return;
+   AllegroFlare::TileMaps::PrimMesh &prim_mesh = tunnel_mesh->get_prim_mesh_ref();
+   AllegroFlare::TileMaps::TileMap<int> &collision_tile_map = tunnel_mesh->get_collision_tile_map_ref();
+   AllegroFlare::Random random;
+   for (int y=0; y<prim_mesh.get_num_rows(); y++)
+      for (int x=0; x<prim_mesh.get_num_columns(); x++)
+      {
+         bool will_place = random.get_one_in_chance(one_in_chance);
+         if (will_place)
+         {
+            std::pair<int, int> random_element = random.get_random_element(inclusion_list);
+            prim_mesh.set_tile_id(x, y, random_element.first);
+            collision_tile_map.set_tile(x, y, random_element.second);
+         }
       }
    return;
 }
