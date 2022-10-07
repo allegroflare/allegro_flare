bbad0cf5f5b74aab1bd3f11c528e83a30563a9a9
Mark Oates <markoates0@gmail.com>
2022-10-07 09:35:58 -0400

    * Add test for get_sample_at respecting head_position

--- tests/AllegroFlare/AudioDataBlockTest.cpp
+++ tests/AllegroFlare/AudioDataBlockTest.cpp
@@ -109,3 +109,16 @@ TEST(AllegroFlare_AudioDataBlockTest,
 }
 
 
+TEST(AllegroFlare_AudioDataBlockTest,
+   get_sample_at__will_return_the_sample_at_the_position_respecting_the_sample_head_position)
+{
+   AllegroFlare::AudioDataBlock audio_data_block(4);
+   audio_data_block.initialize();
+   audio_data_block.get_block_ref() = { 0.0f, 0.0f,   0.0f, 0.0f,   0.0f, 0.0f,   0.4f, 0.5f, };
+   audio_data_block.move_sample_head_position_by(2);
+   std::pair<float, float> expected_sample = { 0.4f, 0.5f };
+   std::pair<float, float> actual_sample = audio_data_block.get_sample_at(1);
+   EXPECT_EQ(expected_sample, actual_sample);
+}
+
+
