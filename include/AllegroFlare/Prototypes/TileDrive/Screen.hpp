f9a3ec64e7996d99fc4b990440bcdaaa5095a90f
Mark Oates <markoates0@gmail.com>
2022-10-11 10:30:51 -0400

    * Add basic scaffold for TileDrive/Screen

--- include/AllegroFlare/Prototypes/TileDrive/Screen.hpp
+++ include/AllegroFlare/Prototypes/TileDrive/Screen.hpp
@@ -1,6 +1,13 @@
 #pragma once
 
 
+#include <AllegroFlare/BitmapBin.hpp>
+#include <AllegroFlare/EventEmitter.hpp>
+#include <AllegroFlare/FontBin.hpp>
+#include <AllegroFlare/Prototypes/TileDrive/TileDrive.hpp>
+#include <AllegroFlare/SampleBin.hpp>
+#include <AllegroFlare/Screens/Base.hpp>
+#include <allegro5/allegro.h>
 
 
 namespace AllegroFlare
@@ -9,17 +16,34 @@ namespace AllegroFlare
    {
       namespace TileDrive
       {
-         class Screen
+         class Screen : public AllegroFlare::Screens::Base
          {
          private:
+            AllegroFlare::EventEmitter* event_emitter;
+            AllegroFlare::BitmapBin* bitmap_bin;
+            AllegroFlare::FontBin* font_bin;
+            AllegroFlare::SampleBin* sample_bin;
+            AllegroFlare::Prototypes::TileDrive::TileDrive tile_drive;
+            bool initialized;
 
          protected:
 
 
          public:
-            Screen();
-            ~Screen();
+            Screen(AllegroFlare::EventEmitter* event_emitter=nullptr, AllegroFlare::BitmapBin* bitmap_bin=nullptr, AllegroFlare::FontBin* font_bin=nullptr, AllegroFlare::SampleBin* sample_bin=nullptr);
+            virtual ~Screen();
 
+            void set_event_emitter(AllegroFlare::EventEmitter* event_emitter);
+            AllegroFlare::EventEmitter* get_event_emitter() const;
+            AllegroFlare::BitmapBin* get_bitmap_bin() const;
+            AllegroFlare::FontBin* get_font_bin() const;
+            AllegroFlare::SampleBin* get_sample_bin() const;
+            void set_bitmap_bin(AllegroFlare::BitmapBin* bitmap_bin=nullptr);
+            void set_sample_bin(AllegroFlare::SampleBin* sample_bin=nullptr);
+            void set_font_bin(AllegroFlare::FontBin* font_bin=nullptr);
+            void initialize();
+            virtual void key_down_func(ALLEGRO_EVENT* event=nullptr) override;
+            virtual void key_up_func(ALLEGRO_EVENT* event=nullptr) override;
          };
       }
    }
--- src/AllegroFlare/Prototypes/TileDrive/Screen.cpp
+++ src/AllegroFlare/Prototypes/TileDrive/Screen.cpp
@@ -2,7 +2,8 @@
 
 #include <AllegroFlare/Prototypes/TileDrive/Screen.hpp>
 
-
+#include <sstream>
+#include <stdexcept>
 
 
 namespace AllegroFlare
@@ -13,7 +14,14 @@ namespace TileDrive
 {
 
 
-Screen::Screen()
+Screen::Screen(AllegroFlare::EventEmitter* event_emitter, AllegroFlare::BitmapBin* bitmap_bin, AllegroFlare::FontBin* font_bin, AllegroFlare::SampleBin* sample_bin)
+   : AllegroFlare::Screens::Base("Prototypes::TileDrive::Screen")
+   , event_emitter(event_emitter)
+   , bitmap_bin(bitmap_bin)
+   , font_bin(font_bin)
+   , sample_bin(sample_bin)
+   , tile_drive()
+   , initialized(false)
 {
 }
 
@@ -23,6 +31,157 @@ Screen::~Screen()
 }
 
 
+void Screen::set_event_emitter(AllegroFlare::EventEmitter* event_emitter)
+{
+   this->event_emitter = event_emitter;
+}
+
+
+AllegroFlare::EventEmitter* Screen::get_event_emitter() const
+{
+   return event_emitter;
+}
+
+
+AllegroFlare::BitmapBin* Screen::get_bitmap_bin() const
+{
+   return bitmap_bin;
+}
+
+
+AllegroFlare::FontBin* Screen::get_font_bin() const
+{
+   return font_bin;
+}
+
+
+AllegroFlare::SampleBin* Screen::get_sample_bin() const
+{
+   return sample_bin;
+}
+
+
+void Screen::set_bitmap_bin(AllegroFlare::BitmapBin* bitmap_bin)
+{
+   if (!((!initialized)))
+   {
+      std::stringstream error_message;
+      error_message << "Screen" << "::" << "set_bitmap_bin" << ": error: " << "guard \"(!initialized)\" not met";
+      throw std::runtime_error(error_message.str());
+   }
+   this->bitmap_bin = bitmap_bin;
+   return;
+}
+
+void Screen::set_sample_bin(AllegroFlare::SampleBin* sample_bin)
+{
+   if (!((!initialized)))
+   {
+      std::stringstream error_message;
+      error_message << "Screen" << "::" << "set_sample_bin" << ": error: " << "guard \"(!initialized)\" not met";
+      throw std::runtime_error(error_message.str());
+   }
+   this->sample_bin = sample_bin;
+   return;
+}
+
+void Screen::set_font_bin(AllegroFlare::FontBin* font_bin)
+{
+   if (!((!initialized)))
+   {
+      std::stringstream error_message;
+      error_message << "Screen" << "::" << "set_font_bin" << ": error: " << "guard \"(!initialized)\" not met";
+      throw std::runtime_error(error_message.str());
+   }
+   this->font_bin = font_bin;
+   return;
+}
+
+void Screen::initialize()
+{
+   if (!((!initialized)))
+   {
+      std::stringstream error_message;
+      error_message << "Screen" << "::" << "initialize" << ": error: " << "guard \"(!initialized)\" not met";
+      throw std::runtime_error(error_message.str());
+   }
+   initialized = true;
+   return;
+}
+
+void Screen::key_down_func(ALLEGRO_EVENT* event)
+{
+   if (!(initialized))
+   {
+      std::stringstream error_message;
+      error_message << "Screen" << "::" << "key_down_func" << ": error: " << "guard \"initialized\" not met";
+      throw std::runtime_error(error_message.str());
+   }
+   if (!(event))
+   {
+      std::stringstream error_message;
+      error_message << "Screen" << "::" << "key_down_func" << ": error: " << "guard \"event\" not met";
+      throw std::runtime_error(error_message.str());
+   }
+   switch(event->keyboard.keycode)
+   {
+     case ALLEGRO_KEY_UP:
+       tile_drive.driver_press_accelerator();
+       break;
+
+     case ALLEGRO_KEY_DOWN:
+       tile_drive.driver_press_break();
+       break;
+
+     case ALLEGRO_KEY_RIGHT:
+       tile_drive.driver_turn_right();
+       break;
+
+     case ALLEGRO_KEY_LEFT:
+       tile_drive.driver_turn_left();
+       break;
+
+     case ALLEGRO_KEY_R:
+       tile_drive.reset();
+       break;
+   }
+   return;
+}
+
+void Screen::key_up_func(ALLEGRO_EVENT* event)
+{
+   if (!(initialized))
+   {
+      std::stringstream error_message;
+      error_message << "Screen" << "::" << "key_up_func" << ": error: " << "guard \"initialized\" not met";
+      throw std::runtime_error(error_message.str());
+   }
+   if (!(event))
+   {
+      std::stringstream error_message;
+      error_message << "Screen" << "::" << "key_up_func" << ": error: " << "guard \"event\" not met";
+      throw std::runtime_error(error_message.str());
+   }
+   switch(event->keyboard.keycode)
+   {
+      case ALLEGRO_KEY_UP:
+        tile_drive.driver_unpress_accelerator();
+      break;
+
+      case ALLEGRO_KEY_DOWN:
+        tile_drive.driver_unpress_break();
+      break;
+
+      case ALLEGRO_KEY_RIGHT:
+        tile_drive.driver_turn_none();
+      break;
+
+      case ALLEGRO_KEY_LEFT:
+         tile_drive.driver_turn_none();
+      break;
+   }
+   return;
+}
 
 
 } // namespace TileDrive
