5545b4d04b52bc1a8a0e47607d499428d57a7ef2
Mark Oates <markoates0@gmail.com>
2022-10-17 19:21:51 -0400

    * Rename test subject variables to #step_result#

--- tests/AllegroFlare/Physics/TileMapCollisionStepperStepResultTest.cpp
+++ tests/AllegroFlare/Physics/TileMapCollisionStepperStepResultTest.cpp
@@ -6,7 +6,7 @@
 
 TEST(AllegroFlare_Physics_TileMapCollisionStepperStepResultTest, can_be_created_without_blowing_up)
 {
-   AllegroFlare::Physics::TileMapCollisionStepperStepResult tile_map_collision_stepper_step_result;
+   AllegroFlare::Physics::TileMapCollisionStepperStepResult step_result;
 }
 
 
@@ -14,7 +14,16 @@ TEST(AllegroFlare_Physics_TileMapCollisionStepperStepResultTest,
    subject_is_on_or_was_previously_on_tile_value__will_return_true_if_the_collision_exists_matching_the_description)
    // TODO
 {
-   //AllegroFlare::Physics::TileMapCollisionStepperStepResult tile_map_collision_stepper_step_result;
+   AllegroFlare::Physics::TileMapCollisionStepperStepResult step_result;
+   
+}
+
+
+TEST(AllegroFlare_Physics_TileMapCollisionStepperStepResultTest,
+   subject_entered_tile_value__will_return_true_if_the_subject_had_not_prevously_been_on_a_tile_and_now_is)
+   // TODO
+{
+   //AllegroFlare::Physics::TileMapCollisionStepperStepResult step_result;
 }
 
 
