31ee03057828978a45b7d3788787b4710919da42
Mark Oates <markoates0@gmail.com>
2022-11-19 18:03:39 -0500

    * Set more practical default for set_backfill_opacity event

--- include/AllegroFlare/EventEmitter.hpp
+++ include/AllegroFlare/EventEmitter.hpp
@@ -33,7 +33,7 @@ namespace AllegroFlare
       void emit_hide_input_hints_bar_event();
       void emit_set_input_hints_bar_event(std::vector<std::string> tokens={});
       void emit_set_input_hints_bar_text_opacity_event(float opacity=1.0f);
-      void emit_set_input_hints_bar_backfill_opacity_event(float opacity=1.0f);
+      void emit_set_input_hints_bar_backfill_opacity_event(float opacity=0.35f);
       void emit_set_input_hints_bar_height_event(float height=60.0f);
       void emit_post_unlocked_achievement_notification_event(std::string achievement_name="[unset-achievement_name]");
       void emit_play_sound_effect_event(std::string sound_effect_identifier="[unset-sound-effect-identifier]");
