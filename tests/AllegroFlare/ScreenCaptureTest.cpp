4a411cef5f163f221c79dbad66c52b0155176d7a
Mark Oates <markoates0@gmail.com>
2022-11-19 09:49:15 -0500

    * Add capture test of ScreenCapture

--- tests/AllegroFlare/ScreenCaptureTest.cpp
+++ tests/AllegroFlare/ScreenCaptureTest.cpp
@@ -5,7 +5,7 @@
 
 
 #include <AllegroFlare/Testing/TestNameInference.hpp>
-#include <string>
+#include <allegro5/allegro_image.h>
 
 
 TEST(AllegroFlare_ScreenCaptureTest, can_be_created_without_blowing_up)
@@ -16,13 +16,19 @@ TEST(AllegroFlare_ScreenCaptureTest, can_be_created_without_blowing_up)
 
 TEST(AllegroFlare_ScreenCaptureTest, create_capture__will_create_a_capture_of_the_screen)
 {
-   //std::string full_file_save_location = AllegroFlare::Testing::TestNameInference::build_test_snapshot_full_filename();
+   std::string full_file_save_location = AllegroFlare::Testing::TestNameInference::build_test_snapshot_full_filename();
 
-   //al_init();
-   //ALLEGRO_DISPLAY *display = al_create_display(1920, 1080);
-   //ALLEGRO_BITMAP *created_capture = AllegroFlare::ScreenCapture.capture();
-   //al_save_bitmap(created_capture);
-   //al_destroy_bitmap(created_capture);
+   al_init();
+   al_init_image_addon(); // for al_save_bitmap
+
+   ALLEGRO_DISPLAY *display = al_create_display(1920, 1080);
+   ALLEGRO_BITMAP *created_capture = AllegroFlare::ScreenCapture::create_capture();
+   al_init_image_addon();
+   al_save_bitmap(full_file_save_location.c_str(), created_capture);
+
+   al_destroy_bitmap(created_capture);
+   al_destroy_display(display);
+   al_shutdown_image_addon(); // for al_save_bitmap
 }
 
 
