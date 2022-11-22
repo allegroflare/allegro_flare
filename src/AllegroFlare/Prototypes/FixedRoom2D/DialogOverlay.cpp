2dd2736d34eefebade937524f2ca608209993bf2
Mark Oates <markoates0@gmail.com>
2022-11-22 18:50:12 -0500

    * Add scaffold for FixedRoom2D/DialogOverlay

---
+++ src/AllegroFlare/Prototypes/FixedRoom2D/DialogOverlay.cpp
@@ -0,0 +1,97 @@
+
+
+#include <AllegroFlare/Prototypes/FixedRoom2D/DialogOverlay.hpp>
+
+#include <AllegroFlare/Vec2D.hpp>
+#include <allegro5/allegro_font.h>
+#include <allegro5/allegro_primitives.h>
+#include <sstream>
+#include <stdexcept>
+
+
+namespace AllegroFlare
+{
+namespace Prototypes
+{
+namespace FixedRoom2D
+{
+
+
+DialogOverlay::DialogOverlay(AllegroFlare::FontBin* font_bin, std::string quote)
+   : font_bin(font_bin)
+   , quote(quote)
+{
+}
+
+
+DialogOverlay::~DialogOverlay()
+{
+}
+
+
+void DialogOverlay::render()
+{
+   if (!(al_is_system_installed()))
+   {
+      std::stringstream error_message;
+      error_message << "DialogOverlay" << "::" << "render" << ": error: " << "guard \"al_is_system_installed()\" not met";
+      throw std::runtime_error(error_message.str());
+   }
+   if (!(al_is_primitives_addon_initialized()))
+   {
+      std::stringstream error_message;
+      error_message << "DialogOverlay" << "::" << "render" << ": error: " << "guard \"al_is_primitives_addon_initialized()\" not met";
+      throw std::runtime_error(error_message.str());
+   }
+   if (!(al_is_font_addon_initialized()))
+   {
+      std::stringstream error_message;
+      error_message << "DialogOverlay" << "::" << "render" << ": error: " << "guard \"al_is_font_addon_initialized()\" not met";
+      throw std::runtime_error(error_message.str());
+   }
+   if (!(font_bin))
+   {
+      std::stringstream error_message;
+      error_message << "DialogOverlay" << "::" << "render" << ": error: " << "guard \"font_bin\" not met";
+      throw std::runtime_error(error_message.str());
+   }
+   float x = 1920/2;
+   float y = 1080/3;
+   ALLEGRO_FONT *font = obtain_font();
+   float text_width = al_get_text_width(font, quote.c_str());
+   float text_height = al_get_font_line_height(font);
+   float h_text_width = text_width/2;
+   float h_text_height = text_height/2;
+   AllegroFlare::Vec2D padding = {30, 20};
+
+   al_draw_rounded_rectangle(
+      x-h_text_width - padding.x,
+      y-h_text_height - padding.y,
+      x+h_text_width + padding.x,
+      y+h_text_height + padding.y,
+      8.0f,
+      8.0f,
+      ALLEGRO_COLOR{1, 1, 1, 1},
+      8.0f
+   );
+   al_draw_text(font, ALLEGRO_COLOR{1, 1, 1, 1}, x, y-h_text_height, ALLEGRO_ALIGN_CENTER, quote.c_str());
+   return;
+}
+
+ALLEGRO_FONT* DialogOverlay::obtain_font()
+{
+   if (!(font_bin))
+   {
+      std::stringstream error_message;
+      error_message << "DialogOverlay" << "::" << "obtain_font" << ": error: " << "guard \"font_bin\" not met";
+      throw std::runtime_error(error_message.str());
+   }
+   return font_bin->auto_get("Inter-Medium.ttf -52");
+}
+
+
+} // namespace FixedRoom2D
+} // namespace Prototypes
+} // namespace AllegroFlare
+
+
