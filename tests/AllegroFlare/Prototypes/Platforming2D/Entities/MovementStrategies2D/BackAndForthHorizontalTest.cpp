01e1c807dfc8d1ad697ade286621406f3ba63882
Mark Oates <markoates0@gmail.com>
2022-12-07 09:54:34 -0500

    * Fix some line-too-long errors in BackAndForthHorizontal

--- tests/AllegroFlare/Prototypes/Platforming2D/Entities/MovementStrategies2D/BackAndForthHorizontalTest.cpp
+++ tests/AllegroFlare/Prototypes/Platforming2D/Entities/MovementStrategies2D/BackAndForthHorizontalTest.cpp
@@ -10,13 +10,15 @@
 #include <AllegroFlare/Prototypes/Platforming2D/Entities/MovementStrategies2D/BackAndForthHorizontal.hpp>
 
 
-TEST(AllegroFlare_Prototypes_Platforming2D_Entities_MovementStrategies2D_BackAndForthHorizontalTest, can_be_created_without_blowing_up)
+TEST(AllegroFlare_Prototypes_Platforming2D_Entities_MovementStrategies2D_BackAndForthHorizontalTest,
+   can_be_created_without_blowing_up)
 {
    AllegroFlare::Prototypes::Platforming2D::Entities::MovementStrategies2D::BackAndForthHorizontal strategy;
 }
 
 
-TEST(AllegroFlare_Prototypes_Platforming2D_Entities_MovementStrategies2D_BackAndForthHorizontalTest, update__without_an_entity__throws_an_error)
+TEST(AllegroFlare_Prototypes_Platforming2D_Entities_MovementStrategies2D_BackAndForthHorizontalTest,
+   update__without_an_entity__throws_an_error)
 {
    AllegroFlare::Prototypes::Platforming2D::Entities::MovementStrategies2D::BackAndForthHorizontal strategy;
    std::string expected_error_message = "BackAndForthHorizontal::update: error: guard \"entity\" not met";
@@ -24,10 +26,13 @@ TEST(AllegroFlare_Prototypes_Platforming2D_Entities_MovementStrategies2D_BackAnd
 }
 
 
-TEST(AllegroFlare_Prototypes_Platforming2D_Entities_MovementStrategies2D_BackAndForthHorizontalTest, update__will_not_blow_up)
+TEST(AllegroFlare_Prototypes_Platforming2D_Entities_MovementStrategies2D_BackAndForthHorizontalTest,
+   update__will_not_blow_up)
 {
    AllegroFlare::Prototypes::Platforming2D::Entities::Basic2D basic2d_entity;
-   AllegroFlare::Prototypes::Platforming2D::Entities::MovementStrategies2D::BackAndForthHorizontal strategy(&basic2d_entity);
+   AllegroFlare::Prototypes::Platforming2D::Entities::MovementStrategies2D::BackAndForthHorizontal strategy(
+      &basic2d_entity
+   );
    strategy.update();
 }
 
@@ -37,7 +42,10 @@ TEST(AllegroFlare_Prototypes_Platforming2D_Entities_MovementStrategies2D_BackAnd
 {
    float travel_speed = 7.3f;
    AllegroFlare::Prototypes::Platforming2D::Entities::Basic2D basic2d_entity;
-   AllegroFlare::Prototypes::Platforming2D::Entities::MovementStrategies2D::BackAndForthHorizontal strategy(&basic2d_entity, travel_speed);
+   AllegroFlare::Prototypes::Platforming2D::Entities::MovementStrategies2D::BackAndForthHorizontal strategy(
+      &basic2d_entity,
+      travel_speed
+   );
 
    basic2d_entity.get_velocity_ref().position = AllegroFlare::vec2d(26, -35);
    strategy.update();
@@ -45,7 +53,8 @@ TEST(AllegroFlare_Prototypes_Platforming2D_Entities_MovementStrategies2D_BackAnd
 }
 
 
-TEST(AllegroFlare_Prototypes_Platforming2D_Entities_MovementStrategies2D_BackAndForthHorizontalTest, travel_speed__has_the_expected_default_value)
+TEST(AllegroFlare_Prototypes_Platforming2D_Entities_MovementStrategies2D_BackAndForthHorizontalTest,
+   travel_speed__has_the_expected_default_value)
 {
    AllegroFlare::Prototypes::Platforming2D::Entities::MovementStrategies2D::BackAndForthHorizontal strategy;
    ASSERT_EQ(4.0f, strategy.get_travel_speed());
