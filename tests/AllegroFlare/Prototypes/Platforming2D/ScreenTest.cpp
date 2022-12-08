5d37115cab2c8d1c8cd49ba90ab2b9bdeebb114b
Mark Oates <markoates0@gmail.com>
2022-12-08 17:22:24 -0500

    * Disable interactive test

--- tests/AllegroFlare/Prototypes/Platforming2D/ScreenTest.cpp
+++ tests/AllegroFlare/Prototypes/Platforming2D/ScreenTest.cpp
@@ -23,8 +23,8 @@ TEST(AllegroFlare_Prototypes_Platforming2D_ScreenTest, can_be_created_without_bl
 
 
 TEST(AllegroFlare_Prototypes_Platforming2D_ScreenTest,
-   INTERACTIVE__in_an_AllegroFlare_Frameworks_Full_context__will_run_as_expected)
-   //DISABLED__INTERACTIVE__in_an_AllegroFlare_Frameworks_Full_context__will_run_as_expected)
+   //INTERACTIVE__in_an_AllegroFlare_Frameworks_Full_context__will_run_as_expected)
+   DISABLED__INTERACTIVE__in_an_AllegroFlare_Frameworks_Full_context__will_run_as_expected)
 {
    AllegroFlare::Frameworks::Full framework;
    framework.disable_fullscreen();
