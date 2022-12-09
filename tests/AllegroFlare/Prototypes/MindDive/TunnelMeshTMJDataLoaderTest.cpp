6bf6d3a1565504e81437ccdadd4c3165bc3e8806
Mark Oates <markoates0@gmail.com>
2022-12-08 19:23:46 -0500

    * Set Windows-specific TMJ_FIXTURE_PATH in MindDive/TunnelMeshTMJDataLoader

--- tests/AllegroFlare/Prototypes/MindDive/TunnelMeshTMJDataLoaderTest.cpp
+++ tests/AllegroFlare/Prototypes/MindDive/TunnelMeshTMJDataLoaderTest.cpp
@@ -9,7 +9,12 @@
 
 #include <AllegroFlare/Prototypes/MindDive/TunnelMeshTMJDataLoader.hpp>
 
+// TODO: improve this:
+#if defined(_WIN32) || defined(_WIN64)
+#define TMJ_FIXTURE_PATH "/msys64/home/Mark/Repos/allegro_flare/tests/fixtures/"
+#else
 #define TMJ_FIXTURE_PATH "/Users/markoates/Repos/allegro_flare/tests/fixtures/"
+#endif
 #define TMJ_FIXTURE_WITH_MISSING_COLLISION_LAYER_FILENAME "map1.tmj"
 //#define TMJ_FIXTURE_FILENAME "map1-02.tmj"
 #define TMJ_FIXTURE_FILENAME "tunnel_mesh-02.tmj"
