8c5ef123d0797a7c738f16d3931f8399772319a7
Mark Oates <markoates0@gmail.com>
2022-12-30 13:04:36 -0500

    * Add render tests for SectionRenderers/Text

--- tests/AllegroFlare/Elements/RollingCredits/SectionRenderers/TextTest.cpp
+++ tests/AllegroFlare/Elements/RollingCredits/SectionRenderers/TextTest.cpp
@@ -1,26 +1,61 @@
 
 #include <gtest/gtest.h>
 
+#define ASSERT_THROW_WITH_MESSAGE(code, raised_exception_type, expected_exception_message) \
+   try { code; FAIL() << "Expected " # raised_exception_type; } \
+   catch ( raised_exception_type const &err ) { ASSERT_EQ(std::string(expected_exception_message), err.what()); } \
+   catch (...) { FAIL() << "Expected " # raised_exception_type; }
+
+#include <AllegroFlare/Testing/WithAllegroRenderingFixture.hpp>
+
+
+class AllegroFlare_Elements_RollingCredits_SectionRenderers_TextTest : public ::testing::Test
+{};
+
+class AllegroFlare_Elements_RollingCredits_SectionRenderers_TextTestWithAllegroRenderingFixture
+   : public AllegroFlare::Testing::WithAllegroRenderingFixture
+{};
+
+
 #include <AllegroFlare/Elements/RollingCredits/SectionRenderers/Text.hpp>
 
 
-TEST(AllegroFlare_Elements_RollingCredits_SectionRenderers_TextTest, can_be_created_without_blowing_up)
+
+TEST_F(AllegroFlare_Elements_RollingCredits_SectionRenderers_TextTest, can_be_created_without_blowing_up)
 {
    AllegroFlare::Elements::RollingCredits::SectionRenderers::Text text;
 }
 
 
-TEST(AllegroFlare_Elements_RollingCredits_SectionRenderers_TextTest, TYPE__has_the_expected_value)
+TEST_F(AllegroFlare_Elements_RollingCredits_SectionRenderers_TextTest, TYPE__has_the_expected_value)
 {
    AllegroFlare::Elements::RollingCredits::SectionRenderers::Text text;
    EXPECT_EQ("SectionRenderers/Text", text.get_type());
 }
 
 
-TEST(AllegroFlare_Elements_RollingCredits_SectionRenderers_TextTest, type__has_the_expected_value_matching_TYPE)
+TEST_F(AllegroFlare_Elements_RollingCredits_SectionRenderers_TextTest, type__has_the_expected_value_matching_TYPE)
 {
    AllegroFlare::Elements::RollingCredits::SectionRenderers::Text text;
    EXPECT_EQ(AllegroFlare::Elements::RollingCredits::SectionRenderers::Text::TYPE, text.get_type());
 }
 
+
+TEST_F(AllegroFlare_Elements_RollingCredits_SectionRenderers_TextTest,
+   render__without_allegro_initialized__raises_an_error)
+{
+   AllegroFlare::Elements::RollingCredits::SectionRenderers::Text text_section_renderer;
+   std::string expected_error_message =
+      "Text::render: error: guard \"al_is_system_installed()\" not met";
+   ASSERT_THROW_WITH_MESSAGE(text_section_renderer.render(), std::runtime_error, expected_error_message);
+}
+
+
+TEST_F(AllegroFlare_Elements_RollingCredits_SectionRenderers_TextTestWithAllegroRenderingFixture,
+   render__will_not_blow_up)
+{
+   AllegroFlare::Elements::RollingCredits::SectionRenderers::Text text_section_renderer(&get_font_bin_ref());
+   text_section_renderer.render();
+   SUCCEED();
+}
 
