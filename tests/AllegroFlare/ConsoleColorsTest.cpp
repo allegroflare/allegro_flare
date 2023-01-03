9963f75a0ccd8726bec9ae5eeeb4406b506993e1
Mark Oates <markoates0@gmail.com>
2023-01-03 14:37:13 -0500

    * Add #disclaimer font#; Add #Inter# and #Bravura# fonts to #tests/fixtures/fonts#

--- tests/AllegroFlare/ConsoleColorsTest.cpp
+++ tests/AllegroFlare/ConsoleColorsTest.cpp
@@ -4,9 +4,7 @@
 #include <AllegroFlare/ConsoleColors.hpp>
 
 
-TEST(AllegroFlare_ConsoleColorsTest, can_be_created_without_blowing_up)
-{
-   AllegroFlare::ConsoleColors console_colors;
-}
+// Note the constants are globaly #defined
+// TODO: consider how to test
 
 
