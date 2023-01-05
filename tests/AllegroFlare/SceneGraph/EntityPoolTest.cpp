c3dce34d2cf28bb7057a0d1031b8327a1084d8d4
Mark Oates <markoates0@gmail.com>
2023-01-05 18:24:29 -0500

    * Add test for #select_C#

--- tests/AllegroFlare/SceneGraph/EntityPoolTest.cpp
+++ tests/AllegroFlare/SceneGraph/EntityPoolTest.cpp
@@ -12,10 +12,18 @@ class AllegroFlare_SceneGraph_EntityPoolTestWithEntities : public ::testing::Tes
 public:
    AllegroFlare::SceneGraph::EntityPool entity_pool;
 
-   AllegroFlare::SceneGraph::Entities::Base* create_entity(std::string tag)
+   AllegroFlare::SceneGraph::Entities::Base* create_entity(std::string name, std::string tag)
    {
       AllegroFlare::SceneGraph::Entities::Base* result = new AllegroFlare::SceneGraph::Entities::Base();
+      result->set("name", name);
       result->set(tag);
+      return result;
+   }
+   AllegroFlare::SceneGraph::Entities::Base* create_entity(std::string name, std::string attribute, std::string parameter)
+   {
+      AllegroFlare::SceneGraph::Entities::Base* result = new AllegroFlare::SceneGraph::Entities::Base();
+      result->set("name", name);
+      result->set(attribute, parameter);
       return result;
    }
 };
@@ -32,15 +40,33 @@ TEST_F(AllegroFlare_SceneGraph_EntityPoolTestWithEntities, select_A__will_select
 {
    using namespace AllegroFlare::SceneGraph::Entities;
 
-   Base* duck_entity = create_entity("duck");
-   Base* tree_entity1 = create_entity("tree");
-   Base* tree_entity2 = create_entity("tree");
-   Base* piece_of_sky_entity = create_entity("piece_of_sky");
+   Base* duck_entity = create_entity("duck_entity", "duck");
+   Base* tree_entity1 = create_entity("tree_entity1", "tree");
+   Base* tree_entity2 = create_entity("tree_entity2", "tree");
+   Base* piece_of_sky_entity = create_entity("piece_of_sky_entity", "piece_of_sky");
 
    entity_pool.add({ duck_entity, tree_entity1, tree_entity2, piece_of_sky_entity });
 
    std::vector<Base*> expected = { tree_entity1, tree_entity2 };
    std::vector<Base*> actual = entity_pool.select_A("tree");
+
+   EXPECT_THAT(expected, testing::UnorderedElementsAreArray(actual));
+}
+
+
+TEST_F(AllegroFlare_SceneGraph_EntityPoolTestWithEntities, select_C__will_select_entities_with_the_attribute)
+{
+   using namespace AllegroFlare::SceneGraph::Entities;
+
+   Base* entity1 = create_entity("entity1", "on_map", "map-a");
+   Base* entity2 = create_entity("entity2", "on_map", "map-b");
+   Base* entity3 = create_entity("entity3", "on_map", "map-a");
+   Base* entity4 = create_entity("entity4", "on_map", "map-b");
+
+   entity_pool.add({ entity1, entity2, entity3, entity4, });
+
+   std::vector<Base*> expected = { entity1, entity3 };
+   std::vector<Base*> actual = entity_pool.select_C("on_map", "map-a");
 
    EXPECT_THAT(expected, testing::UnorderedElementsAreArray(actual));
 }
