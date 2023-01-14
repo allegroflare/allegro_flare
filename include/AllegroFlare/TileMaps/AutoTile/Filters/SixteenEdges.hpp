a926aea92f07424e2a05004cd54dcee22868e238
Mark Oates <markoates0@gmail.com>
2023-01-14 12:15:17 -0500

    * Setup scaffolds and basic constants in Filters/SixteenEdges

--- include/AllegroFlare/TileMaps/AutoTile/Filters/SixteenEdges.hpp
+++ include/AllegroFlare/TileMaps/AutoTile/Filters/SixteenEdges.hpp
@@ -2,6 +2,8 @@
 
 
 #include <AllegroFlare/TileMaps/AutoTile/Filters/Base.hpp>
+#include <cstdint>
+#include <map>
 #include <vector>
 
 
@@ -16,22 +18,44 @@ namespace AllegroFlare
             class SixteenEdges : public AllegroFlare::TileMaps::AutoTile::Filters::Base
             {
             public:
+               static constexpr uint32_t UNDEF = 0;
+               static constexpr uint32_t TOP_LEFT = 1;
+               static constexpr uint32_t TOP = 2;
+               static constexpr uint32_t TOP_RIGHT = 3;
+               static constexpr uint32_t TOP_TIP = 4;
+               static constexpr uint32_t LEFT = 5;
+               static constexpr uint32_t FULL = 6;
+               static constexpr uint32_t RIGHT = 7;
+               static constexpr uint32_t CENTER = 8;
+               static constexpr uint32_t BOTTOM_LEFT = 9;
+               static constexpr uint32_t BOTTOM = 10;
+               static constexpr uint32_t BOTTOM_RIGHT = 11;
+               static constexpr uint32_t BOTTOM_TIP = 12;
+               static constexpr uint32_t LEFT_TIP = 13;
+               static constexpr uint32_t MIDDLE = 14;
+               static constexpr uint32_t RIGHT_TIP = 15;
+               static constexpr uint32_t ISOLATED = 16;
                static constexpr char* TYPE = (char*)"Filters/SixteenEdges";
 
             private:
                int floor_tile_value;
+               std::map<uint32_t, int> sixteen_edges_tiles_definition;
 
             protected:
 
 
             public:
-               SixteenEdges(int floor_tile_value=0);
+               SixteenEdges(int floor_tile_value=0, std::map<uint32_t, int> sixteen_edges_tiles_definition=build_default_sixteen_edges_tiles_definition());
                virtual ~SixteenEdges();
 
                void set_floor_tile_value(int floor_tile_value);
+               void set_sixteen_edges_tiles_definition(std::map<uint32_t, int> sixteen_edges_tiles_definition);
                int get_floor_tile_value() const;
+               std::map<uint32_t, int> get_sixteen_edges_tiles_definition() const;
                virtual bool process() override;
                bool matrix_matches(std::vector<std::vector<int>> match_matrix={}, int x=0, int y=0);
+               static int tile_coord_to_contiguous(int tile_x=0, int tile_y=0, int tile_atlas_num_columns=0);
+               static std::map<uint32_t, int> build_default_sixteen_edges_tiles_definition();
             };
          }
       }
--- src/AllegroFlare/TileMaps/AutoTile/Filters/SixteenEdges.cpp
+++ src/AllegroFlare/TileMaps/AutoTile/Filters/SixteenEdges.cpp
@@ -17,9 +17,10 @@ namespace Filters
 {
 
 
-SixteenEdges::SixteenEdges(int floor_tile_value)
+SixteenEdges::SixteenEdges(int floor_tile_value, std::map<uint32_t, int> sixteen_edges_tiles_definition)
    : AllegroFlare::TileMaps::AutoTile::Filters::Base(AllegroFlare::TileMaps::AutoTile::Filters::SixteenEdges::TYPE)
    , floor_tile_value(floor_tile_value)
+   , sixteen_edges_tiles_definition(sixteen_edges_tiles_definition)
 {
 }
 
@@ -35,9 +36,21 @@ void SixteenEdges::set_floor_tile_value(int floor_tile_value)
 }
 
 
+void SixteenEdges::set_sixteen_edges_tiles_definition(std::map<uint32_t, int> sixteen_edges_tiles_definition)
+{
+   this->sixteen_edges_tiles_definition = sixteen_edges_tiles_definition;
+}
+
+
 int SixteenEdges::get_floor_tile_value() const
 {
    return floor_tile_value;
+}
+
+
+std::map<uint32_t, int> SixteenEdges::get_sixteen_edges_tiles_definition() const
+{
+   return sixteen_edges_tiles_definition;
 }
 
 
@@ -88,6 +101,47 @@ bool SixteenEdges::matrix_matches(std::vector<std::vector<int>> match_matrix, in
          input_matrix.tile_matches(x, y,   match_matrix[0][0]) // WARNING: this is hard-coded (0, 0)
       && input_matrix.tile_matches(x, y+1, match_matrix[1][0]) // WARNING: this is hard-coded (0, 0)
    );
+}
+
+int SixteenEdges::tile_coord_to_contiguous(int tile_x, int tile_y, int tile_atlas_num_columns)
+{
+   if (!((tile_atlas_num_columns > 0)))
+   {
+      std::stringstream error_message;
+      error_message << "[SixteenEdges::tile_coord_to_contiguous]: error: guard \"(tile_atlas_num_columns > 0)\" not met.";
+      std::cerr << "\033[1;31m" << error_message.str() << " An exception will be thrown to halt the program.\033[0m" << std::endl;
+      throw std::runtime_error("SixteenEdges::tile_coord_to_contiguous: error: guard \"(tile_atlas_num_columns > 0)\" not met");
+   }
+   // TODO: this function
+   return 0;
+}
+
+std::map<uint32_t, int> SixteenEdges::build_default_sixteen_edges_tiles_definition()
+{
+   //auto &bitmap = tile_coord_to_contiguous;
+   auto bitmap = 0;
+   auto c = 8; // num_columns_in_tilemap
+
+   std::map<uint32_t, int> result = {
+      { UNDEF,    bitmap },
+      { TOP_LEFT, bitmap },
+      { TOP,      bitmap },
+      { TOP_RIGHT, bitmap },
+      { TOP_TIP, bitmap },
+      { LEFT, bitmap },
+      { FULL, bitmap },
+      { RIGHT, bitmap },
+      { CENTER, bitmap },
+      { BOTTOM_LEFT, bitmap },
+      { BOTTOM, bitmap },
+      { BOTTOM_RIGHT, bitmap },
+      { BOTTOM_TIP, bitmap },
+      { LEFT_TIP, bitmap },
+      { MIDDLE, bitmap },
+      { RIGHT_TIP, bitmap },
+      { ISOLATED, bitmap },
+   };
+   return result;
 }
 
 
