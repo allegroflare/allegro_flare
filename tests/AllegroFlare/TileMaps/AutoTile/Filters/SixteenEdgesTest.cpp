98fe4c968e8f795e6c9bf9d615bb231ff68429e9
Mark Oates <markoates0@gmail.com>
2023-01-14 11:41:09 -0500

    * Modify Filters/SixteenEdges test to use fixtures (Add unused WithAllegroRendering fixture)

--- tests/AllegroFlare/TileMaps/AutoTile/Filters/SixteenEdgesTest.cpp
+++ tests/AllegroFlare/TileMaps/AutoTile/Filters/SixteenEdgesTest.cpp
@@ -3,49 +3,56 @@
 
 #include <AllegroFlare/TileMaps/AutoTile/Filters/SixteenEdges.hpp>
 
+#include <AllegroFlare/Testing/WithAllegroRenderingFixture.hpp>
 
-TEST(AllegroFlare_TileMaps_AutoTile_Filters_SixteenEdgesTest, can_be_created_without_blowing_up)
+class AllegroFlare_TileMaps_AutoTile_Filters_SixteenEdgesTest : public ::testing::Test {};
+class AllegroFlare_TileMaps_AutoTile_Filters_SixteenEdgesTestWithAllegroRenderingFixture
+   : public AllegroFlare::Testing::WithAllegroRenderingFixture {};
+
+
+
+TEST_F(AllegroFlare_TileMaps_AutoTile_Filters_SixteenEdgesTest, can_be_created_without_blowing_up)
 {
-   AllegroFlare::TileMaps::AutoTile::Filters::SixteenEdges basic;
+   AllegroFlare::TileMaps::AutoTile::Filters::SixteenEdges filter;
 }
 
 
-TEST(AllegroFlare_TileMaps_AutoTile_Filters_SixteenEdgesTest, TYPE__has_the_expected_value)
+TEST_F(AllegroFlare_TileMaps_AutoTile_Filters_SixteenEdgesTest, TYPE__has_the_expected_value)
 {
-   AllegroFlare::TileMaps::AutoTile::Filters::SixteenEdges basic;
-   EXPECT_EQ("Filters/SixteenEdges", basic.get_type());
+   AllegroFlare::TileMaps::AutoTile::Filters::SixteenEdges filter;
+   EXPECT_EQ("Filters/SixteenEdges", filter.get_type());
 }
 
 
-TEST(AllegroFlare_TileMaps_AutoTile_Filters_SixteenEdgesTest, type__has_the_expected_value_matching_TYPE)
+TEST_F(AllegroFlare_TileMaps_AutoTile_Filters_SixteenEdgesTest, type__has_the_expected_value_matching_TYPE)
 {
-   AllegroFlare::TileMaps::AutoTile::Filters::SixteenEdges basic;
-   EXPECT_EQ(AllegroFlare::TileMaps::AutoTile::Filters::SixteenEdges::TYPE, basic.get_type());
+   AllegroFlare::TileMaps::AutoTile::Filters::SixteenEdges filter;
+   EXPECT_EQ(AllegroFlare::TileMaps::AutoTile::Filters::SixteenEdges::TYPE, filter.get_type());
 }
 
 
-TEST(AllegroFlare_TileMaps_AutoTile_Filters_SixteenEdgesTest,
+TEST_F(AllegroFlare_TileMaps_AutoTile_Filters_SixteenEdgesTest,
    result_matrix__after_process__will_have_the_same_width_and_height_as_the_input_matrix)
 {
-   AllegroFlare::TileMaps::AutoTile::Filters::SixteenEdges basic_filter;
+   AllegroFlare::TileMaps::AutoTile::Filters::SixteenEdges filter;
    AllegroFlare::TileMaps::AutoTile::FilterMatrix matrix;
    matrix.resize(7, 9);
-   basic_filter.set_input_matrix(matrix);
+   filter.set_input_matrix(matrix);
 
-   EXPECT_EQ(true, basic_filter.process());
+   EXPECT_EQ(true, filter.process());
 
-   auto result_matrix = basic_filter.get_result_matrix();
+   auto result_matrix = filter.get_result_matrix();
 
    EXPECT_EQ(matrix.get_width(), result_matrix.get_width());
    EXPECT_EQ(matrix.get_height(), result_matrix.get_height());
 }
 
 
-TEST(AllegroFlare_TileMaps_AutoTile_Filters_SixteenEdgesTest,
+TEST_F(AllegroFlare_TileMaps_AutoTile_Filters_SixteenEdgesTest,
    result_matrix__after_process__will_have_each_value_in_the_result_matrix_filled_with_tile_value)
 {
-   AllegroFlare::TileMaps::AutoTile::Filters::SixteenEdges basic_filter(9);
-   basic_filter.set_input_matrix(
+   AllegroFlare::TileMaps::AutoTile::Filters::SixteenEdges filter(9);
+   filter.set_input_matrix(
       AllegroFlare::TileMaps::AutoTile::FilterMatrix::build({
          { 0, 0, 0, 0 },
          { 0, 1, 1, 1 },
@@ -53,9 +60,9 @@ TEST(AllegroFlare_TileMaps_AutoTile_Filters_SixteenEdgesTest,
       })
    );
 
-   EXPECT_EQ(true, basic_filter.process());
+   EXPECT_EQ(true, filter.process());
 
-   auto result_matrix = basic_filter.get_result_matrix();
+   auto result_matrix = filter.get_result_matrix();
 
    std::vector<std::vector<int>> expected_matrix = {
       { 0, 0, 0, 0 },
