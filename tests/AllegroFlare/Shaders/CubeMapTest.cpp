6f6f72a2c3ec19ceaaa4e45b15fda70624873f80
Mark Oates <markoates0@gmail.com>
2023-01-19 09:50:06 -0500

    * Setup renering fixture in Shaders/CubeMap

--- tests/AllegroFlare/Shaders/CubeMapTest.cpp
+++ tests/AllegroFlare/Shaders/CubeMapTest.cpp
@@ -3,24 +3,39 @@
 
 #include <AllegroFlare/Shaders/CubeMap.hpp>
 
+#include <AllegroFlare/Testing/WithAllegroRenderingFixture.hpp>
 
-TEST(AllegroFlare_Shaders_CubeMapTest, can_be_created_without_blowing_up)
+
+class AllegroFlare_Shaders_CubeMapTest: public ::testing::Test {};
+class AllegroFlare_Shaders_CubeMapWithAllegroRenderingFixtureTest :
+   public AllegroFlare::Testing::WithAllegroRenderingFixture
+{};
+
+
+TEST_F(AllegroFlare_Shaders_CubeMapTest, can_be_created_without_blowing_up)
 {
    AllegroFlare::Shaders::CubeMap cube_map;
 }
 
 
-TEST(AllegroFlare_Shaders_CubeMapTest, TYPE__has_the_expected_value)
+TEST_F(AllegroFlare_Shaders_CubeMapTest, TYPE__has_the_expected_value)
 {
    AllegroFlare::Shaders::CubeMap cube_map;
    EXPECT_EQ("AllegroFlare/Shaders/CubeMap", cube_map.get_type());
 }
 
 
-TEST(AllegroFlare_Shaders_CubeMapTest, type__has_the_expected_value_matching_TYPE)
+TEST_F(AllegroFlare_Shaders_CubeMapTest, type__has_the_expected_value_matching_TYPE)
 {
    AllegroFlare::Shaders::CubeMap cube_map;
    EXPECT_EQ(AllegroFlare::Shaders::CubeMap::TYPE, cube_map.get_type());
+}
+
+
+TEST_F(AllegroFlare_Shaders_CubeMapWithAllegroRenderingFixtureTest, initialize__will_not_blow_up)
+{
+   AllegroFlare::Shaders::CubeMap cube_map;
+   cube_map.initialize();
 }
 
 
