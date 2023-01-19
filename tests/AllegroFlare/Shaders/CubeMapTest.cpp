0a6615c439ede9ca7aa95b27f25186ee8896d07b
Mark Oates <markoates0@gmail.com>
2023-01-19 09:59:15 -0500

    * Setup object space in Shaders/CubeMap test

--- tests/AllegroFlare/Shaders/CubeMapTest.cpp
+++ tests/AllegroFlare/Shaders/CubeMapTest.cpp
@@ -4,6 +4,7 @@
 #include <AllegroFlare/Shaders/CubeMap.hpp>
 
 #include <AllegroFlare/Testing/WithAllegroRenderingFixture.hpp>
+#include <AllegroFlare/Camera3D.hpp>
 
 
 class AllegroFlare_Shaders_CubeMapTest: public ::testing::Test {};
@@ -14,28 +15,51 @@ class AllegroFlare_Shaders_CubeMapWithAllegroRenderingFixtureTest :
 
 TEST_F(AllegroFlare_Shaders_CubeMapTest, can_be_created_without_blowing_up)
 {
-   AllegroFlare::Shaders::CubeMap cube_map;
+   AllegroFlare::Shaders::CubeMap shader;
 }
 
 
 TEST_F(AllegroFlare_Shaders_CubeMapTest, TYPE__has_the_expected_value)
 {
-   AllegroFlare::Shaders::CubeMap cube_map;
-   EXPECT_EQ("AllegroFlare/Shaders/CubeMap", cube_map.get_type());
+   AllegroFlare::Shaders::CubeMap shader;
+   EXPECT_EQ("AllegroFlare/Shaders/CubeMap", shader.get_type());
 }
 
 
 TEST_F(AllegroFlare_Shaders_CubeMapTest, type__has_the_expected_value_matching_TYPE)
 {
-   AllegroFlare::Shaders::CubeMap cube_map;
-   EXPECT_EQ(AllegroFlare::Shaders::CubeMap::TYPE, cube_map.get_type());
+   AllegroFlare::Shaders::CubeMap shader;
+   EXPECT_EQ(AllegroFlare::Shaders::CubeMap::TYPE, shader.get_type());
 }
 
 
 TEST_F(AllegroFlare_Shaders_CubeMapWithAllegroRenderingFixtureTest, initialize__will_not_blow_up)
 {
-   AllegroFlare::Shaders::CubeMap cube_map;
-   cube_map.initialize();
+   AllegroFlare::Shaders::CubeMap shader;
+   shader.initialize();
+}
+
+
+TEST_F(AllegroFlare_Shaders_CubeMapWithAllegroRenderingFixtureTest, VISUAL__will_appear_as_expected)
+{
+   AllegroFlare::Shaders::CubeMap shader;
+   AllegroFlare::Camera3D camera;
+
+   // TODO: setup object
+
+   shader.initialize();
+   shader.activate();
+
+   int passes = 60;
+   for (int i=0; i<passes; i++)
+   {
+      camera.setup_projection_on(get_display_backbuffer());
+      clear();
+      // TODO: render object
+      al_flip_display();
+   }
+
+   shader.deactivate();
 }
 
 
