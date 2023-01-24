e752a3ea35d78fe6ce21390ececa6c084dd41723
Mark Oates <markoates0@gmail.com>
2023-01-24 13:13:13 -0500

    * Add test for #is_a_display_surface# when the surface is a sub-bitmap of the display backbuffer

--- tests/AllegroFlare/RenderSurfaces/BaseTest.cpp
+++ tests/AllegroFlare/RenderSurfaces/BaseTest.cpp
@@ -34,6 +34,31 @@ public:
 };
 
 
+class RenderSurfacesBase_DisplaySubBitmap_TestClass : public AllegroFlare::RenderSurfaces::Base
+{
+private:
+   ALLEGRO_BITMAP *surface;
+   bool initialized;
+public:
+   RenderSurfacesBase_DisplaySubBitmap_TestClass()
+      : AllegroFlare::RenderSurfaces::Base("RenderSurfacesBase_DisplaySubBitmap_TestClass")
+      , surface(nullptr)
+      , initialized(false)
+   {}
+   ~RenderSurfacesBase_DisplaySubBitmap_TestClass() {};
+
+   void initialize()
+   {
+      ALLEGRO_BITMAP *backbuffer = al_get_backbuffer(al_get_current_display());
+      surface = al_create_sub_bitmap(backbuffer, 0, 0, al_get_bitmap_width(backbuffer), al_get_bitmap_height(backbuffer));
+   }
+   virtual bool set_as_target() override { return true; }
+   virtual ALLEGRO_BITMAP* obtain_surface() override { return surface; }
+   virtual int get_width() override { return 0; }
+   virtual int get_height() override { return 0; }
+};
+
+
 TEST(AllegroFlare_RenderSurfaces_BaseTest, derived_classes_can_be_created_without_blowing_up)
 {
    RenderSurfacesBaseTestClass test_class;
@@ -55,6 +80,20 @@ TEST(AllegroFlare_RenderSurfaces_BaseTest,
 
    RenderSurfacesBase_Display_TestClass display_test_class;
    EXPECT_EQ(true, display_test_class.is_a_display_surface());
+
+   al_destroy_display(display);
+   al_uninstall_system();
+}
+
+
+TEST(AllegroFlare_RenderSurfaces_BaseTest,
+   is_a_display_surface__when_the_surface_is_a_sub_bitmap_of_a_display_backbuffer__will_return_true)
+{
+   al_init();
+   ALLEGRO_DISPLAY *display = al_create_display(1920, 1080);
+
+   RenderSurfacesBase_DisplaySubBitmap_TestClass display_sub_bitmap_test_class;
+   EXPECT_EQ(true, display_sub_bitmap_test_class.is_a_display_surface());
 
    al_destroy_display(display);
    al_uninstall_system();
