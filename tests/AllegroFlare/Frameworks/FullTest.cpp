5f70a5a407313d2074695afd8e5063a8fbd1e6df
Mark Oates <markoates0@gmail.com>
2023-01-25 09:40:53 -0500

    * Remove unused EXPECT_THROW_WITH_MESSAGE macro in Frameworks/Full test

--- tests/AllegroFlare/Frameworks/FullTest.cpp
+++ tests/AllegroFlare/Frameworks/FullTest.cpp
@@ -2,11 +2,6 @@
 
 #include <gtest/gtest.h>
 
-#define EXPECT_THROW_WITH_MESSAGE(code, raised_exception_type, raised_exception_message) \
-   try { code; FAIL() << "Expected " # raised_exception_type; } \
-   catch ( raised_exception_type const &err ) { EXPECT_EQ(err.what(), std::string( raised_exception_message )); } \
-   catch (...) { FAIL() << "Expected " # raised_exception_type; }
-
 #include <AllegroFlare/Frameworks/Full.hpp>
 
 #include <AllegroFlare/Elements/Notifications/AchievementUnlocked.hpp>
