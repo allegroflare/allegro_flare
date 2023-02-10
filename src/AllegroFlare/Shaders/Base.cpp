eca1f29bb9206f7a09c3d4a50404da0e585e0663
Mark Oates <markoates0@gmail.com>
2023-02-10 15:14:55 -0500

    * Add some TODO comments about removing lines

--- src/AllegroFlare/Shaders/Base.cpp
+++ src/AllegroFlare/Shaders/Base.cpp
@@ -65,6 +65,7 @@ void Base::hotload(std::string vertex_source_code, std::string fragment_source_c
    this->fragment_source_code = fragment_source_code;
 
    // set our base copy of the code
+   // TODO: after flattening AllegroFlare::Shader into AllegroFlare::Shaders::Base, this line should be removed
    AllegroFlare::Shader::vertex_source_code = vertex_source_code;
    AllegroFlare::Shader::fragment_source_code = fragment_source_code;
 
