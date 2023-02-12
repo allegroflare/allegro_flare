a901796dbe7b0904498c887edb5711567cda8af8
Mark Oates <markoates0@gmail.com>
2023-02-12 14:00:46 -0500

    Update depreciation guard for Apple so that it will work with Windows

--- include/AllegroFlare/Cubemap.hpp
+++ include/AllegroFlare/Cubemap.hpp
@@ -4,10 +4,13 @@
 #ifdef __APPLE__
 // TODO: Fix or improve this depreciation declaration here
 #define GL_SILENCE_DEPRECATION
-#include <allegro5/allegro_opengl.h>
-#undef GL_SILENCE_DEPRECATION
 #endif
 
+#include<allegro5/allegro_opengl.h>
+
+#ifdef __APPLE__
+#undef GL_SILENCE_DEPRECATION
+#endif
 
 namespace AllegroFlare
 {
