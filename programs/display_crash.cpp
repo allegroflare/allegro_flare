178cf2a8c5a0d3162132e5a496dad39ab72f2235
Mark Oates <markoates0@gmail.com>
2023-03-01 09:26:29 -0500

    Tweak test to include a try>catch block just in case (doesn't seem to catch the error tho)

--- programs/display_crash.cpp
+++ programs/display_crash.cpp
@@ -5,13 +5,21 @@
 int main(int argc, char** argv)
 {
    al_init();
+   ALLEGRO_DISPLAY *display = nullptr;
 
    int attempts = 20;
 
    for (int attempt=0; attempt<attempts; attempt++)
    {
       std::cout << "creating display (attempt " << attempt << ")" << std::endl;
-      ALLEGRO_DISPLAY *display = al_create_display(1920, 1080);
+      try
+      {
+         display = al_create_display(1920, 1080);
+      }
+      catch (std::exception& e)
+      {
+         std::cout << e.what() << '\n';
+      }
       std::cout << "   display created" << std::endl;
       std::cout << "destroying display" << std::endl;
       al_destroy_display(display);
