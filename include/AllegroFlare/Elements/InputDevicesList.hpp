701ecc6d80a0a1801f60b9205cc7933487528209
Mark Oates <markoates0@gmail.com>
2023-03-07 15:46:18 -0500

    * Add scaffold for Elements/InputDevicesList; Copy from Elements/AchievementsList

---
+++ include/AllegroFlare/Elements/InputDevicesList.hpp
@@ -0,0 +1,82 @@
+#pragma once
+
+
+#include <AllegroFlare/FontBin.hpp>
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_font.h>
+#include <cstdint>
+#include <string>
+#include <tuple>
+#include <vector>
+
+
+namespace AllegroFlare
+{
+   namespace Elements
+   {
+      class InputDevicesList
+      {
+      private:
+         AllegroFlare::FontBin* font_bin;
+         std::vector<std::tuple<std::string, std::string, std::string>> input_devices;
+         float input_devices_box_width;
+         float input_devices_box_height;
+         int surface_width;
+         int surface_height;
+         float scrollbar_position;
+         float box_gutter_y;
+         int count_num_input_devices_completed();
+         void draw_header_title_backfill();
+         int count_num_input_devices();
+         std::string build_input_devices_count_string();
+         void draw_input_devices_list_title_text_and_completed_title_text();
+         float infer_container_height();
+         float infer_container_contents_height();
+         float infer_container_scroll_range();
+         void limit_scrollbar_position();
+         void draw_input_devices_list_items_and_scrollbar();
+         void draw_achievement_box(float x=0.0f, float y=0.0f, std::string status="[unset-status]", std::string title="[unset-title]", std::string description="[unset-description]");
+         int32_t infer_icon_character_by_status(std::string status="[unset-status]");
+         ALLEGRO_COLOR infer_icon_color_by_status(std::string status="[unset-status]", ALLEGRO_COLOR icon_locked_color=ALLEGRO_COLOR{1, 1, 1, 1}, ALLEGRO_COLOR icon_hidden_color=ALLEGRO_COLOR{1, 1, 1, 1}, ALLEGRO_COLOR icon_achieved_color=ALLEGRO_COLOR{1, 1, 1, 1});
+         std::string filter_item_title_through_status(std::string title="[unset-title]", std::string status="[unset-status]");
+         std::string filter_item_description_through_status(std::string description="[unset-title]", std::string status="[unset-status]");
+         ALLEGRO_FONT* obtain_title_font();
+         ALLEGRO_FONT* obtain_item_title_font();
+         ALLEGRO_FONT* obtain_item_description_font();
+         ALLEGRO_FONT* obtain_icon_font();
+         void draw_unicode_character(ALLEGRO_FONT* font=nullptr, ALLEGRO_COLOR color=ALLEGRO_COLOR{1, 1, 1, 1}, int x=0, int y=0, uint32_t icon=61444, int flags=0);
+
+      protected:
+
+
+      public:
+         InputDevicesList(AllegroFlare::FontBin* font_bin=nullptr, std::vector<std::tuple<std::string, std::string, std::string>> input_devices={}, float input_devices_box_width=940.0f, float input_devices_box_height=130.0f);
+         ~InputDevicesList();
+
+         void set_font_bin(AllegroFlare::FontBin* font_bin);
+         void set_input_devices(std::vector<std::tuple<std::string, std::string, std::string>> input_devices);
+         void set_input_devices_box_width(float input_devices_box_width);
+         void set_input_devices_box_height(float input_devices_box_height);
+         void set_surface_width(int surface_width);
+         void set_surface_height(int surface_height);
+         void set_box_gutter_y(float box_gutter_y);
+         std::vector<std::tuple<std::string, std::string, std::string>> get_input_devices() const;
+         float get_input_devices_box_width() const;
+         float get_input_devices_box_height() const;
+         int get_surface_width() const;
+         int get_surface_height() const;
+         float get_scrollbar_position() const;
+         float get_box_gutter_y() const;
+         void render();
+         void move_scrollbar_position(float distance_y=0.0f);
+         void set_scrollbar_position(float scrollbar_position=0.0f);
+         void set_scrollbar_position_to_max();
+         float infer_scrollbar_max_position();
+         bool scrollbar_is_autohidden_because_list_contents_is_smaller_than_the_container();
+         static std::vector<std::tuple<std::string, std::string, std::string>> build_placeholder_input_devices();
+      };
+   }
+}
+
+
+
---
+++ tests/AllegroFlare/Elements/InputDevicesListTest.cpp
@@ -0,0 +1,156 @@
+
+#include <gtest/gtest.h>
+
+#define ASSERT_THROW_WITH_MESSAGE(code, raised_exception_type, expected_exception_message) \
+   try { code; FAIL() << "Expected " # raised_exception_type; } \
+   catch ( raised_exception_type const &err ) { ASSERT_EQ(std::string(expected_exception_message), err.what()); } \
+   catch (...) { FAIL() << "Expected " # raised_exception_type; }
+
+#include <AllegroFlare/Testing/WithAllegroRenderingFixture.hpp>
+
+#include <AllegroFlare/EventEmitter.hpp>
+
+
+class AllegroFlare_Elements_InputDevicesListTest : public ::testing::Test
+{};
+
+class AllegroFlare_Elements_InputDevicesListTestWithAllegroRenderingFixture
+   : public AllegroFlare::Testing::WithAllegroRenderingFixture
+{};
+
+
+#include <AllegroFlare/Elements/InputDevicesList.hpp>
+
+
+TEST_F(AllegroFlare_Elements_InputDevicesListTest, can_be_created_without_blowing_up)
+{
+   AllegroFlare::Elements::InputDevicesList input_devices_list;
+}
+
+
+TEST_F(AllegroFlare_Elements_InputDevicesListTest, render__without_allegro_initialized__raises_an_error)
+{
+   AllegroFlare::Elements::InputDevicesList input_devices_list;
+   std::string expected_error_message =
+      "InputDevicesList::render: error: guard \"al_is_system_installed()\" not met";
+   ASSERT_THROW_WITH_MESSAGE(input_devices_list.render(), std::runtime_error, expected_error_message);
+}
+
+
+TEST_F(AllegroFlare_Elements_InputDevicesListTestWithAllegroRenderingFixture, render__will_not_blow_up)
+{
+   AllegroFlare::Elements::InputDevicesList input_devices_list(&get_font_bin_ref());
+   input_devices_list.render();
+   SUCCEED();
+}
+
+
+TEST_F(AllegroFlare_Elements_InputDevicesListTestWithAllegroRenderingFixture, CAPTURE__render__will_render_as_expected)
+{
+   AllegroFlare::Elements::InputDevicesList input_devices_list(&get_font_bin_ref());
+   input_devices_list.set_input_devices(AllegroFlare::Elements::InputDevicesList::build_placeholder_input_devices());
+   input_devices_list.render();
+   al_flip_display();
+   sleep_for(1);
+   SUCCEED();
+}
+
+TEST_F(AllegroFlare_Elements_InputDevicesListTestWithAllegroRenderingFixture,
+   CAPTURE__render__will_offset_the_list_of_items_by__scrollbar_position)
+{
+   AllegroFlare::Elements::InputDevicesList input_devices_list(&get_font_bin_ref());
+   input_devices_list.set_input_devices(AllegroFlare::Elements::InputDevicesList::build_placeholder_input_devices());
+
+   int num_scrolls = 120;
+   float amount_per_scroll = 2.6;
+   for (int i=0; i<num_scrolls; i++)
+   {
+      input_devices_list.set_scrollbar_position(i * amount_per_scroll);
+
+      clear();
+      input_devices_list.render();
+      al_flip_display();
+   }
+}
+
+
+TEST_F(AllegroFlare_Elements_InputDevicesListTestWithAllegroRenderingFixture,
+   CAPTURE__render__will_hide_the_scrollbar_if_the_height_of_items_in_the_list_is_smaller_than_the_container_height)
+{
+   std::vector<std::tuple<std::string, std::string, std::string>> input_devices_elements =
+      AllegroFlare::Elements::InputDevicesList::build_placeholder_input_devices();
+   input_devices_elements.resize(4);
+   AllegroFlare::Elements::InputDevicesList input_devices_list_list(&get_font_bin_ref(), input_devices_elements);
+
+   clear();
+   input_devices_list_list.render();
+   al_flip_display();
+}
+
+
+TEST_F(AllegroFlare_Elements_InputDevicesListTestWithAllegroRenderingFixture,
+   DISABLED__INTERACTIVE__will_work_as_expected)
+{
+   // setup system
+   al_install_keyboard();
+   al_install_joystick();
+   ALLEGRO_EVENT_QUEUE *event_queue = al_create_event_queue();
+   ALLEGRO_TIMER *primary_timer = al_create_timer(ALLEGRO_BPS_TO_SECS(60));
+   al_register_event_source(event_queue, al_get_keyboard_event_source());
+   al_register_event_source(event_queue, al_get_timer_event_source(primary_timer));
+   //al_register_event_source(event_queue, al_get_timer_event_source(primary_timer));
+   bool abort = false;
+   ALLEGRO_EVENT event;
+
+   // setup environment
+   AllegroFlare::EventEmitter event_emitter;
+   event_emitter.initialize();
+   al_register_event_source(event_queue, &event_emitter.get_event_source_ref());
+
+   // initialize test subject
+   AllegroFlare::Elements::InputDevicesList input_devices_list(&get_font_bin_ref());
+   input_devices_list.set_input_devices(AllegroFlare::Elements::InputDevicesList::build_placeholder_input_devices());
+
+   // run the interactive test
+   al_start_timer(primary_timer);
+   while(!abort)
+   {
+      al_wait_for_event(event_queue, &event);
+
+      switch(event.type)
+      {
+         case ALLEGRO_EVENT_KEY_CHAR:
+         {
+            switch(event.keyboard.keycode)
+            {
+               case ALLEGRO_KEY_UP:
+               case ALLEGRO_KEY_PGUP:
+                  input_devices_list.move_scrollbar_position(-20.0);
+               break;
+
+               case ALLEGRO_KEY_DOWN:
+               case ALLEGRO_KEY_PGDN:
+                  input_devices_list.move_scrollbar_position(20.0);
+               break;
+            }
+         }
+         break;
+
+         case ALLEGRO_EVENT_TIMER:
+            clear();
+            input_devices_list.render();
+            al_flip_display();
+         break;
+      }
+
+      if (event.type == ALLEGRO_EVENT_KEY_CHAR && event.keyboard.keycode == ALLEGRO_KEY_ESCAPE) abort = true;
+   }
+   
+   // teardown
+   // TODO: Audit if this teardown is complete. It may require other calls to destroy resources.
+   al_destroy_event_queue(event_queue);
+   al_destroy_timer(primary_timer);
+   al_uninstall_keyboard();
+}
+
+
