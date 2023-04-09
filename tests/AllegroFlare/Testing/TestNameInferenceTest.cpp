77e9478193a8a61ef71aa76f02f2341f2bcf9812
Mark Oates <markoates0@gmail.com>
2023-04-09 19:06:00 -0400

    * Add test for #build_full_test_name_str#

--- tests/AllegroFlare/Testing/TestNameInferenceTest.cpp
+++ tests/AllegroFlare/Testing/TestNameInferenceTest.cpp
@@ -19,7 +19,7 @@ TEST(AllegroFlare_Testing_TestNameInferenceTest, get_test_name__returns_the_name
 }
 
 
-TEST(AllegroFlare_Testing_TestNameInferenceTest, get_test_suite_name__returns_the_name_of_the_test)
+TEST(AllegroFlare_Testing_TestNameInferenceTest, get_test_suite_name__returns_the_name_of_the_test_suite)
 {
    AllegroFlare::Testing::TestNameInference test_name_inference;
    std::string expected_test_suite_name = "AllegroFlare_Testing_TestNameInferenceTest";
@@ -28,7 +28,18 @@ TEST(AllegroFlare_Testing_TestNameInferenceTest, get_test_suite_name__returns_th
 }
 
 
-// TODO: test build_full_test_name_str
+TEST(AllegroFlare_Testing_TestNameInferenceTest, build_full_test_name_str__returns_a_full_decorated_name_of_the_test)
+{
+   AllegroFlare::Testing::TestNameInference test_name_inference;
+"AllegroFlare_Testing_TestNameInferenceTest - build_full_test_name_str__returns_a_full_decorated_name_of_the_test";
+
+   std::string expected_full_test_name = "AllegroFlare_Testing_TestNameInferenceTest - build_full_test_name_str__"
+                                         "returns_a_full_decorated_name_of_the_test";
+   std::string actual_full_test_name = test_name_inference.build_full_test_name_str();
+   EXPECT_EQ(expected_full_test_name, actual_full_test_name);
+}
+
+
 // TODO: build_test_snapshot_full_filename
 
 
