b20ec097bde84e312a4e23d38935c81e005f4c1d
Mark Oates <markoates0@gmail.com>
2023-04-09 19:02:52 -0400

    * Add two tests to Testing/TestNameInference

--- tests/AllegroFlare/Testing/TestNameInferenceTest.cpp
+++ tests/AllegroFlare/Testing/TestNameInferenceTest.cpp
@@ -10,8 +10,24 @@ TEST(AllegroFlare_Testing_TestNameInferenceTest, can_be_created_without_blowing_
 }
 
 
-// TODO: test get_test_name
-// TODO: test get_test_suite_name
+TEST(AllegroFlare_Testing_TestNameInferenceTest, get_test_name__returns_the_name_of_the_test)
+{
+   AllegroFlare::Testing::TestNameInference test_name_inference;
+   std::string expected_test_name = "get_test_name__returns_the_name_of_the_test";
+   std::string actual_test_name = test_name_inference.get_test_name();
+   EXPECT_EQ(expected_test_name, actual_test_name);
+}
+
+
+TEST(AllegroFlare_Testing_TestNameInferenceTest, get_test_suite_name__returns_the_name_of_the_test)
+{
+   AllegroFlare::Testing::TestNameInference test_name_inference;
+   std::string expected_test_suite_name = "AllegroFlare_Testing_TestNameInferenceTest";
+   std::string actual_test_suite_name = test_name_inference.get_test_suite_name();
+   EXPECT_EQ(expected_test_suite_name, actual_test_suite_name);
+}
+
+
 // TODO: test build_full_test_name_str
 // TODO: build_test_snapshot_full_filename
 
