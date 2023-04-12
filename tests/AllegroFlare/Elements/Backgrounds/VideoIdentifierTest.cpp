e2e23c8eb071d3c0dbb9046cdfcdec2c618199e2
Mark Oates <markoates0@gmail.com>
2023-04-12 15:07:46 -0400

    * Introduce rendering fixture for Backgrounds/VideoIdentifier

--- tests/AllegroFlare/Elements/Backgrounds/VideoIdentifierTest.cpp
+++ tests/AllegroFlare/Elements/Backgrounds/VideoIdentifierTest.cpp
@@ -3,21 +3,33 @@
 
 #include <AllegroFlare/Elements/Backgrounds/VideoIdentifier.hpp>
 
+#include <AllegroFlare/Testing/WithAllegroRenderingFixture.hpp>
 
-TEST(AllegroFlare_Elements_Backgrounds_VideoIdentifierTest, can_be_created_without_blowing_up)
+
+class AllegroFlare_Elements_Backgrounds_VideoIdentifierTest : public ::testing::Test
+{};
+
+class AllegroFlare_Elements_Backgrounds_VideoTestWithAllegroRenderingFixture
+   : public AllegroFlare::Testing::WithAllegroRenderingFixture
+{};
+
+
+TEST_F(AllegroFlare_Elements_Backgrounds_VideoIdentifierTest, can_be_created_without_blowing_up)
 {
    AllegroFlare::Elements::Backgrounds::VideoIdentifier video_identifier;
 }
 
 
-TEST(AllegroFlare_Elements_Backgrounds_VideoIdentifierTest, TYPE__has_the_expected_value)
+TEST_F(AllegroFlare_Elements_Backgrounds_VideoIdentifierTest, TYPE__has_the_expected_value)
 {
-   AllegroFlare::Elements::Backgrounds::VideoIdentifier video_identifier;
-   EXPECT_EQ("AllegroFlare/Elements/Backgrounds/VideoIdentifier", video_identifier.get_type());
+   EXPECT_EQ(
+      "AllegroFlare/Elements/Backgrounds/VideoIdentifier",
+      AllegroFlare::Elements::Backgrounds::VideoIdentifier::TYPE
+   );
 }
 
 
-TEST(AllegroFlare_Elements_Backgrounds_VideoIdentifierTest, type__has_the_expected_value_matching_TYPE)
+TEST_F(AllegroFlare_Elements_Backgrounds_VideoIdentifierTest, type__has_the_expected_value_matching_TYPE)
 {
    AllegroFlare::Elements::Backgrounds::VideoIdentifier video_identifier;
    EXPECT_EQ(AllegroFlare::Elements::Backgrounds::VideoIdentifier::TYPE, video_identifier.get_type());
