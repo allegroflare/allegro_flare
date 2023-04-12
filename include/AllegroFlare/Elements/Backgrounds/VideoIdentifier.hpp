9dd3c07b44da199b607f7d4affa59dbad6879693
Mark Oates <markoates0@gmail.com>
2023-04-12 16:04:45 -0400

    * Add comments in Backgrounds/VideoIdentifier about #video_is_playing# flag

--- include/AllegroFlare/Elements/Backgrounds/VideoIdentifier.hpp
+++ include/AllegroFlare/Elements/Backgrounds/VideoIdentifier.hpp
@@ -21,6 +21,9 @@ namespace AllegroFlare
          private:
             AllegroFlare::VideoBin* video_bin;
             std::string video_identifier;
+            int surface_width;
+            int surface_height;
+            bool video_is_playing;
             ALLEGRO_VIDEO* obtain_video();
 
          protected:
@@ -30,6 +33,10 @@ namespace AllegroFlare
             VideoIdentifier(AllegroFlare::VideoBin* video_bin=nullptr, std::string video_identifier="[unset-video_identifier]");
             ~VideoIdentifier();
 
+            void set_surface_width(int surface_width);
+            void set_surface_height(int surface_height);
+            int get_surface_width() const;
+            int get_surface_height() const;
             void activate() override;
             void deactivate() override;
             void render() override;
--- src/AllegroFlare/Elements/Backgrounds/VideoIdentifier.cpp
+++ src/AllegroFlare/Elements/Backgrounds/VideoIdentifier.cpp
@@ -19,12 +19,39 @@ VideoIdentifier::VideoIdentifier(AllegroFlare::VideoBin* video_bin, std::string 
    : AllegroFlare::Elements::Backgrounds::Base(AllegroFlare::Elements::Backgrounds::VideoIdentifier::TYPE)
    , video_bin(video_bin)
    , video_identifier(video_identifier)
+   , surface_width(1920)
+   , surface_height(1080)
+   , video_is_playing(false)
 {
 }
 
 
 VideoIdentifier::~VideoIdentifier()
 {
+}
+
+
+void VideoIdentifier::set_surface_width(int surface_width)
+{
+   this->surface_width = surface_width;
+}
+
+
+void VideoIdentifier::set_surface_height(int surface_height)
+{
+   this->surface_height = surface_height;
+}
+
+
+int VideoIdentifier::get_surface_width() const
+{
+   return surface_width;
+}
+
+
+int VideoIdentifier::get_surface_height() const
+{
+   return surface_height;
 }
 
 
@@ -37,11 +64,11 @@ void VideoIdentifier::activate()
       std::cerr << "\033[1;31m" << error_message.str() << " An exception will be thrown to halt the program.\033[0m" << std::endl;
       throw std::runtime_error("VideoIdentifier::activate: error: guard \"video_bin\" not met");
    }
-   // Should this logic be in the "show" function instead?
    ALLEGRO_VIDEO *video = obtain_video();
    if (video)
    {
       al_start_video(video, al_get_default_mixer());
+      video_is_playing = true;
       std::cout << "video FPS: " << al_get_video_fps(video) << std::endl;
       std::cout << "video audio rate: " << al_get_video_audio_rate(video) << std::endl;
    }
@@ -71,20 +98,23 @@ void VideoIdentifier::render()
       std::cerr << "\033[1;31m" << error_message.str() << " An exception will be thrown to halt the program.\033[0m" << std::endl;
       throw std::runtime_error("VideoIdentifier::render: error: guard \"video_bin\" not met");
    }
+   if (!video_is_playing) return;
+
    ALLEGRO_VIDEO *video = obtain_video();
    if (!video) return;
 
-   bool video_is_playing = al_is_video_playing(video);
+   bool video_is_playing = al_is_video_playing(video); // I think there is a bug here
    if (!video_is_playing) return;
 
    ALLEGRO_BITMAP* frame = al_get_video_frame(video);
    if (!frame) return;
 
-   float scale = 1.0;
+   float scale_x = surface_width / al_get_video_scaled_width(video);
+   float scale_y = surface_height / al_get_video_scaled_height(video);
    float sw = al_get_bitmap_width(frame);
    float sh = al_get_bitmap_height(frame);
-   float dw = scale * al_get_video_scaled_width(video);
-   float dh = scale * al_get_video_scaled_height(video);
+   float dw = scale_x * al_get_video_scaled_width(video);
+   float dh = scale_y * al_get_video_scaled_height(video);
    al_draw_scaled_bitmap(frame, 0, 0, sw, sh, 0, 0, dw, dh, 0);
    return;
 }
