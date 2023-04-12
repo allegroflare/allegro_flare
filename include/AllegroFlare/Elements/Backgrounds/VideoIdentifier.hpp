d1bed3f956c4ce4c0ac497f54041207583cdb656
Mark Oates <markoates0@gmail.com>
2023-04-12 15:23:38 -0400

    * Add non-blowing up test to render video in Backgrounds/VideoIdentifier

--- include/AllegroFlare/Elements/Backgrounds/VideoIdentifier.hpp
+++ include/AllegroFlare/Elements/Backgrounds/VideoIdentifier.hpp
@@ -3,6 +3,7 @@
 
 #include <AllegroFlare/Elements/Backgrounds/Base.hpp>
 #include <AllegroFlare/VideoBin.hpp>
+#include <allegro5/allegro.h>
 #include <string>
 
 
@@ -20,6 +21,7 @@ namespace AllegroFlare
          private:
             AllegroFlare::VideoBin* video_bin;
             std::string video_identifier;
+            ALLEGRO_VIDEO* obtain_video();
 
          protected:
 
@@ -29,6 +31,7 @@ namespace AllegroFlare
             ~VideoIdentifier();
 
             void activate() override;
+            void deactivate() override;
             void render() override;
          };
       }
--- src/AllegroFlare/Elements/Backgrounds/VideoIdentifier.cpp
+++ src/AllegroFlare/Elements/Backgrounds/VideoIdentifier.cpp
@@ -38,6 +38,22 @@ void VideoIdentifier::activate()
       throw std::runtime_error("VideoIdentifier::activate: error: guard \"video_bin\" not met");
    }
    // Should this logic be in the "show" function instead?
+   ALLEGRO_VIDEO *video = obtain_video();
+   if (video) al_start_video(video, nullptr);
+   return;
+}
+
+void VideoIdentifier::deactivate()
+{
+   if (!(video_bin))
+   {
+      std::stringstream error_message;
+      error_message << "[VideoIdentifier::deactivate]: error: guard \"video_bin\" not met.";
+      std::cerr << "\033[1;31m" << error_message.str() << " An exception will be thrown to halt the program.\033[0m" << std::endl;
+      throw std::runtime_error("VideoIdentifier::deactivate: error: guard \"video_bin\" not met");
+   }
+   ALLEGRO_VIDEO *video = obtain_video();
+   if (video) al_set_video_playing(video, false);
    return;
 }
 
@@ -50,7 +66,21 @@ void VideoIdentifier::render()
       std::cerr << "\033[1;31m" << error_message.str() << " An exception will be thrown to halt the program.\033[0m" << std::endl;
       throw std::runtime_error("VideoIdentifier::render: error: guard \"video_bin\" not met");
    }
+   ALLEGRO_VIDEO *video = obtain_video();
+   // TODO: obtain the frame and render the image
    return;
+}
+
+ALLEGRO_VIDEO* VideoIdentifier::obtain_video()
+{
+   if (!(video_bin))
+   {
+      std::stringstream error_message;
+      error_message << "[VideoIdentifier::obtain_video]: error: guard \"video_bin\" not met.";
+      std::cerr << "\033[1;31m" << error_message.str() << " An exception will be thrown to halt the program.\033[0m" << std::endl;
+      throw std::runtime_error("VideoIdentifier::obtain_video: error: guard \"video_bin\" not met");
+   }
+   return video_bin->auto_get(video_identifier);
 }
 
 
