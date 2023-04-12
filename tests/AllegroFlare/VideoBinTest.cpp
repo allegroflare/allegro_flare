23403e877bf2926575720747e57aac6d325a3d64
Mark Oates <markoates0@gmail.com>
2023-04-12 14:25:08 -0400

    * Setup test fixture for VideoBin

--- tests/AllegroFlare/VideoBinTest.cpp
+++ tests/AllegroFlare/VideoBinTest.cpp
@@ -2,9 +2,16 @@
 #include <gtest/gtest.h>
 
 #include <AllegroFlare/VideoBin.hpp>
+#include <AllegroFlare/Testing/WithAllegroRenderingFixture.hpp>
 
 
-TEST(AllegroFlare_VideoBinTest, can_be_created_without_blowing_up)
+class AllegroFlare_VideoBinTest : public ::testing::Test{};
+class AllegroFlare_VideoBinTestWithAllegroRenderingFixture
+   : public AllegroFlare::Testing::WithAllegroRenderingFixture
+{};
+
+
+TEST_F(AllegroFlare_VideoBinTest, can_be_created_without_blowing_up)
 {
    AllegroFlare::VideoBin video_bin;
 }
