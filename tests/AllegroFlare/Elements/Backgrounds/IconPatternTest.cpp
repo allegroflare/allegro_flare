706e3ee88052e17fe9d9f768bb007ac76e496ada
Mark Oates <markoates0@gmail.com>
2023-04-21 10:18:20 -0400

    * Use ErrorAssertions in Elements/Backgrounds/IconPattern

--- tests/AllegroFlare/Elements/Backgrounds/IconPatternTest.cpp
+++ tests/AllegroFlare/Elements/Backgrounds/IconPatternTest.cpp
@@ -1,11 +1,8 @@
 
 #include <gtest/gtest.h>
 
-#define ASSERT_THROW_WITH_MESSAGE(code, raised_exception_type, raised_exception_message) \
-   try { code; FAIL() << "Expected " # raised_exception_type; } \
-   catch ( raised_exception_type const &err ) { EXPECT_EQ(err.what(), std::string( raised_exception_message )); } \
-   catch (...) { FAIL() << "Expected " # raised_exception_type; }
-
+#include <AllegroFlare/Testing/ErrorAssertions.hpp>
+#include <AllegroFlare/Testing/WithAllegroRenderingFixture.hpp>
 
 #include <AllegroFlare/Elements/Backgrounds/IconPattern.hpp>
 
@@ -15,7 +12,6 @@
 #define TEST_FIXTURE_BITMAP_FOLDER "/Users/markoates/Repos/allegro_flare/bin/data/bitmaps/"
 #endif
 
-#include <AllegroFlare/Testing/WithAllegroRenderingFixture.hpp>
 #include <AllegroFlare/Color.hpp>
 #include <chrono>
 #include <thread>
