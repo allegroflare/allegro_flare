e3d53c86b4c8e2d9c438f28a99fa1fc8418d2ecf
Mark Oates <markoates0@gmail.com>
2023-05-02 09:21:25 -0400

    * Add some tests for #start_session# and #end_session#

--- tests/AllegroFlare/GameSessionTest.cpp
+++ tests/AllegroFlare/GameSessionTest.cpp
@@ -27,3 +27,20 @@ TEST(AllegroFlare_GameSessionTest, end_session__will_set_ended_at)
 }
 
 
+TEST(AllegroFlare_GameSessionTest, start_session__will_set_active_to_true)
+{
+   AllegroFlare::GameSession game_session;
+   game_session.start_session(123.0f);
+   EXPECT_EQ(true, game_session.get_active());
+}
+
+
+TEST(AllegroFlare_GameSessionTest, end_session__with_an_active_session__will_set_active_to_false)
+{
+   AllegroFlare::GameSession game_session;
+   game_session.start_session(123.0);
+   game_session.end_session(456.0f);
+   EXPECT_EQ(false, game_session.get_active());
+}
+
+
