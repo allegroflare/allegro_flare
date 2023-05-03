378caa9d432b567143cf59b4fa6af845ac4287ae
Mark Oates <markoates0@gmail.com>
2023-05-03 18:36:46 -0400

    * Move background render to #primary_timer_func#

--- src/AllegroFlare/Screens/CharacterNameInput.cpp
+++ src/AllegroFlare/Screens/CharacterNameInput.cpp
@@ -127,6 +127,7 @@ void CharacterNameInput::primary_timer_func()
       throw std::runtime_error("CharacterNameInput::primary_timer_func: error: guard \"initialized\" not met");
    }
    if (background) background->update();
+   if (background) background->render();
    render();
    return;
 }
@@ -285,7 +286,6 @@ void CharacterNameInput::render()
       std::cerr << "\033[1;31m" << error_message.str() << " An exception will be thrown to halt the program.\033[0m" << std::endl;
       throw std::runtime_error("CharacterNameInput::render: error: guard \"initialized\" not met");
    }
-   if (background) background->render();
    software_keyboard.render();
    return;
 }
