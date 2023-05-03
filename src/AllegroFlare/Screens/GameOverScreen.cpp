37c1842256ec135003763d7b388c639e47a2df7c
Mark Oates <markoates0@gmail.com>
2023-05-03 18:38:03 -0400

    * Move render to #primary_timer_func# in GameOverScreen

--- src/AllegroFlare/Screens/GameOverScreen.cpp
+++ src/AllegroFlare/Screens/GameOverScreen.cpp
@@ -221,6 +221,7 @@ void GameOverScreen::select_menu_option()
 void GameOverScreen::primary_timer_func()
 {
    if (background) background->update();
+   if (background) background->render();
    render();
    return;
 }
@@ -248,7 +249,6 @@ void GameOverScreen::render()
       std::cerr << "\033[1;31m" << error_message.str() << " An exception will be thrown to halt the program.\033[0m" << std::endl;
       throw std::runtime_error("GameOverScreen::render: error: guard \"get_initialized()\" not met");
    }
-   if (background) background->render();
    draw_primary_text();
    draw_menu();
    return;
