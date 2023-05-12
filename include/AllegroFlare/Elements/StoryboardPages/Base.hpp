01aeb16978b51a49836700d55d051268285e825c
Mark Oates <markoates0@gmail.com>
2023-05-11 20:05:11 -0400

    * Add (untested) #started_at# to StoryboardPages/Base

--- include/AllegroFlare/Elements/StoryboardPages/Base.hpp
+++ include/AllegroFlare/Elements/StoryboardPages/Base.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 
+#include <allegro5/allegro.h>
 #include <string>
 
 
@@ -12,24 +13,32 @@ namespace AllegroFlare
       {
          class Base
          {
+         public:
+            static constexpr char* TYPE = (char*)"AllegroFlare/Elements/StoryboardPages/Base";
+
          private:
             std::string type;
+            float started_at;
             bool finished;
 
          protected:
 
 
          public:
-            Base(std::string type="Base");
+            Base(std::string type=AllegroFlare::Elements::StoryboardPages::Base::TYPE);
             virtual ~Base();
 
+            void set_started_at(float started_at);
             void set_finished(bool finished);
             std::string get_type() const;
+            float get_started_at() const;
             bool get_finished() const;
+            void managed_start(float time_now=al_get_time());
             virtual void start();
             virtual void update();
             virtual void render();
             virtual void advance();
+            float infer_age(float time_now=al_get_time());
          };
       }
    }
--- tests/AllegroFlare/Elements/StoryboardPages/BaseTest.cpp
+++ tests/AllegroFlare/Elements/StoryboardPages/BaseTest.cpp
@@ -10,10 +10,17 @@ TEST(AllegroFlare_Elements_StoryboardPages_BaseTest, can_be_created_without_blow
 }
 
 
+TEST(AllegroFlare_Elements_StoryboardPages_BaseTest, TYPE__has_the_expected_value)
+{
+   AllegroFlare::Elements::StoryboardPages::Base base;
+   EXPECT_STREQ("AllegroFlare/Elements/StoryboardPages/Base", AllegroFlare::Elements::StoryboardPages::Base::TYPE);
+}
+
+
 TEST(AllegroFlare_Elements_StoryboardPages_BaseTest, has_the_expected_type)
 {
    AllegroFlare::Elements::StoryboardPages::Base base;
-   EXPECT_EQ("Base", base.get_type());
+   EXPECT_EQ(AllegroFlare::Elements::StoryboardPages::Base::TYPE, base.get_type());
 }
 
 
