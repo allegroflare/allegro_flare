5ab8cb846bc3f5d11b1dc6325c530b0882a7e93e
Mark Oates <markoates0@gmail.com>
2023-05-20 20:01:04 -0400

    * Reformat a line-too-long

--- tests/AllegroFlare/Story/Characters/PersonalityProfileFactoryTest.cpp
+++ tests/AllegroFlare/Story/Characters/PersonalityProfileFactoryTest.cpp
@@ -25,7 +25,19 @@ TEST(AllegroFlare_Story_Characters_PersonalityProfileFactoryTest,
    std::string personality_profile_writeup =
    personality_profile_factory.build_random_personality_profile("Yuki", 5, 86545);
 
-   std::string expected_personality_profile_writeup = "In the personality category of \"Openness to Experience\" (characters can differ in their curiosity, imagination, and willingness to embrace new ideas or experiences), Yuki ranks HIGH, meaning Yuki is exceptionally curious, imaginative, and open to new ideas.\nIn the personality category of \"Optimism vs. Pessimism\" (characters may have different outlooks on life, ranging from positive and hopeful to negative and cynical), Yuki ranks VERY_HIGH, meaning Yuki has a very optimistic personality.\nIn the personality category of \"Adventurousness\" (characters can vary in their desire for exploration, risk-taking, and seeking out new challenges.), Yuki ranks BALANCED, meaning Yuki is willing to take on an adventure if compelled to do so.\nIn the personality category of \"Empathy\" (the level of empathy a character possesses influences their ability to understand and connect with others' emotions), Yuki ranks VERY_HIGH, meaning Yuki has a high degree of empathy and easily understands others' emotions even when they may not.\nIn the personality category of \"Independence vs. Dependence\" (some characters may be self-reliant and autonomous, while others may rely more heavily on others for support), Yuki ranks VERY_HIGH, meaning Yuki is overly self-reliant and actively goes out of their way to not be dependent on others.\n";
+   std::string expected_personality_profile_writeup = "In the personality category of \"Openness to Experience\" "
+      "(characters can differ in their curiosity, imagination, and willingness to embrace new ideas or experiences), "
+      "Yuki ranks HIGH, meaning Yuki is exceptionally curious, imaginative, and open to new ideas.\nIn the "
+      "personality category of \"Optimism vs. Pessimism\" (characters may have different outlooks on life, ranging "
+      "from positive and hopeful to negative and cynical), Yuki ranks VERY_HIGH, meaning Yuki has a very optimistic "
+      "personality.\nIn the personality category of \"Adventurousness\" (characters can vary in their desire for "
+      "exploration, risk-taking, and seeking out new challenges.), Yuki ranks BALANCED, meaning Yuki is willing to "
+      "take on an adventure if compelled to do so.\nIn the personality category of \"Empathy\" (the level of empathy "
+      "a character possesses influences their ability to understand and connect with others' emotions), Yuki ranks "
+      "VERY_HIGH, meaning Yuki has a high degree of empathy and easily understands others' emotions even when they "
+      "may not.\nIn the personality category of \"Independence vs. Dependence\" (some characters may be self-reliant "
+      "and autonomous, while others may rely more heavily on others for support), Yuki ranks VERY_HIGH, meaning Yuki "
+      "is overly self-reliant and actively goes out of their way to not be dependent on others.\n";
  
    EXPECT_EQ(expected_personality_profile_writeup, personality_profile_writeup);
 }
