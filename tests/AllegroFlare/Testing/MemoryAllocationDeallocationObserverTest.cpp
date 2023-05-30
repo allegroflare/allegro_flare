90351e5aa25e74e2904aa072799516a2acb96b76
Mark Oates <markoates0@gmail.com>
2023-05-30 17:47:30 -0400

    * Add test on allocation count for MemoryAllocationDeallocationObserver

--- tests/AllegroFlare/Testing/MemoryAllocationDeallocationObserverTest.cpp
+++ tests/AllegroFlare/Testing/MemoryAllocationDeallocationObserverTest.cpp
@@ -10,3 +10,15 @@ TEST(AllegroFlare_Testing_MemoryAllocationDeallocationObserverTest, can_be_creat
 }
 
 
+TEST(AllegroFlare_Testing_MemoryAllocationDeallocationObserverTest, when_active__will_count_the_number_allocations)
+{
+   AllegroFlare::Testing::MemoryAllocationDeallocationObserver memory_allocation_deallocation_observer;
+   int *var = nullptr;
+   memory_allocation_deallocation_observer.enable_memory_tracking();
+   var = new int(3);
+   memory_allocation_deallocation_observer.disable_memory_tracking();
+   EXPECT_EQ(1, memory_allocation_deallocation_observer.get_num_allocations());
+   delete var;
+}
+
+
