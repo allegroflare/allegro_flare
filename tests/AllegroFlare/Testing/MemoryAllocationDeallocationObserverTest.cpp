7169697e9c0455e216b9949f8ffe2a5b87008b16
Mark Oates <markoates0@gmail.com>
2023-06-01 20:48:44 -0400

    * Rename test variables

--- tests/AllegroFlare/Testing/MemoryAllocationDeallocationObserverTest.cpp
+++ tests/AllegroFlare/Testing/MemoryAllocationDeallocationObserverTest.cpp
@@ -8,47 +8,45 @@
 
 TEST(AllegroFlare_Testing_MemoryAllocationDeallocationObserverTest, can_be_created_without_blowing_up)
 {
-   AllegroFlare::Testing::MemoryAllocationDeallocationObserver memory_allocation_deallocation_observer;
+   AllegroFlare::Testing::MemoryAllocationDeallocationObserver observer;
 }
 
 
 TEST(AllegroFlare_Testing_MemoryAllocationDeallocationObserverTest, when_active__will_count_the_number_allocations)
 {
-   AllegroFlare::Testing::MemoryAllocationDeallocationObserver memory_allocation_deallocation_observer;
+   AllegroFlare::Testing::MemoryAllocationDeallocationObserver observer;
    int *var = nullptr;
-   memory_allocation_deallocation_observer.enable_memory_tracking();
+   observer.enable_memory_tracking();
    var = new int(3);
-   memory_allocation_deallocation_observer.disable_memory_tracking();
-   EXPECT_EQ(1, memory_allocation_deallocation_observer.get_num_allocations());
+   observer.disable_memory_tracking();
+   EXPECT_EQ(1, observer.get_num_allocations());
    delete var;
 }
 
 
 TEST(AllegroFlare_Testing_MemoryAllocationDeallocationObserverTest, when_active__will_count_the_number_deallocations)
 {
-   AllegroFlare::Testing::MemoryAllocationDeallocationObserver memory_allocation_deallocation_observer;
+   AllegroFlare::Testing::MemoryAllocationDeallocationObserver observer;
    int *var = new int(3);
-   memory_allocation_deallocation_observer.enable_memory_tracking();
+   observer.enable_memory_tracking();
    delete var;
-   memory_allocation_deallocation_observer.disable_memory_tracking();
+   observer.disable_memory_tracking();
 
-   EXPECT_EQ(1, memory_allocation_deallocation_observer.get_num_deallocations());
+   EXPECT_EQ(1, observer.get_num_deallocations());
 }
 
 
 TEST(AllegroFlare_Testing_MemoryAllocationDeallocationObserverTest,
    output_memory_event_logs_enabled__when_true__will_output_to_cout_when_an_allocation_occurs)
 {
-   AllegroFlare::Testing::MemoryAllocationDeallocationObserver memory_allocation_deallocation_observer;
-   memory_allocation_deallocation_observer.reset(); // TODO: create an object that, on destruction, will reset. Prefer
-                                                    // using this object in test cases to guarantee disabling logs
+   AllegroFlare::Testing::MemoryAllocationDeallocationObserver observer;
    std::mutex mutex;
    mutex.lock();
 
    testing::internal::CaptureStdout();
-   memory_allocation_deallocation_observer.enable_output_memory_event_logs();
+   observer.enable_output_memory_event_logs();
    int *var = new int(3);
-   memory_allocation_deallocation_observer.disable_output_memory_event_logs();
+   observer.disable_output_memory_event_logs();
    std::string actual_stdout = testing::internal::GetCapturedStdout();
 
    mutex.unlock();
