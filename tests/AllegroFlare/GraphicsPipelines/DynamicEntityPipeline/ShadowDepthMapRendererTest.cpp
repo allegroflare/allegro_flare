8f15da9942a754839107d7798479d5ae773d8584
Mark Oates <markoates0@gmail.com>
2023-06-15 18:49:47 -0400

    * Get ShadowDepthMapRender function to not crash

--- tests/AllegroFlare/GraphicsPipelines/DynamicEntityPipeline/ShadowDepthMapRendererTest.cpp
+++ tests/AllegroFlare/GraphicsPipelines/DynamicEntityPipeline/ShadowDepthMapRendererTest.cpp
@@ -1,23 +1,31 @@
 
 #include <gtest/gtest.h>
 
+#include <AllegroFlare/Testing/WithAllegroRenderingFixture.hpp>
 #include <AllegroFlare/GraphicsPipelines/DynamicEntityPipeline/ShadowDepthMapRenderer.hpp>
 
 
-TEST(AllegroFlare_GraphicsPipelines_DynamicEntityPipeline_ShadowDepthMapRendererTest, can_be_created_without_blowing_up)
+class AllegroFlare_GraphicsPipelines_DynamicEntityPipeline_ShadowDepthMapRendererTest : public ::testing::Test {};
+class AllegroFlare_GraphicsPipelines_DynamicEntityPipeline_ShadowDepthMapRendererTestWithAllegroRenderingFixtureTest :
+   public AllegroFlare::Testing::WithAllegroRenderingFixture
+{};
+
+
+TEST_F(AllegroFlare_GraphicsPipelines_DynamicEntityPipeline_ShadowDepthMapRendererTest,
+   can_be_created_without_blowing_up)
 {
    AllegroFlare::GraphicsPipelines::DynamicEntityPipeline::ShadowDepthMapRenderer shadow_depth_map_renderer;
 }
 
 
-TEST(AllegroFlare_GraphicsPipelines_DynamicEntityPipeline_ShadowDepthMapRendererTest, render__does_not_blow_up)
+TEST_F(AllegroFlare_GraphicsPipelines_DynamicEntityPipeline_ShadowDepthMapRendererTestWithAllegroRenderingFixtureTest,
+   render__does_not_blow_up)
 {
    AllegroFlare::GraphicsPipelines::DynamicEntityPipeline::EntityPool entity_pool;
    AllegroFlare::GraphicsPipelines::DynamicEntityPipeline::ShadowDepthMapRenderer shadow_depth_map_renderer;
    shadow_depth_map_renderer.set_entity_pool(&entity_pool);
 
-   // TODO: Get this test working
-   //shadow_depth_map_renderer.render();
+   shadow_depth_map_renderer.render();
 }
 
 
