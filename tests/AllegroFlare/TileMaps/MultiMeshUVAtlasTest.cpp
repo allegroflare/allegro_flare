46db9434063933d4ef9d0043177014328e382e3c
Mark Oates <markoates0@gmail.com>
2023-06-23 18:50:56 -0400

    * Add tests for #exists# in MultiMeshUVAtlas

--- tests/AllegroFlare/TileMaps/MultiMeshUVAtlasTest.cpp
+++ tests/AllegroFlare/TileMaps/MultiMeshUVAtlasTest.cpp
@@ -10,3 +10,18 @@ TEST(AllegroFlare_TileMaps_MultiMeshUVAtlasTest, can_be_created_without_blowing_
 }
 
 
+TEST(AllegroFlare_TileMaps_MultiMeshUVAtlasTest, exists__will_return_true_if_an_item_exists)
+{
+   AllegroFlare::TileMaps::MultiMeshUVAtlas multi_mesh_uvatlas;
+   multi_mesh_uvatlas.add(321, 0.0, 1.0, 2.0, 4.0);
+   EXPECT_EQ(true, multi_mesh_uvatlas.exists(321));
+}
+
+
+TEST(AllegroFlare_TileMaps_MultiMeshUVAtlasTest, exists__will_return_false_if_an_item_does_not_exist_at_the_index)
+{
+   AllegroFlare::TileMaps::MultiMeshUVAtlas multi_mesh_uvatlas;
+   EXPECT_EQ(false, multi_mesh_uvatlas.exists(321));
+}
+
+
