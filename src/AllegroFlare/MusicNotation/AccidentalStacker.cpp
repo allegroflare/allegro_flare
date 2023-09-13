c2e42884fe45fe9798c854cc5b576ef136c0680f
Mark Oates <markoates0@gmail.com>
2023-09-13 14:56:02 -0400

    * Use only #solve_from_the_top# to pass test

--- src/AllegroFlare/MusicNotation/AccidentalStacker.cpp
+++ src/AllegroFlare/MusicNotation/AccidentalStacker.cpp
@@ -78,51 +78,52 @@ void AccidentalStacker::solve_one_from_top(int column_to_place_on)
       std::cerr << "\033[1;31m" << error_message.str() << " An exception will be thrown to halt the program.\033[0m" << std::endl;
       throw std::runtime_error("AccidentalStacker::solve_one_from_top: error: guard \"(!pitches.empty())\" not met");
    }
-   auto &pitch = pitches[0];
+   auto &pitch = pitches.front();
 
-   if (!pitch.has_accidental())
-   {
-      // TODO: Replace with AllegroFlare::Logger error
-      throw std::runtime_error("AccidentalStacker::solve_one_from_top: error: "
-            "Cannot solve. First pitch does not have an accidental.");
-   }
-    
-   // Place the accidental at the current column
-   int pitch_staff_position = pitch.get_staff_position();
-   int pitch_staff_position_normalized = pitch_staff_position % 7;
-   int pitch_accidental_weight = pitch.calculate_accidental_weight();
-   AccidentalType accidental_type = find_accidental_type_by_weight(pitch_accidental_weight);
-
-   // Put it in the stack
-   stack.push_back({ accidental_type, { column_to_place_on, pitch_staff_position } });
-
-   // Erase the list of pitches
-   pitches.erase(pitches.begin() + 0);
-   if (pitches.empty()) return;
-
-   // Go through the list of pitches and look for any octaves to the current note, placing their accidentals
-   // in the same column
-   for (int j=0; j<pitches.size(); j++)
-   {
-      auto &possible_octave_to_pitch = pitches[j];
-      int possible_octave_pitch_staff_position = possible_octave_to_pitch.get_staff_position();
-      int possible_octave_pitch_staff_position_normalized = possible_octave_pitch_staff_position % 7;
-      int possible_octave_pitch_accidental_weight = possible_octave_to_pitch.calculate_accidental_weight();
-      bool is_an_octave_apart =
-            (possible_octave_pitch_staff_position_normalized == pitch_staff_position_normalized)
-            && (possible_octave_pitch_accidental_weight == pitch_accidental_weight);
-
-      // This pitch is an octave apart, place it in the same column
-      if (is_an_octave_apart)
+      if (!pitch.has_accidental())
       {
-         AccidentalType accidental_type = find_accidental_type_by_weight(possible_octave_pitch_accidental_weight);
-         stack.push_back({ accidental_type, { column_to_place_on, possible_octave_pitch_staff_position } });
-
-         pitches.erase(pitches.begin() + j);
-         //i--;
-         j--;
+         // TODO: Replace with AllegroFlare::Logger error
+         throw std::runtime_error("AccidentalStacker::solve_one_from_top: error: "
+               "Cannot solve. First pitch does not have an accidental.");
       }
-   }
+       
+      // Place the accidental at the current column
+      int pitch_staff_position = pitch.get_staff_position();
+      int pitch_staff_position_normalized = pitch_staff_position % 7;
+      int pitch_accidental_weight = pitch.calculate_accidental_weight();
+      AccidentalType accidental_type = find_accidental_type_by_weight(pitch_accidental_weight);
+
+      // Put it in the stack
+      stack.push_back({ accidental_type, { column_to_place_on, pitch_staff_position } });
+
+      // Erase the list of pitches
+      pitches.erase(pitches.begin() + 0);
+      if (pitches.empty()) return;
+
+      // Go through the list of pitches and look for any octaves to the current note, placing their accidentals
+      // in the same column
+      for (int j=0; j<pitches.size(); j++)
+      {
+         auto &possible_octave_to_pitch = pitches[j];
+         int possible_octave_pitch_staff_position = possible_octave_to_pitch.get_staff_position();
+         int possible_octave_pitch_staff_position_normalized = possible_octave_pitch_staff_position % 7;
+         int possible_octave_pitch_accidental_weight = possible_octave_to_pitch.calculate_accidental_weight();
+         bool is_an_octave_apart =
+               (possible_octave_pitch_staff_position_normalized == pitch_staff_position_normalized)
+               && (possible_octave_pitch_accidental_weight == pitch_accidental_weight);
+
+         // This pitch is an octave apart, place it in the same column
+         if (is_an_octave_apart)
+         {
+            AccidentalType accidental_type = find_accidental_type_by_weight(possible_octave_pitch_accidental_weight);
+            stack.push_back({ accidental_type, { column_to_place_on, possible_octave_pitch_staff_position } });
+
+            pitches.erase(pitches.begin() + j);
+            //i--;
+            j--;
+         }
+      }
+   return;
 }
 
 void AccidentalStacker::solve_one_from_bottom(int column_to_place_on)
@@ -136,49 +137,50 @@ void AccidentalStacker::solve_one_from_bottom(int column_to_place_on)
    }
    auto &pitch = pitches.back();
 
-   if (!pitch.has_accidental())
-   {
-      // TODO: Replace with AllegroFlare::Logger error
-      throw std::runtime_error("AccidentalStacker::solve_one_from_top: error: "
-            "Cannot solve. First pitch does not have an accidental.");
-   }
-    
-   // Place the accidental at the current column
-   int pitch_staff_position = pitch.get_staff_position();
-   int pitch_staff_position_normalized = pitch_staff_position % 7;
-   int pitch_accidental_weight = pitch.calculate_accidental_weight();
-   AccidentalType accidental_type = find_accidental_type_by_weight(pitch_accidental_weight);
-
-   // Put it in the stack
-   stack.push_back({ accidental_type, { column_to_place_on, pitch_staff_position } });
-
-   // Erase the list of pitches
-   pitches.pop_back();
-   if (pitches.empty()) return;
-
-   // Go through the list of pitches and look for any octaves to the current note, placing their accidentals
-   // in the same column
-   for (int j=0; j<pitches.size(); j++)
-   {
-      auto &possible_octave_to_pitch = pitches[j];
-      int possible_octave_pitch_staff_position = possible_octave_to_pitch.get_staff_position();
-      int possible_octave_pitch_staff_position_normalized = possible_octave_pitch_staff_position % 7;
-      int possible_octave_pitch_accidental_weight = possible_octave_to_pitch.calculate_accidental_weight();
-      bool is_an_octave_apart =
-            (possible_octave_pitch_staff_position_normalized == pitch_staff_position_normalized)
-            && (possible_octave_pitch_accidental_weight == pitch_accidental_weight);
-
-      // This pitch is an octave apart, place it in the same column
-      if (is_an_octave_apart)
+      if (!pitch.has_accidental())
       {
-         AccidentalType accidental_type = find_accidental_type_by_weight(possible_octave_pitch_accidental_weight);
-         stack.push_back({ accidental_type, { column_to_place_on, possible_octave_pitch_staff_position } });
-
-         pitches.erase(pitches.begin() + j);
-         //i--;
-         j--;
+         // TODO: Replace with AllegroFlare::Logger error
+         throw std::runtime_error("AccidentalStacker::solve_one_from_top: error: "
+               "Cannot solve. First pitch does not have an accidental.");
       }
-   }
+       
+      // Place the accidental at the current column
+      int pitch_staff_position = pitch.get_staff_position();
+      int pitch_staff_position_normalized = pitch_staff_position % 7;
+      int pitch_accidental_weight = pitch.calculate_accidental_weight();
+      AccidentalType accidental_type = find_accidental_type_by_weight(pitch_accidental_weight);
+
+      // Put it in the stack
+      stack.push_back({ accidental_type, { column_to_place_on, pitch_staff_position } });
+
+      // Erase the list of pitches
+      pitches.pop_back();
+      if (pitches.empty()) return;
+
+      // Go through the list of pitches and look for any octaves to the current note, placing their accidentals
+      // in the same column
+      for (int j=0; j<pitches.size(); j++)
+      {
+         auto &possible_octave_to_pitch = pitches[j];
+         int possible_octave_pitch_staff_position = possible_octave_to_pitch.get_staff_position();
+         int possible_octave_pitch_staff_position_normalized = possible_octave_pitch_staff_position % 7;
+         int possible_octave_pitch_accidental_weight = possible_octave_to_pitch.calculate_accidental_weight();
+         bool is_an_octave_apart =
+               (possible_octave_pitch_staff_position_normalized == pitch_staff_position_normalized)
+               && (possible_octave_pitch_accidental_weight == pitch_accidental_weight);
+
+         // This pitch is an octave apart, place it in the same column
+         if (is_an_octave_apart)
+         {
+            AccidentalType accidental_type = find_accidental_type_by_weight(possible_octave_pitch_accidental_weight);
+            stack.push_back({ accidental_type, { column_to_place_on, possible_octave_pitch_staff_position } });
+
+            pitches.erase(pitches.begin() + j);
+            //i--;
+            j--;
+         }
+      }
+   return;
 }
 
 void AccidentalStacker::solve()
@@ -190,25 +192,38 @@ void AccidentalStacker::solve()
    bool top_bottom_toggle = true;
    int current_column = 0;
    //int last_ = 0;
-   for (int i=0; i<pitches.size(); i++)
+   int attempts_left = 300;
+   while (!pitches.empty())
    {
-      auto &pitch = pitches[i];
+      auto &pitch = pitches.front();
 
       // If pitch is not an accidental, remove it and continue
       if (!pitch.has_accidental())
       {
-         pitches.erase(pitches.begin() + i);
-         i--;
+         pitches.erase(pitches.begin() + 0);
          continue;
       }
 
-      solve_one_from_top();
+      // With this pitch that has an accidental, solve from either the top or the bottom (depending on the
+      // current solving direction
+      //if (top_bottom_toggle)
+      //{
+         solve_one_from_top(current_column);
+      //}
+      //else
+      //{
+         //solve_one_from_bottom(current_column);
+      //}
 
+      // Toggle the direction for the next pass
+      top_bottom_toggle = !top_bottom_toggle;
+
+      // Shift the column to the left by one
       current_column--;
 
-      if (!pitches.empty()) solve_one_from_bottom();
-
-      current_column--;
+      // Decrement out attempts_left counter to prevent an infinite loop
+      attempts_left--;
+      if (attempts_left <= 0) throw std::runtime_error("AccidentalStacker::error: too many attempts");
    }
 
    solved = true;
--- tests/AllegroFlare/MusicNotation/AccidentalStackerTest.cpp
+++ tests/AllegroFlare/MusicNotation/AccidentalStackerTest.cpp
@@ -30,14 +30,20 @@ TEST(AllegroFlare_MusicNotation_AccidentalStackerTest, solve__will_generate_the_
 {
    using namespace AllegroFlare::MusicNotation;
    std::vector<AllegroFlare::MusicNotation::Parser::PitchToken> pitches = {
-      { 3, -1, false },
+      { 3  , -1, false },
+      { 3+7, -1, false },
+      { 5,    1, false },
+      { 8,    0, true  },
    };
 
    AllegroFlare::MusicNotation::AccidentalStacker accidental_stacker(pitches);
    accidental_stacker.solve();
 
    std::vector<std::pair<AccidentalStacker::AccidentalType, std::pair<int, int>>> expected_stack = {
-      { AccidentalStacker::AccidentalType::FLAT, { 0, 3 } },
+      { AccidentalStacker::AccidentalType::FLAT,    { 0,  3   } },
+      { AccidentalStacker::AccidentalType::FLAT,    { 0,  3+7 } },
+      { AccidentalStacker::AccidentalType::SHARP,   { -1, 5   } },
+      { AccidentalStacker::AccidentalType::NATURAL, { -2, 8   } },
    };
    std::vector<std::pair<AccidentalStacker::AccidentalType, std::pair<int, int>>> actual_stack =
       accidental_stacker.get_stack();
