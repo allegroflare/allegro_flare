07eb9a5ed0c2d74e32e87ddca138dff9fcf66a3f
Mark Oates <markoates0@gmail.com>
2023-09-15 01:10:45 -0400

    * Add remaining accidental stacking test chords from reference material

--- tests/AllegroFlare/MusicNotation/MusicNotationTest.cpp
+++ tests/AllegroFlare/MusicNotation/MusicNotationTest.cpp
@@ -189,7 +189,8 @@ TEST_F(AllegroFlare_MusicNotation_MusicNotationTextWithAllegroRenderingFixture,
 
    // TODO: Add more test cases for accidental stacking. Check out:
    // https://blog.dorico.com/2014/03/development-diary-part-six/#:~:text=The%20basic%20rule%20for%20stacking,the%20fourth%20column%2C%20and%20so
-   std::string music_notation_content_string = "(+0 +2 =5 +'0) (+0 +2 =4 '+0)";
+   std::string music_notation_content_string =
+      "     (+0 +2 =5 +'0) (+0 +2 =4 '+0) (,6 '=1 +2 +4 6) (,=3 +5 '=0 =2 3) (,=1 +2 +4 =6 '=1)";
 
    music_notation.draw(1920/2-600, 1080/2, music_notation_content_string);
 
