488e9dc4ed420dd4f0159e4b0865c8d3c6f279bf
Mark Oates <markoates0@gmail.com>
2023-09-30 12:16:50 -0400

    * Add tests for #character_names_are_present#

--- tests/AllegroFlare/DialogTree/NodeBankInferencerTest.cpp
+++ tests/AllegroFlare/DialogTree/NodeBankInferencerTest.cpp
@@ -7,23 +7,33 @@
 #include <AllegroFlare/DialogTree/BasicScreenplayTextLoader.hpp>
 
 
-TEST(AllegroFlare_DialogTree_NodeBankInferencerTest, can_be_created_without_blowing_up)
+class AllegroFlare_DialogTree_NodeBankInferencerTest : public ::testing::Test
+{
+public:
+   AllegroFlare::DialogTree::NodeBank build_node_bank()
+   {
+      std::string script_text = AllegroFlare::php::file_get_contents(
+         "/Users/markoates/Repos/allegro_flare/tests/fixtures/dialogs/basic_screenplay_text.txt"
+      );
+      AllegroFlare::DialogTree::BasicScreenplayTextLoader basic_screenplay_text_loader;
+      basic_screenplay_text_loader.set_text(script_text);
+      basic_screenplay_text_loader.load();
+      AllegroFlare::DialogTree::NodeBank node_bank = basic_screenplay_text_loader.get_node_bank();
+      //ASSERT_EQ(12, node_bank.num_nodes());
+      return node_bank;
+   }
+};
+
+
+TEST_F(AllegroFlare_DialogTree_NodeBankInferencerTest, can_be_created_without_blowing_up)
 {
    AllegroFlare::DialogTree::NodeBankInferencer node_bank_inferencer;
 }
 
 
-TEST(AllegroFlare_DialogTree_NodeBankInferencerTest, run__returns_the_expected_response)
+TEST_F(AllegroFlare_DialogTree_NodeBankInferencerTest, run__returns_the_expected_response)
 {
-   std::string script_text = AllegroFlare::php::file_get_contents(
-      "/Users/markoates/Repos/allegro_flare/tests/fixtures/dialogs/basic_screenplay_text.txt"
-   );
-   AllegroFlare::DialogTree::BasicScreenplayTextLoader basic_screenplay_text_loader;
-   basic_screenplay_text_loader.set_text(script_text);
-   basic_screenplay_text_loader.load();
-   AllegroFlare::DialogTree::NodeBank node_bank = basic_screenplay_text_loader.get_node_bank();
-   ASSERT_EQ(12, node_bank.num_nodes());
-
+   AllegroFlare::DialogTree::NodeBank node_bank = build_node_bank();
    AllegroFlare::DialogTree::NodeBankInferencer node_bank_inferencer(&node_bank);
 
    std::vector<std::string> expected_speaking_characters = {
@@ -36,6 +46,35 @@ TEST(AllegroFlare_DialogTree_NodeBankInferencerTest, run__returns_the_expected_r
 
    EXPECT_EQ(expected_speaking_characters, actual_speaking_characters);
 
+}
+
+
+TEST_F(AllegroFlare_DialogTree_NodeBankInferencerTest,
+   character_names_are_present__when_all_the_names_are_present__will_return_true)
+{
+   AllegroFlare::DialogTree::NodeBank node_bank = build_node_bank();
+   AllegroFlare::DialogTree::NodeBankInferencer node_bank_inferencer(&node_bank);
+
+   std::vector<std::string> some_expected_character_names = {
+      "COMMISSIONER",
+      "BANKER",
+   };
+   EXPECT_EQ(true, node_bank_inferencer.character_names_are_present(some_expected_character_names));
+}
+
+
+TEST_F(AllegroFlare_DialogTree_NodeBankInferencerTest,
+   character_names_are_present__when_some_of_the_names_are_not_present__will_return_false)
+{
+   AllegroFlare::DialogTree::NodeBank node_bank = build_node_bank();
+   AllegroFlare::DialogTree::NodeBankInferencer node_bank_inferencer(&node_bank);
+
+   std::vector<std::string> some_expected_character_names = {
+      "COMMISSIONER",
+      "MR. MISSING",
+      "BANKER",
+   };
+   EXPECT_EQ(false, node_bank_inferencer.character_names_are_present(some_expected_character_names));
 }
 
 
