53e49b02d9f7499bd57a5a55336b5c66443aab0a
Mark Oates <markoates0@gmail.com>
2023-10-13 12:53:23 -0400

    * Render text and image in ThumbnailWithLabelRenderer

--- include/AllegroFlare/Elements/ChapterSelect/CarouselElementRenderers/ThumbnailWithLabelRenderer.hpp
+++ include/AllegroFlare/Elements/ChapterSelect/CarouselElementRenderers/ThumbnailWithLabelRenderer.hpp
@@ -4,6 +4,8 @@
 #include <AllegroFlare/BitmapBin.hpp>
 #include <AllegroFlare/Elements/ChapterSelect/CarouselElementRenderers/Base.hpp>
 #include <AllegroFlare/FontBin.hpp>
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_font.h>
 #include <string>
 
 
@@ -24,6 +26,9 @@ namespace AllegroFlare
                AllegroFlare::BitmapBin* bitmap_bin;
                AllegroFlare::FontBin* font_bin;
                std::string thumbnail_image_identifier;
+               std::string label_text;
+               ALLEGRO_BITMAP* obtain_thumbnail_image();
+               ALLEGRO_FONT* obtain_label_font();
 
             protected:
 
@@ -35,9 +40,11 @@ namespace AllegroFlare
                void set_bitmap_bin(AllegroFlare::BitmapBin* bitmap_bin);
                void set_font_bin(AllegroFlare::FontBin* font_bin);
                void set_thumbnail_image_identifier(std::string thumbnail_image_identifier);
+               void set_label_text(std::string label_text);
                AllegroFlare::BitmapBin* get_bitmap_bin() const;
                AllegroFlare::FontBin* get_font_bin() const;
                std::string get_thumbnail_image_identifier() const;
+               std::string get_label_text() const;
                void render();
             };
          }
--- src/AllegroFlare/Elements/ChapterSelect/CarouselElementRenderers/ThumbnailWithLabelRenderer.cpp
+++ src/AllegroFlare/Elements/ChapterSelect/CarouselElementRenderers/ThumbnailWithLabelRenderer.cpp
@@ -22,6 +22,7 @@ ThumbnailWithLabelRenderer::ThumbnailWithLabelRenderer()
    , bitmap_bin(nullptr)
    , font_bin(nullptr)
    , thumbnail_image_identifier("[unset-thumbnail_image_identifier]")
+   , label_text("[unset-label_text]")
 {
 }
 
@@ -49,6 +50,12 @@ void ThumbnailWithLabelRenderer::set_thumbnail_image_identifier(std::string thum
 }
 
 
+void ThumbnailWithLabelRenderer::set_label_text(std::string label_text)
+{
+   this->label_text = label_text;
+}
+
+
 AllegroFlare::BitmapBin* ThumbnailWithLabelRenderer::get_bitmap_bin() const
 {
    return bitmap_bin;
@@ -64,6 +71,12 @@ AllegroFlare::FontBin* ThumbnailWithLabelRenderer::get_font_bin() const
 std::string ThumbnailWithLabelRenderer::get_thumbnail_image_identifier() const
 {
    return thumbnail_image_identifier;
+}
+
+
+std::string ThumbnailWithLabelRenderer::get_label_text() const
+{
+   return label_text;
 }
 
 
@@ -83,7 +96,44 @@ void ThumbnailWithLabelRenderer::render()
       std::cerr << "\033[1;31m" << error_message.str() << " An exception will be thrown to halt the program.\033[0m" << std::endl;
       throw std::runtime_error("ThumbnailWithLabelRenderer::render: error: guard \"bitmap_bin\" not met");
    }
+   ALLEGRO_BITMAP *image = obtain_thumbnail_image();
+   ALLEGRO_FONT *label_font = obtain_label_font();
+   float label_y_offset = al_get_bitmap_height(image) + 30;
+   float label_text_max_width = 476;
+   float label_text_line_height = al_get_font_line_height(label_font);
+
+   // Render the image
+   al_draw_bitmap(image, 0, 0, 0);
+
+   // Render the text
+   al_draw_multiline_text(
+         label_font,
+         ALLEGRO_COLOR{1, 1, 1, 1},
+         0,
+         label_y_offset,
+         label_text_max_width,
+         label_text_line_height,
+         ALLEGRO_ALIGN_LEFT,
+         label_text.c_str()
+      );
    return;
+}
+
+ALLEGRO_BITMAP* ThumbnailWithLabelRenderer::obtain_thumbnail_image()
+{
+   return bitmap_bin->auto_get(thumbnail_image_identifier);
+}
+
+ALLEGRO_FONT* ThumbnailWithLabelRenderer::obtain_label_font()
+{
+   if (!(font_bin))
+   {
+      std::stringstream error_message;
+      error_message << "[ThumbnailWithLabelRenderer::obtain_label_font]: error: guard \"font_bin\" not met.";
+      std::cerr << "\033[1;31m" << error_message.str() << " An exception will be thrown to halt the program.\033[0m" << std::endl;
+      throw std::runtime_error("ThumbnailWithLabelRenderer::obtain_label_font: error: guard \"font_bin\" not met");
+   }
+   return font_bin->auto_get("Inter-Regular.ttf -26");
 }
 
 
