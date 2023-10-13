1d6fa5e040d9dc9eb087ed6b8bd55fcd7215acbb
Mark Oates <markoates0@gmail.com>
2023-10-13 12:55:00 -0400

    * Uncomment tests in ThumbnailWithLabelRenderer

--- tests/AllegroFlare/Elements/ChapterSelect/CarouselElementRenderers/ThumbnailWithLabelRendererTest.cpp
+++ tests/AllegroFlare/Elements/ChapterSelect/CarouselElementRenderers/ThumbnailWithLabelRendererTest.cpp
@@ -4,7 +4,6 @@
 #include <AllegroFlare/Elements/ChapterSelect/CarouselElementRenderers/ThumbnailWithLabelRenderer.hpp>
 
 
-
 using namespace AllegroFlare::Elements::ChapterSelect::CarouselElementRenderers;
 
 
@@ -15,19 +14,21 @@ TEST(AllegroFlare_Elements_ChapterSelect_CarouselElementRenderers_ThumbnailWithL
 }
 
 
-//TEST(AllegroFlare_Elements_ChapterSelect_CarouselElementRenderers_ThumbnailWithLabelRendererTest, TYPE__has_the_expected_value)
-//{
-   //EXPECT_STREQ(
-     //"AllegroFlare/Elements/ChapterSelect/CarouselElementRenderers/ThumbnailWithLabelRenderer",
-     //AllegroFlare::Elements::ChapterSelect::CarouselElementRenderers::ThumbnailWithLabelRenderer::TYPE
-   //);
-//}
+TEST(AllegroFlare_Elements_ChapterSelect_CarouselElementRenderers_ThumbnailWithLabelRendererTest,
+      TYPE__has_the_expected_value)
+{
+   EXPECT_STREQ(
+     "AllegroFlare/Elements/ChapterSelect/CarouselElementRenderers/ThumbnailWithLabelRenderer",
+     AllegroFlare::Elements::ChapterSelect::CarouselElementRenderers::ThumbnailWithLabelRenderer::TYPE
+   );
+}
 
 
-//TEST(AllegroFlare_Elements_ChapterSelect_CarouselElementRenderers_ThumbnailWithLabelRendererTest, type__has_the_expected_value_matching_TYPE)
-//{
-   //AllegroFlare::Elements::ChapterSelect::CarouselElementRenderers::ThumbnailWithLabelRenderer thumbnail_with_label_renderer;
-   //EXPECT_EQ(AllegroFlare::Elements::ChapterSelect::CarouselElementRenderers::ThumbnailWithLabelRenderer::TYPE, thumbnail_with_label_renderer.get_type());
-//}
+TEST(AllegroFlare_Elements_ChapterSelect_CarouselElementRenderers_ThumbnailWithLabelRendererTest,
+      type__has_the_expected_value_matching_TYPE)
+{
+   ThumbnailWithLabelRenderer thumbnail_with_label_renderer;
+   EXPECT_EQ(ThumbnailWithLabelRenderer::TYPE, thumbnail_with_label_renderer.get_type());
+}
 
 
