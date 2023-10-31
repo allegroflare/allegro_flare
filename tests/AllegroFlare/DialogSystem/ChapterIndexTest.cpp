b84049c354c7f128f82de85d48f6a8455955b88c
Mark Oates <markoates0@gmail.com>
2023-10-31 12:35:43 -0400

    * Add test to ensure ordering is preserved when items are inserted

--- tests/AllegroFlare/DialogSystem/ChapterIndexTest.cpp
+++ tests/AllegroFlare/DialogSystem/ChapterIndexTest.cpp
@@ -26,3 +26,28 @@ TEST(AllegroFlare_DialogSystem_ChapterIndexTest, chapters__retains_order_when_se
 }
 
 
+TEST(AllegroFlare_DialogSystem_ChapterIndexTest, chapters__retains_order_when_items_are_inserted)
+{
+   AllegroFlare::DialogSystem::ChapterIndex chapter_index;
+   chapter_index.set_chapters({
+      { "Chapter 1: One", nullptr },
+      { "Chapter 0: Zero", nullptr },
+      { "Chapter 4: Four", nullptr },
+   });
+
+   chapter_index.add_chapter("Zoo", nullptr);
+   chapter_index.add_chapter("Advent", nullptr);
+
+   std::vector<std::string> expected_ordered_identifiers = {
+      "Chapter 1: One",
+      "Chapter 0: Zero",
+      "Chapter 4: Four",
+      "Zoo",
+      "Advent",
+   };
+   std::vector<std::string> actual_identifiers = chapter_index.get_chapter_identifiers();
+
+   EXPECT_EQ(expected_ordered_identifiers, actual_identifiers);
+}
+
+
