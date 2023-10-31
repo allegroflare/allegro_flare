0f6fdd52030ebbcac48784e5edd45fa2b8962b25
Mark Oates <markoates0@gmail.com>
2023-10-31 18:59:46 -0400

    * Remove dead function from BasicCharacterDialogDriver

--- include/AllegroFlare/DialogSystemDrivers/Base.hpp
+++ include/AllegroFlare/DialogSystemDrivers/Base.hpp
@@ -1,7 +1,6 @@
 #pragma once
 
 
-#include <AllegroFlare/DialogTree/Nodes/Base.hpp>
 #include <AllegroFlare/ForwardDeclarations/AllegroFlare/DialogSystem/DialogSystem.hpp>
 #include <functional>
 #include <string>
@@ -36,7 +35,6 @@ namespace AllegroFlare
          virtual void on_switch_in();
          virtual void on_deactivate();
          virtual void on_render();
-         virtual bool activate_dialog_node_by_name(AllegroFlare::DialogSystem::DialogSystem* dialog_system=nullptr, std::string active_dialog_node_name="[unset-dialog_node_identifier]", AllegroFlare::DialogTree::Nodes::Base* active_dialog_node=nullptr, void* user_data=nullptr);
          bool is_type(std::string possible_type="");
       };
    }
--- include/AllegroFlare/DialogSystemDrivers/BasicCharacterDialogDriver.hpp
+++ include/AllegroFlare/DialogSystemDrivers/BasicCharacterDialogDriver.hpp
@@ -6,12 +6,9 @@
 #include <AllegroFlare/DialogSystem/CharacterRoster.hpp>
 #include <AllegroFlare/DialogSystem/CharacterStagingLayouts/Base.hpp>
 #include <AllegroFlare/DialogSystem/Characters/Basic.hpp>
-#include <AllegroFlare/DialogSystem/DialogSystem.hpp>
 #include <AllegroFlare/DialogSystem/SceneIndex.hpp>
 #include <AllegroFlare/DialogSystemDrivers/Base.hpp>
-#include <AllegroFlare/DialogTree/Nodes/Base.hpp>
 #include <AllegroFlare/Elements/DialogRoll.hpp>
-#include <allegro5/allegro.h>
 #include <string>
 
 
@@ -50,9 +47,6 @@ namespace AllegroFlare
          virtual void on_deactivate() override;
          virtual void on_render() override;
          void clear_character_staging_layout();
-         virtual bool activate_dialog_node_by_name(AllegroFlare::DialogSystem::DialogSystem* dialog_system=nullptr, std::string active_dialog_node_name="[unset-dialog_node_identifier]", AllegroFlare::DialogTree::Nodes::Base* active_dialog_node=nullptr, void* user_data=nullptr) override;
-         void set_speaking_character_avatar(std::string speaking_character_identifier="[unset-speaking_character_identifier]", std::string speaking_character_expression="[unset-speaking_character_expression]");
-         ALLEGRO_BITMAP* lookup_speaking_character_avatar(std::string speaking_character_identifier="[unset-speaking_character_identifier]", std::string speaking_character_expression="[unset-speaking_character_expression]");
          AllegroFlare::DialogSystem::Characters::Basic* find_character_by_identifier_as_Basic(std::string character_identifier="[unset-character_identifier]");
          void append_to_dialog_roll(std::string speaking_character="[unset-speaking_character]", std::string dialog="[unset-dialog]");
       };
