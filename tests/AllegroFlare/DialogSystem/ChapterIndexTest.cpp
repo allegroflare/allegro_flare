eba3db21e4b113454a7325f08ebed90006da9729
Mark Oates <markoates0@gmail.com>
2023-10-31 12:30:39 -0400

    * Add test to ensure ChapterIndex retains ordering when set

--- tests/AllegroFlare/DialogSystem/ChapterIndexTest.cpp
+++ tests/AllegroFlare/DialogSystem/ChapterIndexTest.cpp
@@ -10,3 +10,19 @@ TEST(AllegroFlare_DialogSystem_ChapterIndexTest, can_be_created_without_blowing_
 }
 
 
+TEST(AllegroFlare_DialogSystem_ChapterIndexTest, chapters__retains_order_when_set)
+{
+   AllegroFlare::DialogSystem::ChapterIndex chapter_index;
+   chapter_index.set_chapters({
+      { "Chapter 1: One", nullptr },
+      { "Chapter 0: Zero", nullptr },
+      { "Chapter 4: Four", nullptr },
+   });
+
+   std::vector<std::string> expected_ordered_identifiers = { "Chapter 1: One", "Chapter 0: Zero", "Chapter 4: Four" };
+   std::vector<std::string> actual_identifiers = chapter_index.get_chapter_identifiers();
+
+   EXPECT_EQ(expected_ordered_identifiers, actual_identifiers);
+}
+
+
