e31d0635cfc3b26c8660f54d75a8280d598911d5
Mark Oates <markoates0@gmail.com>
2023-11-06 12:47:01 -0500

    * Add AllegroFlare/UnicodeCharacters

---
+++ include/AllegroFlare/UnicodeCharacters.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+
+#include <string>
+
+
+namespace AllegroFlare
+{
+   class UnicodeCharacters
+   {
+   public:
+      static constexpr char* COPYRIGHT_CHARACTER = (char*)"";
+   };
+}
+
+
+
---
+++ src/AllegroFlare/UnicodeCharacters.cpp
@@ -0,0 +1,6 @@
+
+
+// This is header only
+//#include <AllegroFlare/UnicodeCharacters.hpp>
+
+
