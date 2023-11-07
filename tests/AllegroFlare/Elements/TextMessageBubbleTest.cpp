49065a9986cd79b8eeb07575092c3ffbc9306381
Mark Oates <markoates0@gmail.com>
2023-11-07 12:26:08 -0500

    * Fix labeling of tests for TextMessageBubble

--- tests/AllegroFlare/Elements/TextMessageBubbleTest.cpp
+++ tests/AllegroFlare/Elements/TextMessageBubbleTest.cpp
@@ -6,19 +6,19 @@
 #include <AllegroFlare/Testing/WithAllegroRenderingFixture.hpp>
 
 
-class Krampus21_SmartPhoneDialogBubbleTest : public ::testing::Test {};
-class Krampus21_SmartPhoneDialogBubbleWithAllegroRenderingFixtureTest
+class AllegroFlare_Elements_TextMessageBubbleTest : public ::testing::Test {};
+class AllegroFlare_Elements_TextMessageBubbleWithAllegroRenderingFixtureTest
    : public AllegroFlare::Testing::WithAllegroRenderingFixture
 {};
 
 
-TEST(AllegroFlare_Elements_TextMessageBubbleTest, can_be_created_without_blowing_up)
+TEST_F(AllegroFlare_Elements_TextMessageBubbleTest, can_be_created_without_blowing_up)
 {
    AllegroFlare::Elements::TextMessageBubble text_message_bubble;
 }
 
 
-TEST_F(Krampus21_SmartPhoneDialogBubbleWithAllegroRenderingFixtureTest, can_be_created_without_blowing_up)
+TEST_F(AllegroFlare_Elements_TextMessageBubbleWithAllegroRenderingFixtureTest, can_be_created_without_blowing_up)
 {
    AllegroFlare::Elements::TextMessageBubble dialog_bubble(
       &get_font_bin_ref(),
