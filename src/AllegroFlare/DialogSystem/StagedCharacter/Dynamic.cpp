f7a973bda74339792cae4d1cd838bfee703324ed
Mark Oates <markoates0@gmail.com>
2023-11-12 16:41:01 -0500

    * Add guard on set_state for initialized allegro

--- src/AllegroFlare/DialogSystem/StagedCharacter/Dynamic.cpp
+++ src/AllegroFlare/DialogSystem/StagedCharacter/Dynamic.cpp
@@ -132,6 +132,13 @@ void Dynamic::hide()
 
 void Dynamic::set_state(uint32_t state, bool override_if_busy)
 {
+   if (!(al_is_system_installed()))
+   {
+      std::stringstream error_message;
+      error_message << "[Dynamic::set_state]: error: guard \"al_is_system_installed()\" not met.";
+      std::cerr << "\033[1;31m" << error_message.str() << " An exception will be thrown to halt the program.\033[0m" << std::endl;
+      throw std::runtime_error("Dynamic::set_state: error: guard \"al_is_system_installed()\" not met");
+   }
    if (!(is_valid_state(state)))
    {
       std::stringstream error_message;
