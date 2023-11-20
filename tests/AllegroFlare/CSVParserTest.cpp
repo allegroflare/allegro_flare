9467eda3e4b29b0c9347db5d640953f8d95b62f7
Mark Oates <markoates0@gmail.com>
2023-11-20 10:10:23 -0500

    * Disable test/support for double quotes in quoted tokens

--- tests/AllegroFlare/CSVParserTest.cpp
+++ tests/AllegroFlare/CSVParserTest.cpp
@@ -28,16 +28,25 @@ TEST(AllegroFlare_CSVParserTest, parse_row__returns_the_row_parsed_into_tokens)
 }
 
 
-TEST(AllegroFlare_CSVParserTest, parse_row__will_account_for_quoted_tokens_and_escaped_quotes_within_quoted_tokens)
+TEST(AllegroFlare_CSVParserTest,
+   DISABLED__parse_row__will_account_for_quoted_tokens_and_escaped_quotes_within_quoted_tokens)
 {
+   // TODO: Enable quotes within quoted string tokens
    AllegroFlare::CSVParser csv_parser;
-   std::string line = "John,Doe,\"123, Main St\",\"This is a \\\"\"quoted\\\"\" string\",42";
+   std::string line = "John,Doe,\"123, Main St\",\"This is a \"\"quoted\"\" string\",42";
    std::vector<std::string> columns = csv_parser.parse_row(line);
 
    EXPECT_EQ(5, columns.size());
 
-   //TODO: Assert parsed_tokens
+   std::vector<std::string> expected_parsed_tokens = {
+      "John",
+      "Doe",
+      "123, Main St",
+      "This is a \"quoted\" string",
+      "42"
+   };
 
+   EXPECT_EQ(columns, expected_parsed_tokens);
    // Debug: Print the parsed columns
    for (const auto& col : columns)
    {
