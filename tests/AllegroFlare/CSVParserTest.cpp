d39b1080d783c843bed8b580d7e61fe90f88eb88
Mark Oates <markoates0@gmail.com>
2023-11-20 09:50:55 -0500

    * Add new incomplete test cases, with skips

--- tests/AllegroFlare/CSVParserTest.cpp
+++ tests/AllegroFlare/CSVParserTest.cpp
@@ -12,11 +12,35 @@ TEST(AllegroFlare_CSVParserTest, can_be_created_without_blowing_up)
 
 TEST(AllegroFlare_CSVParserTest, parse_row__returns_the_row_parsed_into_tokens)
 {
+   GTEST_SKIP();
    AllegroFlare::CSVParser csv_parser;
    std::string line = "Name,Age,Address,\"City, State\",Country";
    std::vector<std::string> columns = csv_parser.parse_row(line);
 
-   // Print the parsed columns
+   EXPECT_EQ(5, columns.size());
+
+   //TODO: Assert parsed_tokens
+
+   // Debug: Print the parsed columns
+   for (const auto& col : columns)
+   {
+       std::cout << col << std::endl;
+   }
+}
+
+
+TEST(AllegroFlare_CSVParserTest, parse_row__will_account_for_quoted_tokens_and_escaped_quotes_within_quoted_tokens)
+{
+   GTEST_SKIP();
+   AllegroFlare::CSVParser csv_parser;
+   std::string line = "John,Doe,\"123, Main St\",\"This is a \\\"\"quoted\\\"\" string\",42";
+   std::vector<std::string> columns = csv_parser.parse_row(line);
+
+   EXPECT_EQ(5, columns.size());
+
+   //TODO: Assert parsed_tokens
+
+   // Debug: Print the parsed columns
    for (const auto& col : columns)
    {
        std::cout << col << std::endl;
