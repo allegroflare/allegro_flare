9cfa98d3abf21849f5023c5c5b22f08faec78edb
Mark Oates <markoates0@gmail.com>
2023-11-27 15:13:13 -0500

    * Add throw on unhandled type in WorldMapRenderers/Basic

--- src/AllegroFlare/WorldMapRenderers/Basic.cpp
+++ src/AllegroFlare/WorldMapRenderers/Basic.cpp
@@ -61,7 +61,10 @@ void Basic::render_location(AllegroFlare::WorldMaps::Locations::Base* location)
    }
    else
    {
-      // TODO: Throw, Could not handle this type
+      AllegroFlare::Logger::throw_unhandled_case(
+         "AllegroFlare::WorldMapRenderers::Basic::render_location",
+         location->get_type()
+      );
    }
    return;
 }
@@ -108,26 +111,6 @@ void Basic::render()
       render_location(location.second);
    }
    return;
-   float x = 1920/2;
-   float y = 1080/3;
-   ALLEGRO_FONT *font = obtain_font();
-   float text_width = al_get_text_width(font, quote.c_str());
-   float text_height = al_get_font_line_height(font);
-   float h_text_width = text_width/2;
-   float h_text_height = text_height/2;
-   AllegroFlare::Vec2D padding = {30, 20};
-
-   al_draw_rounded_rectangle(
-      x-h_text_width - padding.x,
-      y-h_text_height - padding.y,
-      x+h_text_width + padding.x,
-      y+h_text_height + padding.y,
-      8.0f,
-      8.0f,
-      ALLEGRO_COLOR{1, 1, 1, 1},
-      8.0f
-   );
-   al_draw_text(font, ALLEGRO_COLOR{1, 1, 1, 1}, x, y-h_text_height, ALLEGRO_ALIGN_CENTER, quote.c_str());
    return;
 }
 
