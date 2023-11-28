90e155550aeeee52b8a5f7ee2b9e1f1494cd9bd1
Mark Oates <markoates0@gmail.com>
2023-11-28 13:43:16 -0500

    * Add test fixture in WorldMaps/Maps/Basic

--- tests/AllegroFlare/WorldMaps/Maps/BasicTest.cpp
+++ tests/AllegroFlare/WorldMaps/Maps/BasicTest.cpp
@@ -22,7 +22,33 @@ public:
 };
 
 
-TEST(AllegroFlare_WorldMaps_Maps_BasicTest, MyTestLocation_collides__has_correct_collision_bounds)
+class AllegroFlare_WorldMaps_Maps_BasicTest : public ::testing::Test {};
+class AllegroFlare_WorldMaps_Maps_BasicTestWithMapFixture : public ::testing::Test
+{
+public:
+   AllegroFlare::WorldMaps::Maps::Basic map;
+   virtual void SetUp() override
+   {
+      ::testing::Test::SetUp();
+      map.set_locations({
+         { "home",   new MyTestLocation(20, 20) },
+         { "office", new MyTestLocation(80, 120) },
+         { "store",  new MyTestLocation(-100, -30) },
+      });
+   }
+   virtual void TearDown() override
+   {
+      for (auto &location : map.get_locations())
+      {
+         delete location.second;
+         location.second = nullptr;
+      }
+      ::testing::Test::TearDown();
+   }
+};
+
+
+TEST_F(AllegroFlare_WorldMaps_Maps_BasicTest, MyTestLocation_collides__has_correct_collision_bounds)
 {
    auto location = MyTestLocation(0, 0);
 
@@ -42,13 +68,13 @@ TEST(AllegroFlare_WorldMaps_Maps_BasicTest, MyTestLocation_collides__has_correct
 }
 
 
-TEST(AllegroFlare_WorldMaps_Maps_BasicTest, can_be_created_without_blowing_up)
+TEST_F(AllegroFlare_WorldMaps_Maps_BasicTest, can_be_created_without_blowing_up)
 {
    AllegroFlare::WorldMaps::Maps::Basic basic;
 }
 
 
-TEST(AllegroFlare_WorldMaps_Maps_BasicTest, TYPE__has_the_expected_value)
+TEST_F(AllegroFlare_WorldMaps_Maps_BasicTest, TYPE__has_the_expected_value)
 {
    EXPECT_STREQ(
      "AllegroFlare/WorldMaps/Maps/Basic",
@@ -57,14 +83,14 @@ TEST(AllegroFlare_WorldMaps_Maps_BasicTest, TYPE__has_the_expected_value)
 }
 
 
-TEST(AllegroFlare_WorldMaps_Maps_BasicTest, type__has_the_expected_value_matching_TYPE)
+TEST_F(AllegroFlare_WorldMaps_Maps_BasicTest, type__has_the_expected_value_matching_TYPE)
 {
    AllegroFlare::WorldMaps::Maps::Basic basic;
    EXPECT_EQ(AllegroFlare::WorldMaps::Maps::Basic::TYPE, basic.get_type());
 }
 
 
-TEST(AllegroFlare_WorldMaps_Maps_BasicTest, location_id_at__will_return_the_id_of_the_location_at_that_point)
+TEST_F(AllegroFlare_WorldMaps_Maps_BasicTest, location_id_at__will_return_the_id_of_the_location_at_that_point)
 {
    AllegroFlare::WorldMaps::Maps::Basic basic;
    basic.set_locations({
@@ -76,6 +102,13 @@ TEST(AllegroFlare_WorldMaps_Maps_BasicTest, location_id_at__will_return_the_id_o
    EXPECT_EQ("home", basic.location_id_at(20+2, 20+4));
    EXPECT_EQ("office", basic.location_id_at(80-4, 120+2));
    EXPECT_EQ("store", basic.location_id_at(-100-4, -30-4));
+}
+
+
+TEST_F(AllegroFlare_WorldMaps_Maps_BasicTestWithMapFixture,
+   primary_point_of_interest_is_set__if_the_primary_point_of_interest_is_not_the_default__returns_true)
+{
+   // TODO: Add test here
 }
 
 
