8e44ce6fcc6b9183dab6eb09aca94335aedd4c3c
Mark Oates <markoates0@gmail.com>
2024-01-12 15:46:58 -0500

    * Add basic test on #create_tile_map# in factory

--- tests/AllegroFlare/Prototypes/Platforming2D/Entities/Basic2DFactoryTest.cpp
+++ tests/AllegroFlare/Prototypes/Platforming2D/Entities/Basic2DFactoryTest.cpp
@@ -49,3 +49,43 @@ TEST_F(AllegroFlare_Prototypes_Platforming2D_Entities_Basic2DFactoryWithAllegroR
 }
 
 
+TEST_F(AllegroFlare_Prototypes_Platforming2D_Entities_Basic2DFactoryWithAllegroRenderingFixtureTest,
+   create_tile_map__will_create_a_map_from_the_map_json_filename)
+   //INTERACTIVE__in_an_AllegroFlare_Frameworks_Full_context__will_run_as_expected)
+   //DISABLED__INTERACTIVE__in_an_AllegroFlare_Frameworks_Full_context__will_run_as_expected)
+{
+   //AllegroFlare::Frameworks::Full framework;
+   //framework.set_deployment_environment("test");
+   //framework.disable_auto_created_config_warning();
+   //framework.disable_clear_render_surface_before_calling_primary_timer_funcs();
+   //framework.disable_fullscreen();
+   //framework.initialize();
+
+   std::string data_folder_path = TEST_FIXTURES_PATH;
+
+
+   //AllegroFlare::Prototypes::Platforming2D::Screen platforming_2d;
+   //platforming_2d.set_bitmap_bin(&framework.get_bitmap_bin_ref());
+   //platforming_2d.set_display(framework.get_primary_display());
+   //platforming_2d.set_event_emitter(&framework.get_event_emitter_ref());
+
+   AllegroFlare::Prototypes::Platforming2D::Entities::Basic2DFactory basic2d_factory(&get_bitmap_bin_ref());
+   AllegroFlare::Prototypes::Platforming2D::Entities::TileMaps::Basic2D *created_tile_map =
+      basic2d_factory.create_tile_map(TEST_FIXTURES_PATH "maps/map1-0x.tmj", "map_a");
+
+
+   //platforming_2d.set_map_dictionary({
+      //{ "map_a", data_folder_path + "maps/map1-0x.tmj" },
+      //{ "map_b", data_folder_path + "maps/map1b-0x.tmj" },
+   //});
+   //platforming_2d.initialize_maps();
+   //platforming_2d.set_currently_active_map("map_a");
+
+   // create some entities
+
+
+
+
+   // create an animation book (to create an frame_animated type from the factory)
+};
+
