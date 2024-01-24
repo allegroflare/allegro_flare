6faf9aab91adc4af30c153ae10f1032fc5205d04
Mark Oates <markoates0@gmail.com>
2024-01-24 10:50:53 -0500

    * Improve comment

--- include/AllegroFlare/Predeclare.hpp
+++ include/AllegroFlare/Predeclare.hpp
@@ -1,8 +1,11 @@
 #pragma once
 
 
-// TODO: Renam this file and move it to a better location
+// TODO: Rename this file and move it to a better location
 
+// NOTE: That they should be placed separately into:
+//    - AllegroFlare/ForwardDeclarations/AllegroFlare/GameConfigurations/Complete
+//    - AllegroFlare/ForwardDeclarations/AllegroFlare/Runners/Complete
 
 namespace AllegroFlare {
    namespace GameConfigurations {
