d14d5f593e9123d4524d557deba173c8eb0325f4
Mark Oates <markoates0@gmail.com>
2024-01-25 10:13:45 -0500

    * Add some ForwardDeclaration classes

---
+++ tests/AllegroFlare/ForwardDeclarations/AllegroFlare/Runners/CompleteTest.cpp
@@ -0,0 +1,12 @@
+
+#include <gtest/gtest.h>
+
+#include <AllegroFlare/ForwardDeclarations/AllegroFlare/Runners/Complete.hpp>
+
+
+TEST(AllegroFlare_ForwardDeclarations_AllegroFlare_Runners_Complete, class_name_is_declared)
+{
+   // TODO: See if there is a way to run this test
+}
+
+
