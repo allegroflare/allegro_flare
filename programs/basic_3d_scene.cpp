2869369149164d9ff0f497c4b0c29c56389ed8ec
Mark Oates <markoates0@gmail.com>
2024-01-26 12:13:52 -0500

    Tweak programs/basic_3d_scene to take arguments

--- programs/basic_3d_scene.cpp
+++ programs/basic_3d_scene.cpp
@@ -1,26 +1,17 @@
 #include <allegro5/allegro.h>
 #include <AllegroFlare/Frameworks/Full.hpp>
 #include <AllegroFlare/GraphicsPipelines/Basic3D/Scene.hpp>
-//#include <allegro5/allegro_image.h>
-//#include <AllegroFlare/Model3DObjLoader.hpp>
-//#include <AllegroFlare/Frameworks/Full.hpp>
-//#include <AllegroFlare/Screens/Base.hpp>
-//#include <AllegroFlare/Model3D.hpp>
-//#include <AllegroFlare/Camera3D.hpp>
-//#include <AllegroFlare/Placement3D.hpp>
-//#include <cmath>
-//#include <iostream>
-//#include <cmath>
 
 
 class Basic3DScene : public AllegroFlare::Screens::Base
 {
 public:
    AllegroFlare::BitmapBin *bitmap_bin;
-   AllegroFlare::ModelBin  *model_bin;
+   AllegroFlare::ModelBin *model_bin;
+   AllegroFlare::GraphicsPipelines::Basic3D::Scene scene;
+   AllegroFlare::Camera3D *camera;
 
 private:
-   AllegroFlare::GraphicsPipelines::Basic3D::Scene scene;
    bool initialized;
 
 public:
@@ -29,19 +20,32 @@ public:
       , bitmap_bin(nullptr)
       , model_bin(nullptr)
       , scene()
+      , camera(nullptr)
       , initialized(false)
    {};
 
    void initialize()
    {
+      // Setup
       scene.set_bitmap_bin(bitmap_bin);
       scene.set_model_bin(model_bin);
       scene.initialize();
+      camera = &scene.get_camera_ref();
+
       initialized = true;
+   }
+
+   void add_entity(std::string obj, std::string tex)
+   {
+      scene.add_entity(obj, tex);
    }
 
    virtual void primary_timer_func() override
    {
+      // Update
+      camera->spin += 0.01;
+
+      // Render
       scene.render();
    }
 };
@@ -55,22 +59,26 @@ int main(int argc, char **argv)
    // init AllegroFlare
    AllegroFlare::Frameworks::Full framework;
    framework.initialize();
+   framework.get_bitmap_bin_ref().set_full_path("/");
+   framework.get_model_bin_ref().set_full_path("/");
 
    Basic3DScene basic_3d;
+   std::string obj = "foo.obj";
+   std::string tex = "foo.png";
    if (args.size() == 2)
    {
-      //basic_3d.set_model_obj_filename(args[0]);
-      //basic_3d.set_model_texture_filename(args[1]);
+      obj = args[0];
+      tex = args[1];
    }
    basic_3d.bitmap_bin = &framework.get_bitmap_bin_ref();
    basic_3d.model_bin = &framework.get_model_bin_ref();
    basic_3d.initialize();
+   basic_3d.add_entity(obj, tex);
 
    framework.register_and_activate_screen("basic_3d_scene", &basic_3d);
 
    framework.run_loop();
    return 0;
 }
-
 
 
