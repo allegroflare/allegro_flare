65b03f729405a4e5619a7eec432673b8ea6803d2
Mark Oates <markoates0@gmail.com>
2024-01-30 12:53:47 -0500

    * Add initial test for TMJObjectLoaderObjectCustomProperties

--- tests/AllegroFlare/Prototypes/Platforming2D/TMJObjectLoaderObjectCustomPropertiesTest.cpp
+++ tests/AllegroFlare/Prototypes/Platforming2D/TMJObjectLoaderObjectCustomPropertiesTest.cpp
@@ -6,7 +6,21 @@
 
 TEST(AllegroFlare_Prototypes_Platforming2D_TMJObjectLoaderObjectCustomPropertiesTest, can_be_created_without_blowing_up)
 {
-   AllegroFlare::Prototypes::Platforming2D::TMJObjectLoaderObjectCustomProperties tmjobject_loader_object_custom_properties;
+   AllegroFlare::Prototypes::Platforming2D::TMJObjectLoaderObjectCustomProperties custom_properties;
+}
+
+
+TEST(AllegroFlare_Prototypes_Platforming2D_TMJObjectLoaderObjectCustomPropertiesTest,
+   will_store_objects_of_a_string_type)
+{
+   AllegroFlare::Prototypes::Platforming2D::TMJObjectLoaderObjectCustomProperties custom_properties;
+
+   custom_properties.add("my_property", "foo");
+
+   EXPECT_EQ(true, custom_properties.exists("my_property"));
+   //EXPECT_EQ("string", custom_properties.get_type("my_property"));
+   //EXPECT_EQ(true, custom_properties.is_string("my_property"));
+   //EXPECT_EQ("foo", custom_properties.get_string("my_property"));
 }
 
 
