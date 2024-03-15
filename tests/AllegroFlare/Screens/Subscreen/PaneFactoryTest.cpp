64411184a362d9a7082055b0bf56dd4cfd14d030
Mark Oates <markoates0@gmail.com>
2024-03-15 16:16:41 -0400

    * Remove dead factory test

--- tests/AllegroFlare/Screens/Subscreen/PaneFactoryTest.cpp
+++ tests/AllegroFlare/Screens/Subscreen/PaneFactoryTest.cpp
@@ -10,11 +10,3 @@ TEST(AllegroFlare_Screens_Subscreen_PaneFactoryTest, can_be_created_without_blow
 }
 
 
-TEST(AllegroFlare_Screens_Subscreen_PaneFactoryTest, run__returns_the_expected_response)
-{
-   AllegroFlare::Screens::Subscreen::PaneFactory pane_factory;
-   std::string expected_string = "Hello World!";
-   EXPECT_EQ(expected_string, pane_factory.run());
-}
-
-
