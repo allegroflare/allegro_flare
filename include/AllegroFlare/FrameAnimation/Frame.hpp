a9305a6813c78e4da3152c486e25452b21fc537e
Mark Oates <markoates0@gmail.com>
2024-03-31 13:27:03 -0400

    * Add #align_x#, #align_y#, #anchor_x#, #anchor_y# to FrameAnimation/Frame

--- include/AllegroFlare/FrameAnimation/Frame.hpp
+++ include/AllegroFlare/FrameAnimation/Frame.hpp
@@ -13,16 +13,24 @@ namespace AllegroFlare
       private:
          uint32_t index;
          float duration;
+         float align_x;
+         float align_y;
+         float anchor_x;
+         float anchor_y;
 
       protected:
 
 
       public:
-         Frame(uint32_t index=0, float duration=0.2f);
+         Frame(uint32_t index=0, float duration=0.2f, float align_x=0.5f, float align_y=0.5f, float anchor_x=0.0f, float anchor_y=0.0f);
          ~Frame();
 
          uint32_t get_index() const;
          float get_duration() const;
+         float get_align_x() const;
+         float get_align_y() const;
+         float get_anchor_x() const;
+         float get_anchor_y() const;
       };
    }
 }
--- src/AllegroFlare/FrameAnimation/Frame.cpp
+++ src/AllegroFlare/FrameAnimation/Frame.cpp
@@ -11,9 +11,13 @@ namespace FrameAnimation
 {
 
 
-Frame::Frame(uint32_t index, float duration)
+Frame::Frame(uint32_t index, float duration, float align_x, float align_y, float anchor_x, float anchor_y)
    : index(index)
    , duration(duration)
+   , align_x(align_x)
+   , align_y(align_y)
+   , anchor_x(anchor_x)
+   , anchor_y(anchor_y)
 {
 }
 
@@ -32,6 +36,30 @@ uint32_t Frame::get_index() const
 float Frame::get_duration() const
 {
    return duration;
+}
+
+
+float Frame::get_align_x() const
+{
+   return align_x;
+}
+
+
+float Frame::get_align_y() const
+{
+   return align_y;
+}
+
+
+float Frame::get_anchor_x() const
+{
+   return anchor_x;
+}
+
+
+float Frame::get_anchor_y() const
+{
+   return anchor_y;
 }
 
 
