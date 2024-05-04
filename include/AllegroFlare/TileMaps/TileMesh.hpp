124fa15606372bbcf6d3b173dab0b6ce94fe8f34
Mark Oates <markoates0@gmail.com>
2024-05-04 12:06:05 -0400

    * Add method signatures to TileMaps/TileMesh

--- include/AllegroFlare/TileMaps/TileMesh.hpp
+++ include/AllegroFlare/TileMaps/TileMesh.hpp
@@ -29,10 +29,32 @@ namespace AllegroFlare
 
 
       public:
-         TileMesh();
+         TileMesh(AllegroFlare::TileMaps::PrimMeshAtlas* atlas=nullptr, int num_columns=0, int num_rows=0, int tile_width=1, int tile_height=1);
          ~TileMesh();
 
+         void set_atlas(AllegroFlare::TileMaps::PrimMeshAtlas* atlas);
+         AllegroFlare::TileMaps::PrimMeshAtlas* get_atlas() const;
+         std::vector<int> get_tile_ids() const;
+         int get_num_columns() const;
+         int get_num_rows() const;
+         int get_tile_width() const;
+         int get_tile_height() const;
+         bool get_initialized() const;
+         std::vector<ALLEGRO_VERTEX> &get_vertexes_ref();
          void initialize();
+         void resize(int num_columns=0, int num_rows=0);
+         void render(bool draw_outline=false);
+         void set_tile_id(int tile_x=0, int tile_y=0, int tile_id=0);
+         int get_tile_id(int tile_x=0, int tile_y=0);
+         void set_tile_uv(int tile_x=0, int tile_y=0, int u1=0, int v1=0, int u2=0, int v2=0);
+         int infer_num_vertexes();
+         int infer_num_tiles();
+         void rescale_tile_dimensions_to(int new_tile_width=0, int new_tile_height=0);
+         void set_tile_width(int tile_width=0);
+         void set_tile_height(int tile_width=0);
+         int get_real_width();
+         int get_real_height();
+         void swap_yz();
       };
    }
 }
--- src/AllegroFlare/TileMaps/TileMesh.cpp
+++ src/AllegroFlare/TileMaps/TileMesh.cpp
@@ -13,15 +13,15 @@ namespace TileMaps
 {
 
 
-TileMesh::TileMesh()
-   : atlas(nullptr)
+TileMesh::TileMesh(AllegroFlare::TileMaps::PrimMeshAtlas* atlas, int num_columns, int num_rows, int tile_width, int tile_height)
+   : atlas(atlas)
    , vertexes()
    , vertex_buffer(nullptr)
    , tile_ids({})
-   , num_columns(0)
-   , num_rows(0)
-   , tile_width(1)
-   , tile_height(1)
+   , num_columns(num_columns)
+   , num_rows(num_rows)
+   , tile_width(tile_width)
+   , tile_height(tile_height)
    , yz_swapped(false)
    , initialized(false)
 {
@@ -30,6 +30,60 @@ TileMesh::TileMesh()
 
 TileMesh::~TileMesh()
 {
+}
+
+
+void TileMesh::set_atlas(AllegroFlare::TileMaps::PrimMeshAtlas* atlas)
+{
+   this->atlas = atlas;
+}
+
+
+AllegroFlare::TileMaps::PrimMeshAtlas* TileMesh::get_atlas() const
+{
+   return atlas;
+}
+
+
+std::vector<int> TileMesh::get_tile_ids() const
+{
+   return tile_ids;
+}
+
+
+int TileMesh::get_num_columns() const
+{
+   return num_columns;
+}
+
+
+int TileMesh::get_num_rows() const
+{
+   return num_rows;
+}
+
+
+int TileMesh::get_tile_width() const
+{
+   return tile_width;
+}
+
+
+int TileMesh::get_tile_height() const
+{
+   return tile_height;
+}
+
+
+bool TileMesh::get_initialized() const
+{
+   return initialized;
+}
+
+
+std::vector<ALLEGRO_VERTEX> &TileMesh::get_vertexes_ref()
+{
+   return vertexes;
 }
 
 
@@ -44,6 +98,87 @@ void TileMesh::initialize()
    }
    // TODO: Guard on al_is_system_installed
    // TODO: Guard on al_is_primitives_addon_initialized
+   return;
+}
+
+void TileMesh::resize(int num_columns, int num_rows)
+{
+   // TODO: This method body
+   return;
+}
+
+void TileMesh::render(bool draw_outline)
+{
+   // TODO: This method body
+   return;
+}
+
+void TileMesh::set_tile_id(int tile_x, int tile_y, int tile_id)
+{
+   // TODO: This method body
+   return;
+}
+
+int TileMesh::get_tile_id(int tile_x, int tile_y)
+{
+   // TODO: This method body
+   return 0;
+}
+
+void TileMesh::set_tile_uv(int tile_x, int tile_y, int u1, int v1, int u2, int v2)
+{
+   // NOTE: Should the uv coordinates be floats?
+   // TODO: This method body
+   return;
+}
+
+int TileMesh::infer_num_vertexes()
+{
+   // TODO: This method body
+   return 0;
+}
+
+int TileMesh::infer_num_tiles()
+{
+   // TODO: This method body
+   return 0;
+}
+
+void TileMesh::rescale_tile_dimensions_to(int new_tile_width, int new_tile_height)
+{
+   // TODO: This method body
+   return;
+}
+
+void TileMesh::set_tile_width(int tile_width)
+{
+   // TODO: This method body
+   return;
+}
+
+void TileMesh::set_tile_height(int tile_width)
+{
+   // TODO: This method body
+   return;
+}
+
+int TileMesh::get_real_width()
+{
+   // TODO: This should be a float?
+   // TODO: This method body
+   return 0;
+}
+
+int TileMesh::get_real_height()
+{
+   // TODO: This should be a float?
+   // TODO: This method body
+   return 0;
+}
+
+void TileMesh::swap_yz()
+{
+   // TODO: This method body
    return;
 }
 
