6a9a9311648c5d91dfb69d43b6338ee349684834
Mark Oates <markoates0@gmail.com>
2024-05-05 10:55:01 -0400

    * Add test fixturee with setup

--- tests/AllegroFlare/TileMaps/TileMeshTest.cpp
+++ tests/AllegroFlare/TileMaps/TileMeshTest.cpp
@@ -3,20 +3,78 @@
 #include <gtest/gtest.h>
 
 #include <AllegroFlare/Testing/ErrorAssertions.hpp>
-
 #include <AllegroFlare/Testing/WithAllegroRenderingFixture.hpp>
+
 #include <AllegroFlare/TileMaps/TileMesh.hpp>
+
+const std::string TEST_TILE_MAP_BITMAP = "tiles_dungeon_v1.1.png";
+#include <AllegroFlare/Random.hpp>
+#include <allegro5/allegro_color.h>
+
 
 class AllegroFlare_TileMaps_TileMeshWithAllegroRenderingFixtureTest
    : public AllegroFlare::Testing::WithAllegroRenderingFixture
 {};
+class AllegroFlare_TileMaps_TileMeshWithAllegroRenderingFixtureTestAndSetup
+   : public AllegroFlare::Testing::WithAllegroRenderingFixture
+{
+public:
+   ALLEGRO_BITMAP *atlas_bitmap;
+   AllegroFlare::TileMaps::PrimMeshAtlas atlas;
+   AllegroFlare::TileMaps::TileMesh mesh; //(&atlas, 30, 10, 1, 1); // Set the initial tile size to 1
 
+   AllegroFlare_TileMaps_TileMeshWithAllegroRenderingFixtureTestAndSetup()
+      : atlas_bitmap(nullptr)
+      , atlas()
+      , mesh(&atlas, 30, 10, 1, 1)
+   {}
 
+   void SetUp() override
+   {
+      AllegroFlare::Testing::WithAllegroRenderingFixture::SetUp();
 
-const std::string TEST_TILE_MAP_BITMAP = "tiles_dungeon_v1.1.png";
+      atlas_bitmap = get_bitmap_bin_ref()[TEST_TILE_MAP_BITMAP];
+      atlas.duplicate_bitmap_and_load(atlas_bitmap, 16, 16);
+      //al_destroy_bitmap(atlas_bitmap);
 
-#include <AllegroFlare/Random.hpp>
-#include <allegro5/allegro_color.h>
+      //mesh.set_atlas(&atlas);
+      //mesh.set_num_columns(30);
+      //mesh.set_num_rows(10);
+      //mesh.set_tile_width(1);
+      //mesh.set_tile_height(1);
+
+      mesh.initialize();
+   }
+
+   void TearDown() override
+   {
+      AllegroFlare::Testing::WithAllegroRenderingFixture::TearDown();
+   }
+
+   void render_subject(float duration_sec=1.0f)
+   {
+      al_clear_to_color(al_color_name("dodgerblue"));
+
+      float x = 100;
+      float y = 50;
+
+      float sx = 3;
+      float sy = 3;
+
+      ALLEGRO_TRANSFORM prev, transform;
+      al_copy_transform(&prev, al_get_current_transform());
+      al_identity_transform(&transform);
+      al_translate_transform(&transform, x, y);
+      al_scale_transform(&transform, sx, sy);
+      al_use_transform(&transform);
+
+      mesh.render();
+      al_flip_display();
+
+      al_use_transform(&prev);
+      al_rest(duration_sec);
+   }
+};
 
 
 TEST_F(AllegroFlare_TileMaps_TileMeshWithAllegroRenderingFixtureTest, render__without_initialized__throws_an_error)
@@ -166,6 +224,32 @@ TEST_F(AllegroFlare_TileMaps_TileMeshWithAllegroRenderingFixtureTest,
    // flip the display and sleep
    al_flip_display();
    al_rest(1.0);
+}
+
+
+
+TEST_F(AllegroFlare_TileMaps_TileMeshWithAllegroRenderingFixtureTestAndSetup,
+   CAPTURE__VISUAL__render__will_render_the_mesh_as_expected__solid_tiles_example)
+{
+   // Fill the tile mesh with random values
+   mesh.enable_holding_vertex_buffer_update_until_refresh();
+
+   std::vector<int> possible_random_tiles = { 82, 102, 122, 121, 81 };
+   AllegroFlare::Random random;
+   for (int y=0; y<mesh.get_num_rows(); y++)
+   {
+      for (int x=0; x<mesh.get_num_columns(); x++)
+      {
+         int random_tile = possible_random_tiles[random.get_random_int(0, possible_random_tiles.size()-1)];
+         //int random_tile = random.get_random_int(0, atlas.get_tile_index_size());
+         mesh.set_tile_id(x, y, random_tile);
+      }
+   }
+
+   // Rescale the tile dimensions to something more arbitrary
+   mesh.refresh_vertex_buffer();
+
+   render_subject(1.0f);
 }
 
 
