bbd485010c9f71dac65056cad9c3e802a670cb1f
Mark Oates <markoates0@gmail.com>
2024-05-08 20:32:16 -0400

    * Add test coverage for #get_sprite_sheet_cell_index_num_at# on PLAYMODE_FORWARD_ONCE

--- tests/AllegroFlare/FrameAnimation/AnimationTest.cpp
+++ tests/AllegroFlare/FrameAnimation/AnimationTest.cpp
@@ -47,27 +47,10 @@ public:
       sprite_sheet->initialize();
 
       al_destroy_bitmap(sprite_sheet_bitmap);
-
-      //Animation animation(sprite_sheet, "my_animation", std::vector<Frame>{
-            //{ 1, 0.200f },
-            //{ 2, 0.100f },
-            //{ 3, 0.200f },
-         //},
-         //Animation::PLAYMODE_FORWARD_PING_PONG
-      //);
-
-      //animation.initialize();
    }
    void setup_animation(std::vector<Frame> frames, Animation::Playmode playmode)
    {
-      animation = new Animation(sprite_sheet, "my_animation", frames, playmode); //std::vector<Frame>{
-            //{ 1, 0.200f },
-            //{ 2, 0.100f },
-            //{ 3, 0.200f },
-         //},
-         //playmode
-      //);
-
+      animation = new Animation(sprite_sheet, "my_animation", frames, playmode);
       animation->initialize();
    }
    void display_subject()
@@ -109,72 +92,6 @@ TEST_F(AllegroFlare_FrameAnimation_AnimationTest, can_be_created_without_blowing
 }
 
 
-TEST_F(AllegroFlare_FrameAnimation_AnimationTest,
-   set_playspeed_multiplier__with_a_value_less_than_0__will_throw_an_error)
-{
-   AllegroFlare::FrameAnimation::Animation animation;
-   EXPECT_THROW_WITH_MESSAGE(
-      animation.set_playspeed_multiplier(-0.0001),
-      std::runtime_error,
-      "Animation::set_playspeed_multiplier: error: guard \"(playspeed_multiplier >= 0.0f)\" not met"
-   );
-}
-
-
-TEST_F(AllegroFlare_FrameAnimation_AnimationTest, will_work_as_expected)
-{
-   al_init();
-   al_init_image_addon();
-   al_init_font_addon();
-   al_init_ttf_addon();
-
-   ALLEGRO_DISPLAY *display = al_create_display(1920, 1080);
-   ALLEGRO_FONT *font = al_load_font("./bin/data/fonts/ChronoTrigger.ttf", -50, 0);
-   ALLEGRO_BITMAP *sprite_sheet_bitmap = al_load_bitmap("./bin/data/bitmaps/sprites_grid-x.png");
-   EXPECT_NE(nullptr, sprite_sheet_bitmap);
-   AllegroFlare::FrameAnimation::SpriteSheet sprite_sheet(sprite_sheet_bitmap, 48, 48, 5);
-   sprite_sheet.initialize();
-
-   al_destroy_bitmap(sprite_sheet_bitmap);
-
-   Animation animation(&sprite_sheet, "my_animation", std::vector<Frame>{
-         { 1, 0.200f },
-         { 2, 0.100f },
-         { 3, 0.200f },
-      },
-      Animation::PLAYMODE_FORWARD_PING_PONG
-   );
-
-   animation.initialize();
-
-   
-   int frames = 120;
-   animation.start();
-   for (int i=0; i<frames; i++)
-   {
-      al_clear_to_color(ALLEGRO_COLOR{0, 0, 0, 1});
-
-      // update and draw
-      animation.update();
-      animation.draw();
-
-      // draw info text
-      int sprite_sheet_cell_index_num = animation.get_sprite_sheet_cell_index_num_at(0.21);
-      al_draw_textf(font, ALLEGRO_COLOR{1, 1, 1, 1}, 10, 10, 0, "frame %d", sprite_sheet_cell_index_num);
-
-      al_flip_display(); // assumes a rest of 1/60.0f
-   }
-
-   sprite_sheet.destroy();
-   al_destroy_font(font);
-   al_destroy_display(display);
-   al_shutdown_font_addon();
-   al_shutdown_ttf_addon();
-   al_shutdown_image_addon();
-   al_uninstall_system();
-}
-
-
 TEST_F(AllegroFlare_FrameAnimation_AnimationTest, before_initialization__some_select_functions_will_throw_an_error)
 {
    // Trying this fancy EXPECT_GUARD out here
@@ -198,17 +115,49 @@ TEST_F(AllegroFlare_FrameAnimation_AnimationTest, before_initialization__some_se
 }
 
 
-TEST_F(AllegroFlare_FrameAnimation_AnimationTestWithSetup, FOCUS__test_fixture_will_work_as_expected)
+TEST_F(AllegroFlare_FrameAnimation_AnimationTest,
+   set_playspeed_multiplier__with_a_value_less_than_0__will_throw_an_error)
+{
+   AllegroFlare::FrameAnimation::Animation animation;
+   EXPECT_THROW_WITH_MESSAGE(
+      animation.set_playspeed_multiplier(-0.0001),
+      std::runtime_error,
+      "Animation::set_playspeed_multiplier: error: guard \"(playspeed_multiplier >= 0.0f)\" not met"
+   );
+}
+
+
+TEST_F(AllegroFlare_FrameAnimation_AnimationTestWithSetup, DISABLED__test_fixture_will_work_as_expected)
 {
    setup_animation(
-      std::vector<Frame>{
-         { 1, 0.200f },
-         { 2, 0.100f },
-         { 3, 0.200f },
-      },
+      std::vector<Frame>{{ 1, 0.2f }, { 2, 0.1f }, { 3, 0.2f }},
       Animation::PLAYMODE_FORWARD_PING_PONG
    );
    display_subject();
+}
+
+
+TEST_F(AllegroFlare_FrameAnimation_AnimationTestWithSetup, 
+   get_sprite_sheet_cell_index_num_at__on_a_PLAYMODE_FORWARD_ONCE__will_return_the_cell_index_num_at_that_time)
+{
+   setup_animation(
+      std::vector<Frame>{{ 1, 0.2f }, { 2, 0.1f }, { 3, 0.2f }},
+      Animation::PLAYMODE_FORWARD_ONCE
+   );
+
+   EXPECT_EQ(1, animation->get_sprite_sheet_cell_index_num_at(0.0));
+   EXPECT_EQ(1, animation->get_sprite_sheet_cell_index_num_at(0.101));
+   EXPECT_EQ(1, animation->get_sprite_sheet_cell_index_num_at(0.199));
+
+   EXPECT_EQ(2, animation->get_sprite_sheet_cell_index_num_at(0.2));
+   EXPECT_EQ(2, animation->get_sprite_sheet_cell_index_num_at(0.201));
+   EXPECT_EQ(2, animation->get_sprite_sheet_cell_index_num_at(0.299));
+
+   EXPECT_EQ(3, animation->get_sprite_sheet_cell_index_num_at(0.3));
+   EXPECT_EQ(3, animation->get_sprite_sheet_cell_index_num_at(0.301));
+   EXPECT_EQ(3, animation->get_sprite_sheet_cell_index_num_at(0.499));
+
+   EXPECT_EQ(-1, animation->get_sprite_sheet_cell_index_num_at(0.5001f));
 }
 
 
