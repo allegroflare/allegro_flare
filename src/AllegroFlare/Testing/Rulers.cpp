12683ee295b437e12514641bd42b70b7b23de38a
Mark Oates <markoates0@gmail.com>
2024-05-12 10:51:59 -0400

    * Delegate to Rulers in Testing/Rulers

--- src/AllegroFlare/Testing/Rulers.cpp
+++ src/AllegroFlare/Testing/Rulers.cpp
@@ -2,8 +2,7 @@
 
 #include <AllegroFlare/Testing/Rulers.hpp>
 
-#include <allegro5/allegro.h>
-#include <allegro5/allegro_primitives.h>
+#include <AllegroFlare/Rulers.hpp>
 #include <iostream>
 #include <sstream>
 #include <stdexcept>
@@ -41,28 +40,8 @@ void Rulers::draw_dimensional_grid(float x, float y, float z, float spacing)
       std::cerr << "\033[1;31m" << error_message.str() << " An exception will be thrown to halt the program.\033[0m" << std::endl;
       throw std::runtime_error("Rulers::draw_dimensional_grid: error: guard \"al_is_primitives_addon_initialized()\" not met");
    }
-   for (int zz=-10; zz<10; zz++)
-      for (int xx=-10; xx<10; xx++)
-         for (int yy=-10; yy<10; yy++)
-         {
-            ALLEGRO_STATE previous_target_bitmap_state;
-            al_store_state(&previous_target_bitmap_state, ALLEGRO_STATE_TRANSFORM);
-            //al_set_target_bitmap(display_bitmap);
-            //al_use_projection_transform(&trans);
-
-            ALLEGRO_TRANSFORM trans;
-
-            al_copy_transform(&trans, al_get_current_transform());
-
-            //al_identity_transform(&trans);
-            al_translate_transform_3d(&trans, x+xx*spacing, y+yy*spacing, z+zz*spacing);
-            al_use_transform(&trans);
-            al_draw_filled_rectangle(-0.5, -0.5, 0.5, 0.5, ALLEGRO_COLOR{0.5, 0.5, 0.5, 0.5});
-            //al_start_transform(&trans);
-
-            al_restore_state(&previous_target_bitmap_state);
-         }
-
+   // TODO: Test this method
+   AllegroFlare::Rulers::draw_dimensional_grid(x, y, z, spacing);
    return;
 }
 
@@ -82,27 +61,8 @@ void Rulers::draw_2d_grid(float x, float y, float size, float spacing)
       std::cerr << "\033[1;31m" << error_message.str() << " An exception will be thrown to halt the program.\033[0m" << std::endl;
       throw std::runtime_error("Rulers::draw_2d_grid: error: guard \"al_is_primitives_addon_initialized()\" not met");
    }
-   for (int xx=-10; xx<10; xx++)
-      for (int yy=-10; yy<10; yy++)
-      {
-         ALLEGRO_STATE previous_target_bitmap_state;
-         al_store_state(&previous_target_bitmap_state, ALLEGRO_STATE_TRANSFORM);
-         //al_set_target_bitmap(display_bitmap);
-         //al_use_projection_transform(&trans);
-
-         ALLEGRO_TRANSFORM trans;
-
-         al_copy_transform(&trans, al_get_current_transform());
-
-         //al_identity_transform(&trans);
-         al_translate_transform_3d(&trans, x+xx*spacing, y+yy*spacing, 0); //z+zz*spacing);
-         al_use_transform(&trans);
-         al_draw_filled_rectangle(-size*0.5, -size*0.5, size*0.5, size*0.5, ALLEGRO_COLOR{0.5, 0.5, 0.5, 0.5});
-         //al_start_transform(&trans);
-
-         al_restore_state(&previous_target_bitmap_state);
-      }
-
+   // TODO: Test this method
+   AllegroFlare::Rulers::draw_2d_grid(x, y, size, spacing);
    return;
 }
 
