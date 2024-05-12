53f827aaa55640ff322e02704488b0fb6d36e803
Mark Oates <markoates0@gmail.com>
2024-05-12 10:40:07 -0400

    * Pass along params to test subject method

--- tests/AllegroFlare/RulersTest.cpp
+++ tests/AllegroFlare/RulersTest.cpp
@@ -4,6 +4,7 @@
 #include <AllegroFlare/Testing/WithAllegroRenderingFixture.hpp>
 #include <AllegroFlare/Rulers.hpp>
 #include <allegro5/allegro_primitives.h> // for al_is_primitives_addon_initialized();
+#include <allegro5/allegro_color.h> // for al_color_html();
 
 
 class AllegroFlare_RulersTest : public ::testing::Test {};
@@ -21,8 +22,14 @@ TEST_F(AllegroFlare_RulersTest, can_be_created_without_blowing_up)
 
 TEST_F(AllegroFlare_RulersTestWithAllegroRenderingFixture, CAPTURE__render__will_not_blow_up)
 {
+   AllegroFlare::Placement2D subject_placement = build_centered_placement();
    AllegroFlare::Rulers rulers(&get_font_bin_ref());
-   rulers.draw_vertical_ruler();
+
+   clear();
+   subject_placement.start_transform();
+   rulers.draw_vertical_ruler(0, 0, 30.0, 4.0, 160.0, 5, 0.0, 0.0, al_color_html("b8e8e8"));
+   subject_placement.restore_transform();
+
    al_flip_display();
    sleep_for(1);
 }
