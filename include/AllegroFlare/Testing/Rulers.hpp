7824820e345db7dca41267aa02a11e6fa98eb3b8
Mark Oates <markoates0@gmail.com>
2024-05-13 09:46:28 -0400

    * Use Rulers as parent class for Testing/Rulers; Remove previous methods

--- include/AllegroFlare/Testing/Rulers.hpp
+++ include/AllegroFlare/Testing/Rulers.hpp
@@ -1,13 +1,14 @@
 #pragma once
 
 
+#include <AllegroFlare/Rulers.hpp>
 
 
 namespace AllegroFlare
 {
    namespace Testing
    {
-      class Rulers
+      class Rulers : public AllegroFlare::Rulers
       {
       private:
 
@@ -18,8 +19,6 @@ namespace AllegroFlare
          Rulers();
          ~Rulers();
 
-         static void draw_dimensional_grid(float x=0.0f, float y=0.0f, float z=0.0f, float spacing=(16*4.8));
-         static void draw_2d_grid(float x=0.0f, float y=0.0f, float size=1.0f, float spacing=(16*4.8));
       };
    }
 }
--- src/AllegroFlare/Testing/Rulers.cpp
+++ src/AllegroFlare/Testing/Rulers.cpp
@@ -2,10 +2,7 @@
 
 #include <AllegroFlare/Testing/Rulers.hpp>
 
-#include <AllegroFlare/Rulers.hpp>
-#include <iostream>
-#include <sstream>
-#include <stdexcept>
+
 
 
 namespace AllegroFlare
@@ -15,6 +12,7 @@ namespace Testing
 
 
 Rulers::Rulers()
+   : AllegroFlare::Rulers()
 {
 }
 
@@ -24,47 +22,6 @@ Rulers::~Rulers()
 }
 
 
-void Rulers::draw_dimensional_grid(float x, float y, float z, float spacing)
-{
-   if (!(al_is_system_installed()))
-   {
-      std::stringstream error_message;
-      error_message << "[Rulers::draw_dimensional_grid]: error: guard \"al_is_system_installed()\" not met.";
-      std::cerr << "\033[1;31m" << error_message.str() << " An exception will be thrown to halt the program.\033[0m" << std::endl;
-      throw std::runtime_error("Rulers::draw_dimensional_grid: error: guard \"al_is_system_installed()\" not met");
-   }
-   if (!(al_is_primitives_addon_initialized()))
-   {
-      std::stringstream error_message;
-      error_message << "[Rulers::draw_dimensional_grid]: error: guard \"al_is_primitives_addon_initialized()\" not met.";
-      std::cerr << "\033[1;31m" << error_message.str() << " An exception will be thrown to halt the program.\033[0m" << std::endl;
-      throw std::runtime_error("Rulers::draw_dimensional_grid: error: guard \"al_is_primitives_addon_initialized()\" not met");
-   }
-   // TODO: Test this method
-   AllegroFlare::Rulers::draw_dimensional_grid(x, y, z, spacing);
-   return;
-}
-
-void Rulers::draw_2d_grid(float x, float y, float size, float spacing)
-{
-   if (!(al_is_system_installed()))
-   {
-      std::stringstream error_message;
-      error_message << "[Rulers::draw_2d_grid]: error: guard \"al_is_system_installed()\" not met.";
-      std::cerr << "\033[1;31m" << error_message.str() << " An exception will be thrown to halt the program.\033[0m" << std::endl;
-      throw std::runtime_error("Rulers::draw_2d_grid: error: guard \"al_is_system_installed()\" not met");
-   }
-   if (!(al_is_primitives_addon_initialized()))
-   {
-      std::stringstream error_message;
-      error_message << "[Rulers::draw_2d_grid]: error: guard \"al_is_primitives_addon_initialized()\" not met.";
-      std::cerr << "\033[1;31m" << error_message.str() << " An exception will be thrown to halt the program.\033[0m" << std::endl;
-      throw std::runtime_error("Rulers::draw_2d_grid: error: guard \"al_is_primitives_addon_initialized()\" not met");
-   }
-   // TODO: Test this method
-   AllegroFlare::Rulers::draw_2d_grid(x, y, size, spacing);
-   return;
-}
 
 
 } // namespace Testing
