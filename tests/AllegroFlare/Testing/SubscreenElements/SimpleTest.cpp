5931292f2b8cfc3a3de4c5b420655676c5a02677
Mark Oates <markoates0@gmail.com>
2024-06-06 10:38:57 -0400

    Revert "Remove unused Testing/SubscreenElements/Simple"
    This reverts commit 8c0563dcccfc055a353f4956894201eea269741f.

---
+++ tests/AllegroFlare/Testing/SubscreenElements/SimpleTest.cpp
@@ -0,0 +1,12 @@
+
+#include <gtest/gtest.h>
+
+#include <AllegroFlare/Testing/SubscreenElements/Simple.hpp>
+
+
+TEST(AllegroFlare_Testing_SubscreenElements_SimpleTest, can_be_created_without_blowing_up)
+{
+   AllegroFlare::Testing::SubscreenElements::Simple simple;
+}
+
+
