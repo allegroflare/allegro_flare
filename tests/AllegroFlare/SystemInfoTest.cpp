98374861380e9d125fb6159acc35f5682ead2b16
Mark Oates <markoates0@gmail.com>
2024-06-08 17:11:04 -0400

    * Fix two failing tests in SystemInfo related to new test environment

--- tests/AllegroFlare/SystemInfoTest.cpp
+++ tests/AllegroFlare/SystemInfoTest.cpp
@@ -54,7 +54,8 @@ TEST(AllegroFlare_SystemInfoTest, get_hostname__will_return_a_string_representin
    AllegroFlare::SystemInfo system_info;
    std::vector<std::string> expected_possible_hostnames = {
       "DESKTOP-NC9M1BH",            // Mark's Laptop
-      "Marks-13-MacBook-Pro.local", // Mark's Mac Laptop
+      "Marks-MacBook-Pro.local", // Mark's Mac Laptop
+      "Marks-13-MacBook-Pro.local", // Mark's 13" Mac Laptop
       "Marks-Mac-mini.local",       // Mark's MacMini
    };
    std::string actual_hostname = system_info.get_hostname();
@@ -76,17 +77,19 @@ TEST(AllegroFlare_SystemInfoTest, get_version__will_return_a_string_representing
    EXPECT_THAT(expected_possible_versions, testing::Contains(actual_version));
 #elif __APPLE__ || __MACH__
    // NOTE: The regex below will match MacOS version strings, some examples: [
-   //    //Mark's Laptop:
+   //    //Mark's 13" Laptop:
    //   "Darwin Kernel Version 22.3.0: Mon Jan 30 20:39:35 PST 2023; root:xnu-8792.81.3~2/RELEASE_ARM64_T8103",
    //   "Darwin Kernel Version 22.5.0: Mon Apr 24 20:53:44 PDT 2023; root:xnu-8796.121.2~5/RELEASE_ARM64_T8103",
    //   // Mark's MacMini:
    //   "Darwin Kernel Version 21.6.0: Wed Aug 10 14:25:27 PDT 2022; root:xnu-8020.141.5~2/RELEASE_X86_64",
+   //   // Mark's Macbook Pro
+   //   "Darwin Kernel Version 23.3.0: Wed Dec 20 21:31:10 PST 2023; root:xnu-10002.81.5~7/RELEASE_ARM64_T6031",
    //]
 
    std::string regex_pattern = "^Darwin Kernel Version \\d+\\.\\d+\\.\\d+: "
       "(Mon|Tue|Wed|Thu|Fri|Sat|Sun) "
       "(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec) ( ?\\d{1}|\\d{2}) \\d{2}:\\d{2}:\\d{2} (PST|PDT) \\d{4}; "
-      "root:xnu-\\d+\\.\\d+\\.\\d+~\\d+/RELEASE_(ARM64_T8103|X86_64)$";
+      "root:xnu-\\d+\\.\\d+\\.\\d+~\\d+\\/RELEASE_(ARM64_T(\\d{4})|X86_64)$";
    std::regex version_regex(regex_pattern);
    bool is_match = std::regex_match(actual_version, version_regex);
    EXPECT_TRUE(is_match) << "\"" << actual_version << "\"" << " does not match the regex \"" << regex_pattern << "\"";
