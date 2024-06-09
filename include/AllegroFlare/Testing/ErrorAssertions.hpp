163f1cd22a7a9961feb175d47b1de1c85b5f60c9
Mark Oates <markoates0@gmail.com>
2024-06-09 10:27:01 -0400

    * Fix multiline comment warning

--- include/AllegroFlare/Testing/ErrorAssertions.hpp
+++ include/AllegroFlare/Testing/ErrorAssertions.hpp
@@ -14,14 +14,9 @@
    catch ( raised_exception_type const &err ) { ASSERT_EQ(err.what(), std::string( raised_exception_message )); } \
    catch (...) { FAIL() << "Expected " # raised_exception_type; }
 
-
+// NOTE: this error message format will need to be updated with brackets if/when the error format changes
 #define BUILD_GUARD_ERROR_MESSAGE(throw_from, guard_statement) \
    throw_from ": error: guard \"" guard_statement "\" not met"
-
-// NOTE: this will be used in the future if/when the error format changes
-//#define BUILD_GUARD_ERROR_MESSAGE(throw_from, guard_statement) \
-// "[" throw_from "]: error: guard \"" guard_statement "\" not met."
-
 
 #define EXPECT_THROW_GUARD_ERROR(code, throw_from, guard_statement) \
    EXPECT_THROW_WITH_MESSAGE(code, std::runtime_error, BUILD_GUARD_ERROR_MESSAGE(throw_from, guard_statement))
