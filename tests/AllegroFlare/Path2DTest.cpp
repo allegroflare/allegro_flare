f81e1b44801c92612eb3c82fd550082bfb3993ec
Mark Oates <markoates0@gmail.com>
2024-09-08 11:16:53 -0400

    * Add Path2D test for #soften#

--- tests/AllegroFlare/Path2DTest.cpp
+++ tests/AllegroFlare/Path2DTest.cpp
@@ -25,8 +25,27 @@ TEST_F(AllegroFlare_Path2DTestWithAllegroRenderingFixture, CAPTURE__render__will
    path_2d.add_point(cx+380, cy+20);
    path_2d.add_point(cx-50, cy+180);
 
-   path_2d.draw(true, false, false, true, ALLEGRO_COLOR{0.2, 0.9, 0.76, 1.0});
+   path_2d.draw(true, false, false, true, ALLEGRO_COLOR{0.1, 0.45, 0.36, 1.0});
 
+   al_flip_display();
+}
+
+
+TEST_F(AllegroFlare_Path2DTestWithAllegroRenderingFixture, CAPTURE__soften__will_make_the_path_smoother)
+{
+   AllegroFlare::Path2D path_2d;
+   float cx = 1920/2;
+   float cy = 1080/2;
+   path_2d.add_point(cx-400, cy-10);
+   path_2d.add_point(cx-0, cy-220);
+   path_2d.add_point(cx+380, cy+20);
+   path_2d.add_point(cx-50, cy+180);
+
+   path_2d.draw(true, false, false, true, ALLEGRO_COLOR{0.1, 0.45, 0.38, 1.0}, 1.0);
+   path_2d.soften();
+   path_2d.draw(true, false, false, true, ALLEGRO_COLOR{0.18, 0.65, 0.52, 1.0}, 2.0);
+   path_2d.soften();
+   path_2d.draw(true, false, false, true, ALLEGRO_COLOR{0.72, 1.0, 0.71, 1.0}, 3.0);
    al_flip_display();
 
    al_rest(1);
