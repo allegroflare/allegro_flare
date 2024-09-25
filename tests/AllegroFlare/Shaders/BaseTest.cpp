242d547d33b9be915e19600431d14606da6454b7
Mark Oates <markoates0@gmail.com>
2024-09-25 11:51:06 -0400

    * Remove comment causing warning on win build

--- tests/AllegroFlare/Shaders/BaseTest.cpp
+++ tests/AllegroFlare/Shaders/BaseTest.cpp
@@ -8,12 +8,6 @@
 #include <AllegroFlare/Testing/ErrorAssertions.hpp>
 
 
-//#define ASSERT_THROW_WITH_MESSAGE(code, raised_exception_type, raised_exception_message) \
-   //try { code; FAIL() << "Expected " # raised_exception_type; } \
-   //catch ( raised_exception_type const &err ) { EXPECT_EQ(err.what(), std::string( raised_exception_message )); } \
-   //catch (...) { FAIL() << "Expected " # raised_exception_type; }
-
-
 class AllegroFlare_Shaders_BaseTest : public ::testing::Test {};
 class AllegroFlare_Shaders_BaseWithAllegroRenderingFixtureTest
    : public AllegroFlare::Testing::WithAllegroRenderingFixture
