c175c76650aa31b90b807cdbb7b368de31d3d37d
Mark Oates <markoates0@gmail.com>
2024-10-15 19:44:36 -0400

    * Add Testing/ColorPickingAssertions

---
+++ include/AllegroFlare/Testing/ColorPickingAssertions.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+
+#include <AllegroFlare/Testing/Comparison/ALLEGRO_COLOR.hpp>
+
+
+#define EXPECT_PICKED_COLOR_EQ(expected_ALLEGRO_COLOR, source_ALLEGRO_BITMAP, pixel_x, pixel_y) \
+   { \
+      ALLEGRO_COLOR actual_ALLEGRO_COLOR = al_get_pixel(source_ALLEGRO_BITMAP, pixel_x, pixel_y); \
+      EXPECT_EQ(expected_ALLEGRO_COLOR, actual_ALLEGRO_COLOR) << "Color at (" << pixel_x << ", " << pixel_y << ")" \
+      "does not match"; \
+   }
+
+
---
+++ tests/AllegroFlare/Testing/ColorPickingAssertionsTest.cpp
@@ -0,0 +1,24 @@
+
+#include <gtest/gtest.h>
+
+#include <AllegroFlare/Testing/ColorPickingAssertions.hpp>
+
+
+TEST(AllegroFlare_Testing_ColorPickingAssertions, EXPECT_PICKED_COLOR_EQ__will_return_true_when_the_bitmap_has_a_\
+pixel_at_that_coordinate)
+{
+   al_init();
+   ALLEGRO_DISPLAY *display = al_create_display(800, 600);
+   ALLEGRO_COLOR color = ALLEGRO_COLOR{0.6f, 0.2f, 0.8f, 1.0f};
+   al_clear_to_color(color);
+
+   ALLEGRO_COLOR expected_color = color;
+   ALLEGRO_BITMAP *subject_surface = al_get_backbuffer(al_get_current_display());
+
+   EXPECT_PICKED_COLOR_EQ(expected_color, subject_surface, 400, 300);
+
+   al_destroy_display(display);
+   al_uninstall_system();
+}
+
+
