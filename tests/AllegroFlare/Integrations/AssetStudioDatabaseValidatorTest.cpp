818c6ab081810ac60aa05cc610fb1356913e50f7
Mark Oates <markoates0@gmail.com>
2024-10-19 23:37:23 -0400

    * Uncomment and cleanup test for AssetStudioDatabaseValidator

--- tests/AllegroFlare/Integrations/AssetStudioDatabaseValidatorTest.cpp
+++ tests/AllegroFlare/Integrations/AssetStudioDatabaseValidatorTest.cpp
@@ -4,6 +4,7 @@
 #include <AllegroFlare/Integrations/AssetStudioDatabaseValidator.hpp>
 #include <AllegroFlare/AssetStudio/Database.hpp>
 #include <AllegroFlare/AssetStudio/DatabaseCSVLoader.hpp>
+#include <AllegroFlare/DeploymentEnvironment.hpp>
 
 
 // TODO: See if this hard-coded pathing can be improved - move it to a fixture variable, in-test variable, for example
@@ -30,27 +31,51 @@ TEST(AllegroFlare_Integrations_AssetStudioDatabaseValidatorTest, load_records__o
 }
 
 
-/*
 TEST(AllegroFlare_AssetStudio_DatabaseCSVLoaderTest,
-   load__when_loading_from_the_source_data__will_create_the_expected_assets)
+   when_loading_records_from_the_assets_db_csv_file__will_work_as_expected)
 {
    al_init();
    al_init_image_addon();
 
-   AllegroFlare::BitmapBin assets_bitmap_bin;
-   assets_bitmap_bin.set_full_path(ASSETS_FULL_PATH);
    AllegroFlare::AssetStudio::DatabaseCSVLoader loader;
-   loader.set_assets_bitmap_bin(&assets_bitmap_bin);
    loader.set_csv_full_path(ASSETS_DB_CSV_FILENAME);
-   loader.load();
+   loader.load_records();
 
-   EXPECT_EQ(true, loader.asset_exists("seethingswarm/catset/cat01_walk_strip8"));
+   EXPECT_EQ(true, loader.record_exists("seethingswarm/catset/cat01_walk_strip8"));
+   // TODO: Consider testing additional records and fields
 
-   assets_bitmap_bin.clear();
    al_uninstall_system();
 }
 
 
+TEST(AllegroFlare_AssetStudio_DatabaseCSVLoaderTest,
+   when_loading_assets_from_records_imported_from_the_csv_records__will_work_as_expected)
+{
+   AllegroFlare::DeploymentEnvironment deployment_environment("test");
+   std::string data_path = deployment_environment.get_data_folder_path();
+
+   al_init();
+   al_init_image_addon();
+
+   AllegroFlare::AssetStudio::DatabaseCSVLoader loader;
+   loader.set_csv_full_path(ASSETS_DB_CSV_FILENAME);
+   loader.load_records();
+
+   AllegroFlare::BitmapBin assets_bitmap_bin;
+   assets_bitmap_bin.set_full_path("/Users/markoates/Assets/");
+   AllegroFlare::AssetStudio::Database database;
+   database.set_assets_bitmap_bin(&assets_bitmap_bin);
+   database.set_global_records(loader.get_records());
+   database.load_all_global_assets_from_all_global_records();
+
+   //EXPECT_EQ(true, loader.record_exists("seethingswarm/catset/cat01_walk_strip8"));
+   // TODO: Destroy database?
+
+   al_uninstall_system();
+}
+
+
+/*
 TEST(AllegroFlare_AssetStudio_DatabaseCSVLoaderTest,
    //load__when_loading_from_the_source_data__will_create_the_expected_records)
    load__when_loading_from_the_source_data__will_load_assets_with_the_expected_data)
