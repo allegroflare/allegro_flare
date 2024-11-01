54107ec70983081878a0ca4b4c7a36e60bcd9ee3
Mark Oates <markoates0@gmail.com>
2024-11-01 12:46:10 -0400

    * Cleanup some placeholder code in BitmapInfo

--- src/AllegroFlare/BitmapInfo.cpp
+++ src/AllegroFlare/BitmapInfo.cpp
@@ -62,7 +62,6 @@ void BitmapInfo::initialize()
       std::cerr << "\033[1;31m" << error_message.str() << " An exception will be thrown to halt the program.\033[0m" << std::endl;
       throw std::runtime_error("[AllegroFlare::BitmapInfo::initialize]: error: guard \"(!initialized)\" not met");
    }
-   //this->bitmap = bitmap;
    flags = al_get_bitmap_flags(bitmap);
    pixel_format = al_get_bitmap_format(bitmap);
    //depth = al_get_bitmap_depth(bitmap); // NOTE: Depth cannot be included unless ALLEGRO_UNSTABLE is defined
@@ -108,21 +107,6 @@ std::string BitmapInfo::build_report()
       { "sub_bitmap_y", std::to_string(sub_bitmap_y) },
       { "parent_bitmap", parent_bitmap_ss.str() },
    };
-   //int longest_label_length = 0;
-   //int longest_duration_length_in_chars = 0;
-   //for (auto &timer : timers)
-   //{
-      //int duration = timer.second.get_elapsed_time_milliseconds();
-
-   //data.push_back({timer.first, duration));
-   //int duration_length_in_chars = count_digits(duration);
-
-      //if (timer.first.size() > longest_label_length) longest_label_length = timer.first.size();
-      //if (duration_length_in_chars > longest_duration_length_in_chars)
-      //{
-         //longest_duration_length_in_chars = duration_length_in_chars;
-      //}
-   //}
 
    return format_table(
       data,
@@ -148,7 +132,6 @@ std::string BitmapInfo::format_table(std::vector<std::tuple<std::string, std::st
       result << std::right << std::setw(label_width) << label;
       result << " " << std::right << std::setfill('-') << std::setw(number_width + 1) << " " << number;
       result << std::setfill(' ');
-      //result << " " << unit << "\n";
       result << std::endl;
    }
 
