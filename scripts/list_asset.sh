264e7d7f67067f8dd534df3dd34267aa1b145dcf
Mark Oates <markoates0@gmail.com>
2025-01-17 13:00:15 -0500

    Expand "scripts/list_assets.sh" to accept multiple arguments

--- scripts/list_asset.sh
+++ scripts/list_asset.sh
@@ -1,15 +1,30 @@
 #!/bin/bash
 
 # Validate input
-if [ "$#" -ne 1 ]; then
-   echo "Usage: list_asset <asset_key>" >&2
+if [ "$#" -lt 1 ]; then
+   echo "Usage: list_asset <keyword_1> [<keyword_2> ... <keyword_n>]" >&2
    exit 1
 fi
 
 # Input variables
-asset_key=$1
 csv_file="/Users/markoates/Assets/assets_db.csv"
+num_keywords=$#
+search_pattern=$1
 
-# Search for the asset in the CSV file
-grep "$asset_key" "$csv_file"
+# Combine all arguments into a grep pattern
+shift
+for keyword in "$@"; do
+   search_pattern="$search_pattern|$keyword"
+done
+
+# Search for the combined pattern in the CSV file and count the results
+results=$(grep -E "$search_pattern" "$csv_file")
+num_results=$(echo "$results" | wc -l)
+
+# Output the results
+echo "$results"
+echo ""
+echo "Number of keywords: $num_keywords"
+echo "Number of results: $num_results"
+
 
