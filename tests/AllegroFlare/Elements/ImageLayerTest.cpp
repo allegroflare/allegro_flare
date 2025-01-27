143ea85e519c2f7c700cf7b09990ea7d0fa36f06
Mark Oates <markoates0@gmail.com>
2025-01-27 09:30:20 -0500

    * Add initial Elements/ImageLayer

---
+++ tests/AllegroFlare/Elements/ImageLayerTest.cpp
@@ -0,0 +1,12 @@
+
+#include <gtest/gtest.h>
+
+#include <AllegroFlare/Elements/ImageLayer.hpp>
+
+
+TEST(AllegroFlare_Elements_ImageLayerTest, can_be_created_without_blowing_up)
+{
+   AllegroFlare::Elements::ImageLayer image_layer;
+}
+
+
