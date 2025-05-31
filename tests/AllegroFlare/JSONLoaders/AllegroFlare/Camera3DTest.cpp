5302e8d654c23a84061d8b1bfd37f2a55b4d3a4c
Mark Oates <markoates0@gmail.com>
2025-05-31 18:07:14 -0400

    * Add tests for generated JSON when adding params

--- tests/AllegroFlare/JSONLoaders/AllegroFlare/Camera3DTest.cpp
+++ tests/AllegroFlare/JSONLoaders/AllegroFlare/Camera3DTest.cpp
@@ -27,27 +27,43 @@ R"({
 
 
 
-/*
 TEST(AllegroFlare_JSONLoaders_AllegroFlare_Camera3D, to_json__produces_the_expected_json)
 {
-   // NOTE: these test floating point values were chosen because they are not represented
-   // as long numbers when converted to outputs (e.g. "1.7f" would appear as "1.6999999...")
    AllegroFlare::Camera3D camera3d;
    camera3d.position = {3.5, 7.0, 1.625};
+   camera3d.stepout = {-2.125, -5.25, 0.0};
+   camera3d.spin = 123.0;
+   camera3d.tilt = 4.5;
+   camera3d.zoom = 1.0;
+   camera3d.roll = 2.0;
+   camera3d.near_plane = 0.125;
+   camera3d.far_plane = 100.0;
 
    nlohmann::json j = camera3d;
 
-   std::string expected_position_values =
+   std::string expected_json_string =
 R"({
-  "x": 3.5,
-  "y": 7.0,
-  "z": 1.625
+  "far_plane": 100.0,
+  "near_plane": 0.125,
+  "position": {
+    "x": 3.5,
+    "y": 7.0,
+    "z": 1.625
+  },
+  "roll": 2.0,
+  "spin": 123.0,
+  "stepout": {
+    "x": -2.125,
+    "y": -5.25,
+    "z": 0.0
+  },
+  "tilt": 4.5,
+  "zoom": 1.0
 })";
 
-   std::string actual_position_values = j["position"].dump(2);
-   EXPECT_EQ(expected_position_values, actual_position_values);
+   std::string actual_json_string = j.dump(2);
+   EXPECT_EQ(expected_json_string, actual_json_string);
 }
-*/
 
 
 
