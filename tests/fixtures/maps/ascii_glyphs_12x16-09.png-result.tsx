c92f8b1fe07b76cf99358177d19ba5a640d7cd1b
Mark Oates <markoates0@gmail.com>
2026-02-15 18:07:19 -0500

    Update

---
+++ tests/fixtures/maps/ascii_glyphs_12x16-09.png-result.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="ascii_glyphs_12x16-09.png-result" tilewidth="12" tileheight="16" tilecount="780" columns="30">
+ <image source="../Repos/allegro_flare/tests/fixtures/bitmaps/ascii_glyphs_12x16-09.png-result.png" width="360" height="416"/>
+</tileset>
