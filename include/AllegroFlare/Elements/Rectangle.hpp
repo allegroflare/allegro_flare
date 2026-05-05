0c4ab31e5bbe0a7e8aa9e199ff7400a4d2b29b81
Mark Oates <markoates0@gmail.com>
2026-05-05 14:07:51 -0400

    * Fix static method in Elements/Rectangle

--- include/AllegroFlare/Elements/Rectangle.hpp
+++ include/AllegroFlare/Elements/Rectangle.hpp
@@ -29,7 +29,7 @@ namespace AllegroFlare
          float h();
          void scale(float scale_x=1.0f, float scale_y=1.0f);
          void draw(ALLEGRO_COLOR color=ALLEGRO_COLOR{0.9961, 0.3451, 0.3412, 1.0}, float line_thickness=1.0f);
-         AllegroFlare::Elements::Rectangle build_xywh(float x=0.0f, float y=0.0f, float w=0.0f, float h=0.0f);
+         static AllegroFlare::Elements::Rectangle build_xywh(float x=0.0f, float y=0.0f, float w=0.0f, float h=0.0f);
          bool collides(AllegroFlare::Vec2D point={});
       };
    }
